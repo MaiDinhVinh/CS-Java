9bf6e96a920cb20cb76da561bc09907d5ae8aa25
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-04-19 01:32:04 +0700

    not finish yet

---
+++ 14-4-2024/bmi.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Lab2_ex4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your gender (m = male, f = female): ");
+        char gender = sc.next().charAt(0); 
+        switch (gender) {
+            case 'm':
+                
+        }
+    }
+}
