02febf9696e373b34616252b9b61d9eff5c69cbe
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-04-18 08:46:49 +0700

    Hoan thanh lab 1, co 2 cach co the su dung

---
+++ 14-4-2024/leap_year.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+//cach 1
+class Lab02_ex1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year;
+
+        System.out.print("Enter a year: ");
+        year = sc.nextInt();
+
+        if (year % 4 == 0) {
+            System.out.println("It is a leap year");
+        } else if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println("It is a leap year");
+        } else {
+            System.out.println("It is NOT a leap year");
+        }
+    }
+}
+
+//cach 2
+class Lab02_ex1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year;
+
+        System.out.print("Enter a year: ");
+        year = sc.nextInt();
+
+        if (year % 4 == 0 || year % 100 == 0 && year % 400 == 0) {
+            System.out.println("It is a leap year");
+        } else {
+            System.out.println("It is NOT a leap year");
+        }
+    }
+}
