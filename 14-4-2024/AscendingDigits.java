37e5dce128ce0cf56aa83b3d716158f0e82c01eb
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-04-19 14:35:00 +0700

    Chua lam duoc ex6 - lab02

---
+++ 14-4-2024/AscendingDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Lab02_ex6{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 3 positive integers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        //chia lay thuong so nguyen 
+        int x = a / 100;
+        int y = b / 100;
+        int z = c / 100;
+    }
+}
