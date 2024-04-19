16b68cc4980d23e59dc8cc9585d35b3392bfbcf3
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-04-19 22:53:39 +0700

    Homework Verified: Chinh lai phan input bi lap lai, tinh gian code

--- 14-4-2024/bmi.java
+++ 14-4-2024/bmi.java
@@ -4,28 +4,24 @@ class Lab2_ex4{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your gender (1 = male, 0 = female): ");
         int gender = sc.nextInt();
+        System.out.print("Enter your weight (kg) and height (m): ");
+        double weight_0 = sc.nextDouble();
+        double height_0 = sc.nextDouble();
+        double BMI = weight_0/(height_0*height_0);
         switch (gender){
             case 0:
-                System.out.print("Enter your weight (kg) and height (m): ");
-                double weight_0 = sc.nextDouble();
-                double height_0 = sc.nextDouble();
-                double BMI_0 = weight_0/(height_0*height_0);
-                if (BMI_0 <= 19){
+                if (BMI <= 19){
                     System.out.println("You're a little skinny");
-                } else if (BMI_0 <= 24){
+                } else if (BMI <= 24){
                     System.out.println("You're in good shape");
                 } else{
                     System.out.println("You're a little big");
                 }
                 break;
             case 1:
-                System.out.print("Enter your weight (kg) and height (m): ");
-                double weight_1 = sc.nextDouble();
-                double height_1 = sc.nextDouble();
-                double BMI_1 = weight_1/(height_1*height_1);
-                if (BMI_1 <= 20){
+                if (BMI <= 20){
                     System.out.println("You're a little skinny");
-                } else if (BMI_1 <= 25){
+                } else if (BMI <= 25){
                     System.out.println("You're in good shape");
                 } else{
                     System.out.println("You're a little big");
@@ -35,4 +31,4 @@ class Lab2_ex4{
                 System.out.println("Please enter appropriate gender!");
         }
     }
-}
+} //xoa lap lai
