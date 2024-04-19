7c0d5d7d89a50743d8000b4231ffb37bd69e4fa5
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-04-19 11:48:25 +0700

    Hoàn thành ex4 - lab2

--- 14-4-2024/bmi.java
+++ 14-4-2024/bmi.java
@@ -2,11 +2,37 @@ import java.util.Scanner;
 class Lab2_ex4{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your gender (m = male, f = female): ");
-        char gender = sc.next().charAt(0); 
-        switch (gender) {
-            case 'm':
-                
+        System.out.print("Enter your gender (1 = male, 0 = female): ");
+        int gender = sc.nextInt();
+        switch (gender){
+            case 0:
+                System.out.print("Enter your weight (kg) and height (m): ");
+                double weight_0 = sc.nextDouble();
+                double height_0 = sc.nextDouble();
+                double BMI_0 = weight_0/(height_0*height_0);
+                if (BMI_0 <= 19){
+                    System.out.println("You're a little skinny");
+                } else if (BMI_0 <= 24){
+                    System.out.println("You're in good shape");
+                } else{
+                    System.out.println("You're a little big");
+                }
+                break;
+            case 1:
+                System.out.print("Enter your weight (kg) and height (m): ");
+                double weight_1 = sc.nextDouble();
+                double height_1 = sc.nextDouble();
+                double BMI_1 = weight_1/(height_1*height_1);
+                if (BMI_1 <= 20){
+                    System.out.println("You're a little skinny");
+                } else if (BMI_1 <= 25){
+                    System.out.println("You're in good shape");
+                } else{
+                    System.out.println("You're a little big");
+                }
+                break;
+            default:
+                System.out.println("Please enter appropriate gender!");
         }
     }
 }
