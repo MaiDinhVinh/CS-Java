0a59ad65b3ce37f2b3aa8a871af1d82e67e2d549
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-04-19 23:07:14 +0700

    Xoa cac case co chu thuong, tu do se co 2 truong hop valid va in ra

--- 14-4-2024/week_case_sen.java
+++ 14-4-2024/week_case_sen.java
@@ -5,45 +5,31 @@ class Lab2_ex3{
         System.out.print("Enter a character: ");
         char inp = sc.next().charAt(0); //lay input ky tu tai vi tri index = 0 (same as python)
         switch (inp){
-            case 'm':
-                System.out.println("It's Monday");
-                break;
+            case 'm': 
             case 'M':
                 System.out.println("It's Monday");
                 break;
             case 't':
-                System.out.println("It's Tuesday");
-                break;
             case 'T':
                 System.out.println("It's Tuesday");
                 break;
             case 'w':
-                System.out.println("It's Wednesday");
-                break;
             case 'W':
                 System.out.println("It's Wednesday");
                 break;
             case 'h':
-                System.out.println("It's Thursday");
-                break;
             case 'H':
                 System.out.println("It's Thursday");
                 break;
             case 'f':
-                System.out.println("It's Friday");
-                break;
             case 'F':
                 System.out.println("It's Friday");
                 break;
             case 's':
-                System.out.println("It's Saturday");
-                break;
             case 'S':
                 System.out.println("It's Saturday");
                 break;
             case 'u':
-                System.out.println("It's Sunday");
-                break;
             case 'U':
                 System.out.println("It's Sunday");
                 break;
@@ -51,4 +37,4 @@ class Lab2_ex3{
                 System.out.println("Invalid input !");
         }
     }
-}
+} //xoa code lap lai
