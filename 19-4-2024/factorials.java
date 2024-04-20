5f4d3d7f5fcbf7efe7d5d2bcd61a1a8379fff02e
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-04-20 13:21:56 +0700

    Hoan thanh ex6 - lab3

---
+++ 19-4-2024/factorials.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Lab3_ex6{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+        int res = 1;
+        while (a < 0){
+            System.out.println("You should enter a positive number!");
+            System.out.print("Enter a number: ");
+            a = sc.nextInt();
+        }
+        for(int fact = 1; fact <= a; fact++){
+            res *= fact;
+        }
+        System.out.println(a + "! " + "= " + res);
+    }
+}
