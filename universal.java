5a7a2fcd8b2feeab0c7de5740a03b73ba069d8ad
MaiDinhVinh <vingit96@gmail.com>
2024-04-22 01:42:10 +0700

    re-verifing thing fỏ 3nd time

--- universal.java
+++ universal.java
@@ -1,6 +1,6 @@
 class test_only{
     public static void main(String[] args){
-        System.out.println("fucked up thing for 3rd time");
+        System.out.println("fucked up thing for 4th time");
     }
 }
 
