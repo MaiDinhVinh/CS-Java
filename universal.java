41e2011d962af0270b5177afa0eca11eb51c9186
MaiDinhVinh <vingit96@gmail.com>
2024-04-22 01:56:58 +0700

    final verification?

--- universal.java
+++ universal.java
@@ -1,6 +1,6 @@
 class test_only{
     public static void main(String[] args){
-        System.out.println("fucked up thing for 5th time");
+        System.out.println("fucked up thing for 6th time");
     }
 }
 
