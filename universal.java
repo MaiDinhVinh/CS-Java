76900590ae20ae5dd29780f499992c071332efdd
MaiDinhVinh <vingit96@gmail.com>
2024-04-26 01:37:53 +0700

    you should kys

--- universal.java
+++ universal.java
@@ -1,14 +1,7 @@
 import java.util.Scanner;
 class test_only{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("FINAL?: ");
-        int a = sc.nextInt();
-        if (a == 1){
-            System.out.println("YESSSS");
-        } else{
-            System.out.println("no lo diga");
-        }
+        System.out.println(45/10);
     }
 }
 
