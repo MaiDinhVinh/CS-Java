3263461ba485a4d9fe35818793bf07dba93ae581
MaiDinhVinh <vingit96@gmail.com>
2024-04-26 23:39:16 +0700

    Test for q08 - lab4

--- universal.java
+++ universal.java
@@ -1,31 +1,8 @@
 import java.util.Scanner;
 class test_only{
-    public static boolean containDigit7(int a){
-        boolean res = true;
-        while (a > 0){
-            res = a % 10 == 7;
-            if(res == true){
-                break;
-            } else{
-                a /= 10;
-            }
-        }
-        return res;
-    }
-    
-    public static int sumWithout7(int inp){
-        int sum = 0;
-        for (int i = 1; i <= inp; i++){
-            if(containDigit7(i) == true){
-                continue;
-            } else{
-                sum += i;
-            }
-        }
-        return sum;
-    }
     public static void main(String[] args){
-        System.out.println(sumWithout7(10));
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
     }
 }
 
