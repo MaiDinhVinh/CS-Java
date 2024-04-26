898e56cc8706381631bfba43a400489f2fb8e30f
MaiDinhVinh <vingit96@gmail.com>
2024-04-26 12:35:35 +0700

    testing ex7

--- universal.java
+++ universal.java
@@ -1,7 +1,31 @@
 import java.util.Scanner;
 class test_only{
+    public static boolean containDigit7(int a){
+        boolean res = true;
+        while (a > 0){
+            res = a % 10 == 7;
+            if(res == true){
+                break;
+            } else{
+                a /= 10;
+            }
+        }
+        return res;
+    }
+    
+    public static int sumWithout7(int inp){
+        int sum = 0;
+        for (int i = 1; i <= inp; i++){
+            if(containDigit7(i) == true){
+                continue;
+            } else{
+                sum += i;
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args){
-        System.out.println(45/10);
+        System.out.println(sumWithout7(10));
     }
 }
 
