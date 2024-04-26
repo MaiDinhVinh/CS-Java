d9486916383d9b8bb37da6348d0edf0a5a3213e3
MaiDinhVinh <vingit96@gmail.com>
2024-04-26 23:58:13 +0700

    change repo name

--- universal.java
+++ universal.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 class test_only{
     public static void main(String[] args){
+<<<<<<< HEAD
         System.out.print("\033[H\033[2J");  
         System.out.flush();  
+=======
+        System.out.println("Changed the directory and repo name lol !!!!!");
+>>>>>>> c68d5c8 (change repo name)
     }
 }
 
