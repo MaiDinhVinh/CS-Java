cb3796b40b9698adda2862310a07b528b91f4156
MaiDinhVinh <vingit96@gmail.com>
2024-04-26 23:27:21 +0700

    HOMEWORK VERIFIED - Tinh gian while -> do while

--- 19-4-2024/q1.java
+++ 19-4-2024/q1.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 class Lab3_ex01{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int a = sc.nextInt();
-        while (a >= 0){
+        int a;
+        // System.out.print("Enter a number: ");
+        // // int a = sc.nextInt();
+        // // while (a >= 0){
+        // //     System.out.print("Enter a number: ");
+        // //     a = sc.nextInt();
+        do {
             System.out.print("Enter a number: ");
             a = sc.nextInt();
-        }
+        } while (a >= 0);
     } 
 }
