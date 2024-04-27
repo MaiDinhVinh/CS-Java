ecb4316a559de3999432c63a9c677f0f53506631
MaiDinhVinh <vingit96@gmail.com>
2024-04-28 00:24:04 +0700

    i think i should kms

--- universal.java
+++ universal.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 class test_only{
     public static void main(String[] args){
-<<<<<<< HEAD
         System.out.print("\033[H\033[2J");  
         System.out.flush();  
-=======
-        System.out.println("Changed the directory and repo name lol !!!!!");
->>>>>>> c68d5c8 (change repo name)
     }
-}
+} //i think i should kms"
 
