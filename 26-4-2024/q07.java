164ad50a1f092f1481c4cbb8a666e8efb764962e
MaiDinhVinh <vingit96@gmail.com>
2024-04-28 23:26:27 +0700

    HOMEWORK VERIFIED - Function isSubSet sai, sua lai

--- 26-4-2024/q07.java
+++ 26-4-2024/q07.java
@@ -1,13 +1,18 @@
 class lab5_ex7{
     public static boolean isPartOf(int x, int[] arr){
-        boolean res = false;
+        // boolean res = false;
         for (int i = 0; i < arr.length; i++){
-            res = x == arr[i];
-            if (res == true){
-                break;
+            // res = x == arr[i];
+            // if (res == true){
+            //     break;
+            // }
+            if (x == arr[i]){
+                return true;
             }
+
         }
-        return res;
+        // return res;
+        return false;
     }
     
     public static boolean isSubSet(int[] arrA, int[] arrB){
@@ -18,7 +23,7 @@ class lab5_ex7{
         return ketqua;
     }
     public static void main(String[] args){
-        int[] arrA = {37, 13, 37, 10, 9, 11};
+        int[] arrA = {11, 37, 13, 37, 10, 9};
         int[] arrB = {37, 0, 13, 37, 3, 10, 9};
         if (isSubSet(arrA, arrB) == true){
             System.out.println("arrA is subset of arrB");
@@ -26,4 +31,4 @@ class lab5_ex7{
             System.out.println("arrA is NOT subset of arrB");
         }
     }
-}
+} //sai phan isSubSet
