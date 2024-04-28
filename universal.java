2fa24fba41c3dc4b5cb3cda28a2719431087fdda
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-04-29 00:37:46 +0700

    I want to kms again because of wsl hot shit

--- universal.java
+++ universal.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 class test_only{
     public static void main(String[] args){
-        System.out.print("\033[H\033[2J");  
-        System.out.flush();  
-    }
-} //i think i should kms"
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value: ");
+        x = sc.nextDouble();
 
+        System.out.print("Enter value: ");
+        y = sc.nextDouble();
+
+        int s = x + y;
+        System.out.println(s);
+    }
+} //this is wrong shit dont care, but still want to kms
