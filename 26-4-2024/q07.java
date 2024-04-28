25d0af8e1571a7b35f3fa13f76d221bab60f6740
MaiDinhVinh <vingit96@gmail.com>
2024-04-28 17:02:49 +0700

    Hoan thanh ex6 - lab5
    Co-authored-by: DinhVinhMai official.vinhdev@icloud.com

---
+++ 26-4-2024/q07.java
@@ -0,0 +1,29 @@
+class lab5_ex7{
+    public static boolean isPartOf(int x, int[] arr){
+        boolean res = false;
+        for (int i = 0; i < arr.length; i++){
+            res = x == arr[i];
+            if (res == true){
+                break;
+            }
+        }
+        return res;
+    }
+    
+    public static boolean isSubSet(int[] arrA, int[] arrB){
+        boolean ketqua = false;
+        for (int a = 0; a < arrA.length; a++){
+            ketqua = isPartOf(arrA[a], arrB);
+        }
+        return ketqua;
+    }
+    public static void main(String[] args){
+        int[] arrA = {37, 13, 37, 10, 9, 11};
+        int[] arrB = {37, 0, 13, 37, 3, 10, 9};
+        if (isSubSet(arrA, arrB) == true){
+            System.out.println("arrA is subset of arrB");
+        } else{
+            System.out.println("arrA is NOT subset of arrB");
+        }
+    }
+}
