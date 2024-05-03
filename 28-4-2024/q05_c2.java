cd7c13c2590d6e3b5c0116d97046d1e4200e0525
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-05-03 19:28:19 +0700

    Hoan thanh ex5 - lab6

---
+++ 28-4-2024/q05_c2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class lab6_ex5b{
+    public static boolean isPalindrome(String inp){
+        char[] before = new char[inp.length()];
+        char[] after = new char[inp.length()];
+        for (int i = 0; i < inp.length(); i++){
+            before[i] = inp.charAt(i);
+        }
+        //lam nguoc lai tiep 
+        int z = 0;
+        for (int i = inp.length() - 1; i >= 0; i--){
+            after[z] = before[i];
+            z++;
+        }
+        //check 2 array tiep 
+        boolean res = false;
+        for (int i = 0; i < inp.length(); i++){
+            if (after[i] == before[i]){
+                res = true;
+            } else if(after[i] - before[i] == 32 || after[i] - before[i] == -32){
+                res = true;
+            } else{
+                break;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter word: ");
+        String s = sc.nextLine();
+        if (isPalindrome(s) == true){
+            System.out.println("\" " + s + " \"" + " is a palindrome");
+        } else{
+            System.out.println("\" " + s + " \"" + " is not a palindrome");
+        }
+    }
+}
