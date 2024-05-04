277fe56f6b5276769a32ade15f7cbc5ab9be6dec
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-05-04 21:07:40 +0700

    Da sua lai ex3 - lab5

--- 26-4-2024/q03.java
+++ 26-4-2024/q03.java
@@ -1,19 +1,22 @@
 class Lab5_ex3{
     public static int greatestSum(int[] arr){
         int first = arr[0];
-        int second = arr[1];
-        for (int i = 2; i < arr.length; i++){
-            if (arr[i] >= first){
+        int second = 0;
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] > first){
                 second = first;
                 first = arr[i];
+            } else if (arr[i] == first){
+                continue;
+            } else if(arr[i] > second){
+                second = arr[i];
             }
-        } 
-        int sum = 0;
-        return sum = first + second;
+        }
+        return first + second;
     }
     
     public static void main(String[] args){
-        int[] arr = {37, 13, 36, 10, 9};
+        int[] arr = {9, 0, 7, 3, 8};
         System.out.println("the greatest sum is " + greatestSum(arr));
     }
-} //sai
+} 
