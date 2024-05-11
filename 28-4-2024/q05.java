3c08c8c53bde1b92a6cf60b6fe61f1312d120b5b
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-05-12 02:40:33 +0700

    Da tu tinh gian lai ex5 - lab6 theo bai chua

--- 28-4-2024/q05.java
+++ 28-4-2024/q05.java
@@ -1,34 +1,12 @@
-//kayak hannah
-//hannah length = 6
-//0-5, 1-4, 2-3
 import java.util.Scanner;
 class lab6_ex5a{
     public static boolean isPalindrome(String inp){
-        //check do dai string 
-        int check = inp.length();
-        String middle1 = "";
-        String middle2 = "";
-        if(check % 2 == 1){
-            check = (inp.length() - 1)/2;
-            middle1 = inp.substring(0, check);
-            middle2 = inp.substring(check+1);
-        } else if(check % 2 == 0){
-            check = inp.length() / 2;
-            middle1 = inp.substring(0, check);
-            middle2 = inp.substring(check);
+        for(int i = 0; i < inp.length(); i++){
+            if(!(inp.charAt(i) == inp.charAt(inp.length() - 1 - i))){
+                return false;
+            }
         }
-        //lat nguoc middle2
-        char[] arr = new char[middle2.length()];
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = middle2.charAt(middle2.length() - 1 - i);
-        }
-        String fin_middle2 = "";
-        fin_middle2 = fin_middle2.copyValueOf(arr, 0, arr.length);
-        if(middle1.compareTo(fin_middle2) == 0){
-            return true;
-        } else{
-            return false;
-        }
+        return true;
     }
     
     public static void main(String[] args){
