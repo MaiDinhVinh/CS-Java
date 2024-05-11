736bf1546ec0305610f867130b2506ebe978a05c
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-05-12 02:42:59 +0700

    Da tu chua lai ex6 - lab6 theo bai chua

--- 28-4-2024/q06.java
+++ 28-4-2024/q06.java
@@ -1,50 +1,27 @@
 import java.util.Scanner;
 class lab6_ex6{
     public static String rearrange(String inp){
-        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        String conso = "";
+        String vowels = "aeiou";
         String vow = "";
-        String res = "";
-        int status = 0;
-        for (int i = 0; i < vowels.length; i++){
-            if(inp.charAt(0) == vowels[i]){
-                status++;
-                break;
+        String conso = "";
+        boolean containVow = false;
+        if(vowels.contains(inp.substring(0, 1))){
+            containVow = true;
+        }
+
+        for(int i = 0; i < inp.length(); i++){
+            if(vowels.contains(inp.substring(i, i+1))){
+                vow += inp.charAt(i);
+            } else{
+                conso += inp.charAt(i);
             }
         }
-        boolean check = false;
-        for(int i = 0; i <= inp.length() - 1; i++){
-            for(int z = 0; z < vowels.length; z++){
-                if(inp.charAt(i) == vowels[z]){
-                    if(i == inp.length() - 1){
-                        vow += inp.substring(i);
-                        check = true;
-                        break;
-                    } else {
-                        vow += inp.substring(i, i+1);
-                        check = true;
-                        break;
-                    }
-                }
-            }
-            if(!check){
-                if(i == inp.length() -1){
-                    conso += inp.substring(i);
-                } else{
-                    conso += inp.substring(i, i+1);
-                }
-            }
-            check = false;
+
+        if(containVow){
+           return vow + conso; 
+        } else{
+           return conso + vow;
         }
-        switch (status){
-            case 0:
-                res = conso + vow;
-                break;
-            case 1:
-                res = vow + conso;
-                break;
-        }
-        return res;
     }
 
     public static void main(String[] args){
