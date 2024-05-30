8ed339193af9176eadeb76835c7fe0231726919f
VinhDev <108355711+MaiDinhVinh@users.noreply.github.com>
2024-05-30 17:06:17 +0700

    First ver deployed !

--- 10-5-2024/CaesarEncryption_v2.0.java
+++ 10-5-2024/CaesarEncryption_v2.0.java
@@ -11,27 +11,23 @@ class ad_review1{
         String res = "";
         char encryption = ' ';
         int loop = 0;
+        int shift = 0;
         if(ert > 0){
             loop = 1;
+            shift = 1;
         } else if(ert < 0){
             loop = -1;
+            shift = -1;
         }
         for(int i = 0; i < inp.length(); i++){
             encryption = inp.charAt(i);
             for(int j = 0; j != ert; j+=loop){
-                if(ert > 0){
-                    if(encryption == 'Z' || encryption == 'z'){
-                        encryption -= 25;
-                    } else{
-                        encryption += 1;
-                    }
-                }
-                if(ert < 0){
-                    if(encryption == 'A' || encryption == 'a'){
-                        encryption += 25;
-                    } else{
-                        encryption -= 1;
-                    }
+                if((ert > 0) && (encryption == 'z' || encryption == 'Z')){
+                    encryption -= 25;
+                } else if((ert < 0) && (encryption == 'a' || encryption == 'A')){
+                    encryption += 25;
+                } else{
+                    encryption += shift;
                 }
             }
             res += encryption;
