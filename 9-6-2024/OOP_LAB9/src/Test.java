b65c46d91b43982ba8aaa8bca934a8d564f1b4bd
MaiDinhVinh <vingit96@gmail.com>
2024-06-11 02:45:11 +0700

    Upload all proj file

---
+++ 9-6-2024/OOP_LAB9/src/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vingi
+ */
+public class Test {
+    public static void main(String[] args){
+        Shape s1 = new Triangle(10, 30, "Nigga");
+        System.out.println(s1);
+        System.out.println("s1 area=" + s1.getArea());
+    }
+}
