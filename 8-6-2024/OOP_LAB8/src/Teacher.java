d1633b6975bf392847d63316280330b074da463f
MaiDinhVinh <vingit96@gmail.com>
2024-06-11 23:56:45 +0700

    Ex2 - lab8 solved

---
+++ 8-6-2024/OOP_LAB8/src/Teacher.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vingi
+ */
+public class Teacher extends Person{
+    
+    //attribute - instance variables
+    private int numCourses = 0;
+    private String[] courses = {};
+    
+    //constructor with args
+    public Teacher(String name, String address){
+        super(name, address);
+    }
+    
+    public boolean addCourse(String course){
+        boolean check = false;
+        for(String i: courses){
+            if(course == i){
+                check = true;
+                break;
+            } 
+        }
+        return check;
+    }
+    
+    public boolean removeCourse(String course){
+        boolean check = false;
+        for(String i: courses){
+            if(course == i){
+                check = true;
+                break;
+            }
+        }
+        return check;
+    }
+    
+    @Override
+    public String toString(){
+        return "Teacher: " + this.getName() + "(" + this.getAddress() +
+                ")";
+    }
+    
+    
+}
