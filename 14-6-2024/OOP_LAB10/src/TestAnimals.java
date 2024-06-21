e82d7e296a3382b330087ed8da303e8a46c76876
MaiDinhVinh <vingit96@gmail.com>
2024-06-21 20:03:57 +0700

    Upload test driver for ex3 - lab10

---
+++ 14-6-2024/OOP_LAB10/src/TestAnimals.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vingi
+ */
+public class TestAnimals {
+    public static void main(String[] args){
+        Fish f1 = new Fish();
+        Animal f2 = new Fish("nemo");
+        Spider s1 = new Spider();
+        Cat c1 = new Cat();
+        Animal c2 = new Cat("talking tom");
+        
+        
+    }
+}
