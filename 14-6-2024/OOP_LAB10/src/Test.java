bf7cf3d3c956b072846835a6619b814d944bf485
MaiDinhVinh <vingit96@gmail.com>
2024-06-21 07:27:26 +0700

    Lab10 - ex1 completed

---
+++ 14-6-2024/OOP_LAB10/src/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vingi
+ */
+public class Test {
+    public static void main(String[] args){
+        Triangle r1 = new Triangle(2, 10, "green");
+        
+        System.out.println(r1.getArea());
+        System.out.println(r1);
+    }
+}
