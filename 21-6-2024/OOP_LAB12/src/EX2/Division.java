2ab8f19357341b26849d0e58239a0a2c48c6b423
MaiDinhVinh <vingit96@gmail.com>
2024-06-23 22:08:11 +0700

    Class verified - correct result

---
+++ 21-6-2024/OOP_LAB12/src/EX2/Division.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EX2;
+
+/**
+ *
+ * @author vingi
+ */
+public class Division extends MathExpression{
+    public Division(String exp){
+        super(exp);
+    }
+    
+    @Override
+    public int calculate(){
+        return super.getLHS() / super.getRHS(); 
+    }
+}
