05f4f5ac112da59a3be1021b9331588cc4b1791c
MaiDinhVinh <vingit96@gmail.com>
2024-06-23 22:12:53 +0700

    Class method changed from return 0 to return MAX_INTEGER

---
+++ 21-6-2024/OOP_LAB12/src/EX2/NonValidExpression.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EX2;
+
+/**
+ *
+ * @author vingi
+ */
+public class NonValidExpression extends MathExpression{
+    public NonValidExpression(String exp){
+        super(exp);
+    }
+    
+    @Override
+    public int calculate(){
+//        System.out.print("(Invalid Operator)");
+//        return 0; 
+        return Integer.MAX_VALUE;
+    }
+}
