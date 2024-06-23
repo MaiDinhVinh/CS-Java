0829667eedc20aca6e028a137ee93637f5462a69
MaiDinhVinh <vingit96@gmail.com>
2024-06-23 22:10:26 +0700

    Class verified - correct result

---
+++ 21-6-2024/OOP_LAB12/src/EX2/MathExpression.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EX2;
+
+/**
+ *
+ * @author vingi
+ */
+public abstract class MathExpression {
+    private int left;
+    private int right;
+    private String operator;
+    
+    public MathExpression(){
+        this.left = 0;
+        this.right = 0;
+        this.operator = null;
+    }
+    
+    public MathExpression(String exp){
+        this.left = Integer.parseInt(exp.substring(0, (exp.length() / 2) - 1));
+        this.right = Integer.parseInt(exp.substring((exp.length() / 2 ) + 2));
+        this.operator = exp.substring((exp.length() / 2), (exp.length() / 2) + 1);
+    }
+    
+    public int getLHS(){ //mean getLeftHandSide
+        return this.left;
+    }
+    
+    public int getRHS(){ //mean getRightHandSide
+        return this.right;
+    }
+    
+    public String getOperator(){ //what is the purpose of those getters and setters ?
+        return this.operator;
+    }
+    
+    public void setLHS(int left){
+        this.left = left;
+    }
+    
+    public void setRHS(int right){
+        this.right = right;
+    }
+    
+    public void setOperator(String operator){
+        this.operator = operator;
+    }
+    
+    public abstract int calculate();
+    
+    @Override
+    public String toString(){
+        return this.left + this.operator + this.right + "=" +
+                this.calculate();
+    }
+    
+}
