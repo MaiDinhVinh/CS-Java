0941cd3e2d677ae54ed90f333158c23b41c7e0f6
MaiDinhVinh <vingit96@gmail.com>
2024-06-27 07:17:26 +0700

    HW not finished

--- 21-6-2024/OOP_LAB12/src/EX2/MathExpression.java
+++ 21-6-2024/OOP_LAB12/src/EX2/MathExpression.java
@@ -13,6 +13,10 @@ public abstract class MathExpression {
     private int left;
     private int right;
     private String operator;
+    private String invalid_left;
+    private String invalid_right;
+    private boolean is_invalid;
+    
     
     public MathExpression(){
         this.left = 0;
@@ -24,6 +28,13 @@ public abstract class MathExpression {
         this.left = Integer.parseInt(exp.substring(0, (exp.length() / 2) - 1));
         this.right = Integer.parseInt(exp.substring((exp.length() / 2 ) + 2));
         this.operator = exp.substring((exp.length() / 2), (exp.length() / 2) + 1);
+    }
+    
+    public MathExpression(String exp, boolean check){
+        this.invalid_left = exp.substring(0, (exp.length() / 2) - 1);
+        this.invalid_right = exp.substring((exp.length() / 2 ) + 2);
+        this.operator = exp.substring((exp.length() / 2), (exp.length() / 2) + 1);
+        this.is_invalid = true;
     }
     
     public int getLHS(){ //mean getLeftHandSide
@@ -54,8 +65,13 @@ public abstract class MathExpression {
     
     @Override
     public String toString(){
-        return this.left + this.operator + this.right + "=" +
+        if(!is_invalid){
+            return this.left + this.operator + this.right + "=" +
                 this.calculate();
+        } else{
+            return this.invalid_left + this.operator + this.invalid_right + 
+                    "=" + this.calculate();
+        }
     }
     
 }
