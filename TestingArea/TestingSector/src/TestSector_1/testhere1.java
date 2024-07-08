7497dfeb5c025cd117ccf03c8fcd267617563d0c
MaiDinhVinh <vingit96@gmail.com>
2024-07-08 23:44:00 +0700

    test driver 1

--- TestingArea/TestingSector/src/TestSector_1/testhere1.java
+++ TestingArea/TestingSector/src/TestSector_1/testhere1.java
@@ -1,2 +1,8 @@
-package TestSector_1;public class testhere1 {
+package TestSector_1;
+
+public class testhere1 {
+    public static void main(String[] args) {
+        CalculatorExecutor1 c = new CalculatorExecutor1();
+        System.out.println(c.Calculate(1, 2));
+    }
 }
