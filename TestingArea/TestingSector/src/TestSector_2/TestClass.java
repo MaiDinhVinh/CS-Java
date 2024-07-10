e31b0a02276b9ec7b9dbfab27f0fe5ee1a60c6ee
MaiDinhVinh <vingit96@gmail.com>
2024-07-10 16:37:07 +0700

    Upload test file 1

--- TestingArea/TestingSector/src/TestSector_2/TestClass.java
+++ TestingArea/TestingSector/src/TestSector_2/TestClass.java
@@ -1,2 +1,7 @@
-package TestSector_2;public class TestClass {
+package TestSector_2;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println(0/0);
+    }
 }
