78a5062ce2fd14692ea04f7b26f6c3bcc4d50cea
MaiDinhVinh <vingit96@gmail.com>
2024-07-10 16:37:48 +0700

    Upload test file 2

--- TestingArea/TestingSector/src/TestSector_3/testshit1.java
+++ TestingArea/TestingSector/src/TestSector_3/testshit1.java
@@ -1,2 +1,7 @@
-package TestSector_3;public class testshit1 {
+package TestSector_3;
+
+public class testshit1 {
+    public static void main(String[] args) {
+        System.out.println("Hello" + "" + "dmm");
+    }
 }
