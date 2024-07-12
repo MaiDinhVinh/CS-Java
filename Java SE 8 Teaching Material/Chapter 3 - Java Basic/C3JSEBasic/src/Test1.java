a60dde906fa32494e1356104ea517cee5edd1418
MaiDinhVinh <vingit96@gmail.com>
2024-07-12 11:55:23 +0700

    upload example chapter 3

---
+++ Java SE 8 Teaching Material/Chapter 3 - Java Basic/C3JSEBasic/src/Test1.java
@@ -0,0 +1,19 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+/*
+Java Program executing procedure:
+1 - Go to Terminal or CMD, type "javac <file name>.java
+2 - Next, type "java <file name>"
+3 - Enjoy
+
+Java Program execution explain:
+1 - This step will create a ".class" file, which is the compiled Java Program
+containing Java Bytecode for JVM
+2 - This step is injecting the ".class" file to JVM, so JVM will further interprete
+it into processor => executed
+ */
+
