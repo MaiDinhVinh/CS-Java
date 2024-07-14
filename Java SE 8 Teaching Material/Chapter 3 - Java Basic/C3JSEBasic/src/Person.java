5f576af16e3d3be62986e27bd92920773d2ddba7
MaiDinhVinh <vingit96@gmail.com>
2024-07-14 23:02:27 +0700

    Upload class example thing

---
+++ Java SE 8 Teaching Material/Chapter 3 - Java Basic/C3JSEBasic/src/Person.java
@@ -0,0 +1,56 @@
+import java.time.LocalDate;
+
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int birthYear;
+    private String country;
+
+    public Person(String firstName, String lastName, String gender, int birthYear, String country){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthYear = birthYear;
+        this.country = country;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public String getGender(){
+        return this.gender;
+    }
+
+    public int getBirthYear(){
+        return this.birthYear;
+    }
+
+    public String getCountry(){
+        return this.country;
+    }
+
+    //own method
+    public int getAge(){
+        int currentYear = LocalDate.now().getYear();
+        return currentYear - this.birthYear;
+    }
+
+    @Override
+    public String toString(){
+        return "Person[" + "name= " + this.firstName + " " + this.lastName + ", " + "gender= " + this.gender +
+                ", " + "birthYear= " + this.birthYear + ", " + "age= " + this.getAge() + ", " + "country= " +
+                this.country + "]";
+    }
+}
+
+
+
+
+
