cf1f0c423f451a09a97926ac24259f491a611d32
MaiDinhVinh <vingit96@gmail.com>
2024-07-14 22:59:20 +0700

    Example chap3 1

---
+++ Java SE 8 Teaching Material/Chapter 3 - Java Basic/C3JSEBasic/src/DifferenceRefVSPrim.java
@@ -0,0 +1,9 @@
+public class DifferenceRefVSPrim {
+    public static void main(String[] args) {
+        int a = 2222;
+        String str = "hello";
+
+        //String test = 0; Error since "test" is a reference variable while 0 is primitive
+        //int num = null; Error since int is a primitive data type and it never points to any obj
+    }
+}
