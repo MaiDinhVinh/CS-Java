3e3ee60e599ffbadba0193615ef00cad255f93e1
MaiDinhVinh <vingit96@gmail.com>
2024-07-14 23:08:06 +0700

    Upload all changes

--- Java SE 8 Teaching Material/Chapter 3 - Java Basic/C3JSEBasic/src/Test2.java
+++
@@ -1,8 +0,0 @@
-public class Test2 {
-    public static void main(String[] args) {
-        System.out.println(args.length);
-        for(int i = 0; i < args.length; i++){
-            System.out.println("arg[" + i + "] = \"" + args[i] + "\"");
-        }
-    }
-}
