a72e65c50f98d40062ff8f03a5396bf4ba1d0f5e
MaiDinhVinh <vingit96@gmail.com>
2024-07-14 23:06:17 +0700

    example changes again

--- Java SE 8 Teaching Material/Chapter 3 - Java Basic/C3JSEBasic/src/MainTest1.java
+++ Java SE 8 Teaching Material/Chapter 3 - Java Basic/C3JSEBasic/src/MainTest1.java
@@ -2,21 +2,19 @@ public class MainTest1 {
     //most common "main" method
     //public static void main(String[] args) {}
 
-    //String... = String[] (JVM understand this), "..." is called "vargargs"
-    //public static void main(String... args){}
 
     //The main method is allowed to throw any exception
     //public static void main(String args[]) throws Exception{ throw new Exception(); }
 
     //INCORRECT: this main method doesn't has public modifier
-    //static void main(String abc[]){}
+//    static void main(String abc[]){}
 
     //INCORRECT: this main method doesn't has static keyword
-    //public void main(String[] x){}
+//    public void main(String[] x){}
 
     //INCORRECT: this main method has 2 arguments
-    //public static void main(String[] a, String b){}
+//    public static void main(String[] a, String b){}
 
     //INCORRECT: this main method doesn't has public modifier and the method name is "Main" not "main
-    //static void Main(String[] args){}
+//    static void Main(String[] args){}
 }
