6f5e036aaf7bd59461005111d8adb90e51c2c0e6
MaiDinhVinh <vingit96@gmail.com>
2024-07-16 12:43:27 +0700

    Upload test example

--- TestingArea/TestingSector/src/TestSector_3/testshit1.java
+++ TestingArea/TestingSector/src/TestSector_3/testshit1.java
@@ -2,6 +2,7 @@ package TestSector_3;
 
 public class testshit1 {
     public static void main(String[] args) {
-        System.out.println("Hello" + "" + "dmm");
+        long dmm = 1234;
+        System.out.println(dmm);
     }
 }
