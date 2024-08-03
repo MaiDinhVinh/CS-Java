d24b02ccb19b94c0e6fd840f7e31ec2ce3d415e7
MaiDinhVinh <vingit96@gmail.com>
2024-08-03 12:32:03 +0700

    upload proj file

---
+++ day em anh duc java basic/JavaSEBasic_anhducHCM/src/test1/helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+    public static void main(String[] args) {
+        int a = 0;
+        float b = 1.1f;
+        double c = 0.2;
+        char d = 'a'; //primitve data type
+        String z = "abcd";
+    }
+
+    
+
+}
