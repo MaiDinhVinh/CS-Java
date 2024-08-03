755fe7707b9b96b9be23f9162972515752df998a
MaiDinhVinh <vingit96@gmail.com>
2024-08-03 12:40:37 +0700

    upload teach 1

--- day em anh duc java basic/JavaSEBasic_anhducHCM/src/test1/helloworld.java
+++ day em anh duc java basic/JavaSEBasic_anhducHCM/src/test1/helloworld.java
@@ -1,12 +1,20 @@
-public class helloworld {
-    public static void main(String[] args) {
-        int a = 0;
-        float b = 1.1f;
-        double c = 0.2;
-        char d = 'a'; //primitve data type
-        String z = "abcd";
+package test1;
+
+class Xemay{
+    private int speed; //instance variable
+    private String brand;
+
+    public Xemay(){
+        this.speed = 0;
+        this.brand = "honda";
     }
-
-    
-
+    //accessor or getter
+    public int getSpeed(){
+        return this.speed;
+    }
+    //mutator or setter
+    public void changeSpeed(int Speed){
+        this.speed = Speed;
+    }
 }
+
