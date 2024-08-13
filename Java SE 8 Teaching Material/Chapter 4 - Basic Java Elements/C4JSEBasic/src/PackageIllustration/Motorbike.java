40b84169f56fad71b4ca1641c15114beb5edf2be
MaiDinhVinh <vingit96@gmail.com>
2024-08-13 23:36:47 +0700

    Upload class example

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/PackageIllustration/Motorbike.java
@@ -0,0 +1,33 @@
+package PackageIllustration;
+
+public class Motorbike {
+    private String brand;
+    private int speed;
+    private int id;
+
+
+    public Motorbike(String brand){
+        this.brand = brand;
+        this.speed = 0;
+        this.id = 1234;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public int getSpeed(){
+        return this.speed;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+
+    @Override
+    public String toString(){
+        return "brand: " + this.brand + ", " +
+                "ID: " + this.id + ", " +
+                "current speed: " + this.speed;
+    }
+}
