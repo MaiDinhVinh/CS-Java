5873034b5a065b02fd0e82b5033458500616be3c
MaiDinhVinh <vingit96@gmail.com>
2024-08-13 23:40:36 +0700

    Unverified knowledge

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/com/fpt/TestNonEnforcedPackageRule.java
@@ -0,0 +1,8 @@
+package com.bvis;
+
+public class TestNonEnforcedPackageRule {
+    public static void main(String[] args) {
+        System.out.println("I am a file in default package");
+    }
+}
+
