9206ac829139db4b501fafa07880dac5dec24d37
MaiDinhVinh <vingit96@gmail.com>
2024-08-13 23:40:10 +0700

    Static and Instance initializer example

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/com/fpt/Student.java
@@ -0,0 +1,32 @@
+package com.fpt;
+import java.util.Date;
+public class Student {
+    private static int count = 0; //static field
+    private String studentID; //instance field
+
+    static{ //static initializer
+        System.out.println("com.fpt.Student class loaded");
+    }
+
+    //instance initializer
+    {
+        Student.count++;
+        System.out.println("com.fpt.Student count incremented");
+    }
+
+    public Student(String id){
+        this.studentID = id;
+        System.out.println(new Date() + " com.fpt.Student instance created. Total students created = " + count);
+    }
+
+    @Override
+    public String toString(){
+        return "com.fpt.Student[studentID: " + this.studentID + "]";
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student("A1234");
+        System.out.println(s);
+    }
+
+}
