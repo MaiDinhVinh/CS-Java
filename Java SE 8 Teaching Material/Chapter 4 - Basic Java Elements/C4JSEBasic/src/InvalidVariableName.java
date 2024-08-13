5b51ad0b604e5d3fc1f68e3d309bc871453cb437
MaiDinhVinh <vingit96@gmail.com>
2024-08-13 23:34:35 +0700

    Upload invalid vairable declaration example

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/InvalidVariableName.java
@@ -0,0 +1,13 @@
+public class InvalidVariableName {
+    public static void main(String[] args) {
+        /*
+
+        int int; //this integer variable declaration is invalid since "int" is keyword
+        String class; //this String variable declarion is invalid since "class" is keyword
+        Account 1a; //this object reference declartion is invalid since the first characters is number not letter
+        byte true; //this byte variable declartion is invalid since "true" is a literal
+        String _; //this String variable declartion is valid in Java 8 only but not Java 9
+
+        */
+    }
+}
