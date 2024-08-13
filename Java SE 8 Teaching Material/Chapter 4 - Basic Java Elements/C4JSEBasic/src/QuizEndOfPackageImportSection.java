8ac9136201be2af60e0d0e3fa6bd2909af9bcb78
MaiDinhVinh <vingit96@gmail.com>
2024-08-13 23:35:10 +0700

    Upload quiz for the end of chapter

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/QuizEndOfPackageImportSection.java
@@ -0,0 +1,59 @@
+/*
+* A file named Student.java, which of these codes
+* below will compile without any error ?*/
+
+//CODE SNIPPET 1
+
+//Start of the file
+public class Student{
+    public static void main(String[] args) {
+    }
+}
+public class Grade{
+}
+//end of the file
+
+//CODE SNIPPET 2
+
+//start of the file
+class Student{
+    public static void main(String[] args) {
+    }
+}
+
+class Grade{
+}
+
+class Score{
+}
+//end of the file
+
+
+//CODE SNIPPET 3
+
+//start of the file
+public class Gradeable{
+}
+
+public class Person{
+}
+//end of the file
+
+//CODE SNIPPET 4
+
+//start of the file
+class Student{ //doan nay de bi bay
+    public static void main(String[] args) {
+    }
+}
+
+public class Professor{
+}
+//end of the file
+
+//CODE SNIPPET 5
+
+//start of the file
+package com.fpt.education;
+//end of the file
+
