a58bb8bbe57b57d014f00e8ad8553b016de55d27
MaiDinhVinh <vingit96@gmail.com>
2024-08-13 23:28:15 +0700

    Upload change in test file

--- TestingArea/TestingSector/src/TestSector_3/testshit1.java
+++ TestingArea/TestingSector/src/TestSector_3/testshit1.java
@@ -2,14 +2,6 @@ package TestSector_3;
 
 public class testshit1 {
 
-    public static boolean dmm(){
-        return true;
-    }
-
     public static void main(String[] args) {
-        boolean ditconmemay = true;
-        if(dmm() && ditconmemay){
-            System.out.println("lon");
-        }
     }
 }
