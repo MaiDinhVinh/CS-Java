67998e0139b607539f2ea66ab7a685572f6f72ed
MaiDinhVinh <vingit96@gmail.com>
2024-08-13 23:37:11 +0700

    Upload test driver for test class

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/PackageIllustration/TestMotorbike.java
@@ -0,0 +1,8 @@
+package PackageIllustration;
+
+public class TestMotorbike {
+    public static void main(String[] args) {
+        Motorbike demo = new Motorbike("honda");
+        System.out.println(demo);
+    }
+}
