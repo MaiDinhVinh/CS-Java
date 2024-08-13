df50016e7f3b2ac87a5be627946dbf281bf7dc2f
MaiDinhVinh <vingit96@gmail.com>
2024-08-13 23:33:17 +0700

    Upload invalid statement example

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/InvalidPackageStatement.java
@@ -0,0 +1,28 @@
+//code snippet 1
+package;
+class Test{
+
+}
+
+//code snippet 2
+package mypackage;
+class Test{
+
+}
+
+//code snippet 3
+package x;
+class Test{
+
+}
+package y;
+class Test2{
+
+}
+
+//code snippet 4
+package x;
+package y;
+class Test{
+
+}
