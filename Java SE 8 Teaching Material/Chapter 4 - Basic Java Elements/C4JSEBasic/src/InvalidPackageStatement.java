7d0ecc443c901185f9c4cd44f5ed0136fb6a111c
MaiDinhVinh <vingit96@gmail.com>
2024-08-15 00:40:19 +0700

    update result for quiz section

--- Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/InvalidPackageStatement.java
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/InvalidPackageStatement.java
@@ -1,18 +1,18 @@
 //THIS IS FOR SECTION 4.3 (THIS IS FOR THE END OF 4.3.1)
 
-//code snippet 1
+//code snippet 1 (INCORRECT)
 package;
 class Test{
 
 }
 
-//code snippet 2
+//code snippet 2 (CORRECT)
 package mypackage;
 class Test{
 
 }
 
-//code snippet 3
+//code snippet 3 (INCORRECT)
 package x;
 class Test{
 
@@ -22,7 +22,7 @@ class Test2{
 
 }
 
-//code snippet 4
+//code snippet 4 (INCORRECT)
 package x;
 package y;
 class Test{
