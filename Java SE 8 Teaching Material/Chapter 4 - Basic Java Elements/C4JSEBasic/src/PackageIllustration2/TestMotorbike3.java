edd3ac4582ee094d76152090752e1f72cb299ec7
MaiDinhVinh <vingit96@gmail.com>
2024-08-15 00:32:15 +0700

    upload new example

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic/src/PackageIllustration2/TestMotorbike3.java
@@ -0,0 +1,8 @@
+package PackageIllustration2;
+
+public class TestMotorbike3 {
+    public static void main(String[] args) {
+        PackageIllustration.Motorbike demo1 = new PackageIllustration.Motorbike("honda");
+        System.out.println();
+    }
+}
