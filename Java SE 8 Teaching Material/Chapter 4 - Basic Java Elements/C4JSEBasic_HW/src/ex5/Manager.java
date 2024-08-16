c970c4905f79db7dfab61ee6f14a53209ddaf418
MaiDinhVinh <vingit96@gmail.com>
2024-08-17 01:48:22 +0700

    upload ex5 homework

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic_HW/src/ex5/Manager.java
@@ -0,0 +1,23 @@
+package ex5;
+
+
+/**
+ * THIS IS THE CODE FROM THE TEXTBOOK
+ * <blockquote><pre>
+ *     class manager{ }
+ *          class fullTimeEmployee{ }
+ *          void FindPrime();
+ *          void computemean(int[] marksobtained);
+ *          int LOOP; String str_lastName; Double Average;
+ *          final int Tax_Rate;
+ * </pre></blockquote>
+ *
+ * THE CODE BELOW IS THE FIXED VERSION USING CAMEL CASES*/
+
+public class Manager {
+    class FullTimeEmployee{}
+    void findPrime();
+    void computeMean(int[] marksObtained);
+    int loop; String strLastName; Double average;
+    final int taxRate;
+}
