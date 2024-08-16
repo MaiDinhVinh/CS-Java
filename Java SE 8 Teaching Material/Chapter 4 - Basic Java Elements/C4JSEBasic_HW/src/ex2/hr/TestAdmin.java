a9ee45d1ab116128fe59c4eaa7bba1faa7718054
MaiDinhVinh <vingit96@gmail.com>
2024-08-17 01:45:20 +0700

    upload ex2 homework

---
+++ Java SE 8 Teaching Material/Chapter 4 - Basic Java Elements/C4JSEBasic_HW/src/ex2/hr/TestAdmin.java
@@ -0,0 +1,10 @@
+package ex2.hr;
+
+import ex2.hr.reporting.TimeCard;
+
+public class TestAdmin {
+    public static void main(String[] args) {
+        Admin adminHuyDo = new Admin("Huy Do");
+        adminHuyDo.getAllAuthorizedEmpls();
+    }
+}
