0a5828f1f641ba50e1afbfcd12a2e6b8321c3f2b
MaiDinhVinh <vingit96@gmail.com>
2024-08-17 23:24:48 +0700

    upload unfinished work

---
+++ Java SE 8 Teaching Material/Chapter 5 - Working With Java Data Type/C5JSEBasic/src/section5_3/section5_3_3/TestClass2.java
@@ -0,0 +1,13 @@
+package section5_3.section5_3_3;
+
+public class TestClass2 {
+    /**
+     * OBSERVATION: This code printed out {@code 0 0}*/
+    static int a;
+    int y;
+
+    public static void main(String[] args) {
+        int p;
+        System.out.println(a + " " + new TestClass2().y);
+    }
+}
