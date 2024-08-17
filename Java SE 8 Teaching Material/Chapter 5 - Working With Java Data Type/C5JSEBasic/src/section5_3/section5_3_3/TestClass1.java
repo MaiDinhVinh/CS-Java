e82b1805055467d222dc7b19f4549bd7e7d20c66
MaiDinhVinh <vingit96@gmail.com>
2024-08-17 23:24:24 +0700

    upload unfinished work

---
+++ Java SE 8 Teaching Material/Chapter 5 - Working With Java Data Type/C5JSEBasic/src/section5_3/section5_3_3/TestClass1.java
@@ -0,0 +1,12 @@
+package section5_3.section5_3_3;
+
+public class TestClass1 {
+    /**
+     * OBSERVATION: The code compiled successfuly without any error*/
+    static int i;
+    int y;
+
+    public static void main(String[] args) {
+        int p;
+    }
+}
