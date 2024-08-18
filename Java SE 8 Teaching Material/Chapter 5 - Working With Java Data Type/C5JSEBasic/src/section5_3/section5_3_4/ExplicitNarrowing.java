9f337c6e76bb58f2bd4eaefa8fb2b2a781730bc2
MaiDinhVinh <vingit96@gmail.com>
2024-08-18 23:57:48 +0700

    upload unfinished work

---
+++ Java SE 8 Teaching Material/Chapter 5 - Working With Java Data Type/C5JSEBasic/src/section5_3/section5_3_4/ExplicitNarrowing.java
@@ -0,0 +1,9 @@
+package section5_3.section5_3_4;
+
+public class ExplicitNarrowing {
+    public static void main(String[] args) {
+        int i = (int) 11.1;
+        //casting a floating point value to a integer value
+
+    }
+}
