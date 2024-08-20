ce1de01aa7daec4810f17e70cc37bb9a9ce7e9c7
MaiDinhVinh <vingit96@gmail.com>
2024-08-21 02:28:14 +0700

    upload unfinished code

---
+++ Java SE 8 Teaching Material/Chapter 6 - Working with Java Operators/C6JSEBasic/src/section6_1/section6_1_1/ArithmeticOperators/AddSubMulDivi.java
@@ -0,0 +1,19 @@
+package section6_1.section6_1_1.ArithmeticOperators;
+
+public class AddSubMulDivi {
+    /**
+     * This just perfrom simple arithmetic operation<p>
+     * This is operator is for Binary Operation (involves 2 operands)*/
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        int d = b - a;
+        int e = b * a;
+        int f = b / a;
+        System.out.println(c); //print 30
+        System.out.println(d); //print 10
+        System.out.println(e); //print 200
+        System.out.println(f); //print 2
+    }
+}
