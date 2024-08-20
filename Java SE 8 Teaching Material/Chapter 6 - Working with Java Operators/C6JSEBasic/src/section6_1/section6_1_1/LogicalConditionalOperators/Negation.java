b9df740b0aaec553e0f295b5d520bf9597f886a4
MaiDinhVinh <vingit96@gmail.com>
2024-08-21 02:31:53 +0700

    upload unfinished code

---
+++ Java SE 8 Teaching Material/Chapter 6 - Working with Java Operators/C6JSEBasic/src/section6_1/section6_1_1/LogicalConditionalOperators/Negation.java
@@ -0,0 +1,12 @@
+package section6_1.section6_1_1.LogicalConditionalOperators;
+
+public class Negation {
+    /**
+     * {@code !} represents Negation<p>
+     * This is Unary Operation (involves 1 operands)*/
+    public static void main(String[] args) {
+        boolean hello = false;
+        boolean goodbye = !hello;
+        System.out.println(goodbye); //true
+    }
+}
