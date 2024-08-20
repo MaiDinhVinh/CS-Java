6d35c09cc1317c4a85f2ce689b58d95edbe787ed
MaiDinhVinh <vingit96@gmail.com>
2024-08-21 02:30:26 +0700

    upload unfinished code

---
+++ Java SE 8 Teaching Material/Chapter 6 - Working with Java Operators/C6JSEBasic/src/section6_1/section6_1_1/AssignmentOperators/SimpleAssignment.java
@@ -0,0 +1,13 @@
+package section6_1.section6_1_1.AssignmentOperators;
+
+public class SimpleAssignment {
+    /**
+     * Simple Assignment: copying the value from the RHS to the
+     * vairable in the LHS*/
+    public static void main(String[] args) {
+        int a = 10; //copy 10 to variable a
+        Object obj1 = new Object();
+        Object obj2 = obj1; //copy memory address from obj1 to obj2
+
+    }
+}
