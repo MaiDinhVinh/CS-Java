7e8ef4488c828b6463201f4a0fbd1cb044f8cb1f
MaiDinhVinh <vingit96@gmail.com>
2024-08-22 09:13:41 +0700

    upload unfinished work

---
+++ Java SE 8 Teaching Material/Chapter 6 - Working with Java Operators/C6JSEBasic/src/section6_1/section6_1_1/MiscellaneousOperators/ArrayAccessOperator.java
@@ -0,0 +1,11 @@
+package section6_1.section6_1_1.MiscellaneousOperators;
+
+public class ArrayAccessOperator {
+    /**
+     * This is Binary Operaton (involves 2 operands)<p>
+     * This operator is used to access array element*/
+    public static void main(String[] args) {
+        int[] arr = new int[10]; //declaring an array with length = 10
+        arr[0] = 1; //change the first element of the array from 0 to 1
+    }
+}
