28b63fd55151f47a53ec96b9266aa4ba862bf051
MaiDinhVinh <vingit96@gmail.com>
2024-08-22 09:15:05 +0700

    upload unfinished work

---
+++ Java SE 8 Teaching Material/Chapter 6 - Working with Java Operators/C6JSEBasic/src/section6_1/section6_1_1/MiscellaneousOperators/CastOperator.java
@@ -0,0 +1,13 @@
+package section6_1.section6_1_1.MiscellaneousOperators;
+
+public class CastOperator {
+    /**
+     * This is Binary Operation (involves 2 operands)<p>
+     * This operator is used for explicit narrowing technique,
+     * where we assign a larger type value to a smaller type
+     * variable<p>
+     * This is also used for Upcast and Downcast*/
+    public static void main(String[] args) {
+        int i = (int) 1.0; //assigning double value to int variable
+    }
+}
