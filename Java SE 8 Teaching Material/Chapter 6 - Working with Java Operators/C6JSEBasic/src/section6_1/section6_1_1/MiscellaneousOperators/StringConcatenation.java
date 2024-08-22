b5538005c472c0b8481711326b6ffbf7a8c65cf7
MaiDinhVinh <vingit96@gmail.com>
2024-08-22 09:18:02 +0700

    upload unfinished work

---
+++ Java SE 8 Teaching Material/Chapter 6 - Working with Java Operators/C6JSEBasic/src/section6_1/section6_1_1/MiscellaneousOperators/StringConcatenation.java
@@ -0,0 +1,15 @@
+package section6_1.section6_1_1.MiscellaneousOperators;
+
+public class StringConcatenation {
+    /**
+     * This is Binary Operation (involves 2 operands)<p>
+     * Although we will study that this is used for String concatenation,
+     * however, we will learn soon that this operator is used to
+     * concatenate others data types with String
+     * */
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = str1 + " world";
+        System.out.println(str2);
+    }
+}
