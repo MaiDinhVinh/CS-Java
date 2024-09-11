2520725be55988cce2e1130dc3159797eaa85505
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:55:47 +0700

    upload main19

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main19.java
@@ -0,0 +1,13 @@
+/**
+ * What is the result of this class<p>
+ * A. It will not compile<p>
+ * B. It will print 10<p>
+ * C. It will print an unknown number*/
+/*class TestClass {
+    public static void main(String[] args) {
+        {
+            int x = 10;
+        }
+        System.out.println(x);
+    }
+}*/
