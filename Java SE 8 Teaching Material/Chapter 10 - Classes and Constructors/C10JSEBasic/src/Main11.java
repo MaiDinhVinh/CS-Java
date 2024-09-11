caa43e6603ed689854bacdc97aef9e51e49a6ea8
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:52:47 +0700

    upload main11

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main11.java
@@ -0,0 +1,26 @@
+/**
+ * Now, consider the following program. Can you tell which line out of the four lines marked
+ * LINE A, LINE B, LINE C, and LINE D should be uncommented so that it will print {@code 111, dummy}?*/
+
+/*
+class Account{
+    int id;
+    String name;
+    public Account(){
+        id = 111;
+        name = "dummy";
+    }
+    public void reset(){
+    //this(); //<-- LINE A
+    //Account(); //<-- LINE B
+    //this = new Account(); //<-- LINE C
+    //new Account(); //<-- LINE D
+    }
+    public static void main(String[] args) {
+        Account a = new Account();
+        a.id = 2;
+        a.name = "amy";
+        a.reset();
+        System.out.println(a.id+", "+a.name);
+    }
+}*/
