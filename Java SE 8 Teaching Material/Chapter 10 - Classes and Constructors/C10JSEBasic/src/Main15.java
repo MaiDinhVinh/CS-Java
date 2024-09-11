22de24042ba0eec0565c6432e935293d56492779
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:54:13 +0700

    upload main15

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main15.java
@@ -0,0 +1,11 @@
+/*
+class TestClass
+{
+    public static void main(String[] args){
+        for(int i = 0; i<10; i++)
+        {
+            int k = 0; //k is block scoped. It is reset to 0 in each iteration
+            System.out.println(i); // i retains its value from previous iteration
+        }//i and k are both out of scope here
+    }
+}*/
