b3852c4ce1f41403fc3c6c2ad32a2f215e151b03
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:53:55 +0700

    upload main14

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main14.java
@@ -0,0 +1,11 @@
+/*
+class TestClass
+{
+    public static void main(String[] args){
+        {
+            int i = 0; //i exists in this block only
+            System.out.println(i); //OK
+        }
+        System.out.println(i); //NOT OK because i has already gone out of scope
+    }
+}*/
