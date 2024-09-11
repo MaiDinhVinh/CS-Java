8c1521b1c794d58cb4a87e2eb0850c0ee492ff6f
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:57:34 +0700

    upload Main7.java

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main7.java
@@ -0,0 +1,10 @@
+/*
+class TestClass{
+    int someValue;
+    String someStr;
+    TestClass(int x) //No return type specified
+    {
+        this.someValue = x; //initializing someValue
+//return; //legal but not required
+    }
+}*/
