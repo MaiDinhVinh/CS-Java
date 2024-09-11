3a10f662a448220758733f368687dc1f0ebdc6b2
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:56:52 +0700

    upload main5

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main5.java
@@ -0,0 +1,16 @@
+/*
+public class Main5 {
+    private int a;
+
+    public void test(){
+        Main5 demo = this;
+        System.out.println("demo mem address: " + demo);
+    }
+
+    public static void main(String[] args) {
+        Main5 demo2 = new Main5();
+        demo2.test();
+        System.out.println("demo2 mem address: " + demo2);
+    }
+}
+*/
