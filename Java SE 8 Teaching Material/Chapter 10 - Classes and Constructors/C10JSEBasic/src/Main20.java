afc33b3a6770adf502e6b1f793fbb4d69235ab32
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:56:12 +0700

    upload main20

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main20.java
@@ -0,0 +1,24 @@
+/**
+ * What will this class print out<p>
+ * A. It will not compile<p>
+ * B. It will print:<p>
+ * {@code null}<p>
+ * {@code null}<p>
+ * {@code null}<p>
+ * C. It will print:<p>
+ * {@code null}<p>
+ * {@code null}<p>
+ * {@code hello}<p>
+ * */
+/*
+class ScopeTest {
+    private String[] args = new String[0];
+    public static void main(String[] args) {
+        args = new String[args.length];
+        for(String arg: args){
+            System.out.println(arg);
+        }
+        String arg = args[0];
+        System.out.println(arg);
+    }
+}*/
