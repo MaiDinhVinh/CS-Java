a3b47866d41bb8f0ea127ce2a23109113cef42f1
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:56:32 +0700

    upload main4

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main4.java
@@ -0,0 +1,10 @@
+/*
+class Student{
+    static int count = 0;
+    public void doSomething(){
+        int count = 10;
+        count = count; //technically valid but logically incorrect
+        Student.count = count; //works fine in instance as well as static methods
+        this.count = count; //works fine in an instance method
+    }
+}*/
