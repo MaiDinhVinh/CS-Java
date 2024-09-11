6cf38d6a0129be5f234e447a9e4153278f9eb56a
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:58:09 +0700

    upload main9

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main9.java
@@ -0,0 +1,16 @@
+/*
+class Account{
+    int id;
+    String name;
+    Account(String name){
+        this(111, name); //invoking another constructor here
+        System.out.println("returned from two args constructor");
+    }
+    Account(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    public static void main(String[] args) {
+        Account a = new Account("amy");
+    }
+}*/
