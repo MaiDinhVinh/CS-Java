6bd9f4a6ab343a604dcdea5a43954c9a75b831b5
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:52:26 +0700

    upload main10

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main10.java
@@ -0,0 +1,17 @@
+/*
+class Account{
+    int id;
+    String name;
+
+    Account(String name){
+        System.out.println("calling two args constructor");
+        this(111, name); //call to another constructor must be the first line
+    }
+    Account(){
+        this(111);
+        this(111, "amy"); //call to another constructor must be the first line
+    }
+    Account(String name){
+        Account(111, name); //incorrect way to call to another constructor but if the Account class had a method named Account, this would be a valid call to that method
+    }
+}*/
