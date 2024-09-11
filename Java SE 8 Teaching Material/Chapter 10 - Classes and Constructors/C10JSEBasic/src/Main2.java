f649e5ed3aa52322d986f5aca2164a91a2ab897d
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:51:49 +0700

    upload main2

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main2.java
@@ -0,0 +1,14 @@
+/*
+class Student {
+    String name;
+
+    public static void main(String[] args) {
+        Student s1 = new Student(); //1
+        s1.name = "mitchell"; //2
+        s1.printName(); //3 prints mitchell
+    }
+
+    public void printName() {
+        System.out.println(name); //5
+    }
+}*/
