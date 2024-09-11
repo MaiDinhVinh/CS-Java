b2eef8acebdd6dc97e95960240f3e08df99e6527
MaiDinhVinh <vingit96@gmail.com>
2024-09-11 18:59:02 +0700

    upload obscuring example

---
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Obscuring/Main.java
@@ -0,0 +1,20 @@
+/*
+package Obscuring;
+
+public class Main {
+    private int Test;
+
+    public Main(){
+        this.Test = 1;
+    }
+
+    public void mainMethod(){
+        Test.testMethod();
+    }
+
+    public static void main(String[] args) {
+        Main demo = new Main();
+        demo.mainMethod();
+    }
+}
+*/
