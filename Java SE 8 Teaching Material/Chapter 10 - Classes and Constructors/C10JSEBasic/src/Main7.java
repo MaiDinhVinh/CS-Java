04159504deb4ca0c793cf27e891043a46b925bdd
MaiDinhVinh <vingit96@gmail.com>
2024-09-14 23:40:59 +0700

    commit changes

--- Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main7.java
+++ Java SE 8 Teaching Material/Chapter 10 - Classes and Constructors/C10JSEBasic/src/Main7.java
@@ -1,10 +1,9 @@
-/*
-class TestClass{
+/*class TestClass{
     int someValue;
     String someStr;
     TestClass(int x) //No return type specified
     {
         this.someValue = x; //initializing someValue
-//return; //legal but not required
+    //return; //legal but not required
     }
 }*/
