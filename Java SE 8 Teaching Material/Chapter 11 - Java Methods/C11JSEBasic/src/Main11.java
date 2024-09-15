d8f5fbbe85f8bcd4949a72e05d517d4f515ad8df
MaiDinhVinh <vingit96@gmail.com>
2024-09-15 21:43:45 +0700

    upload main file

---
+++ Java SE 8 Teaching Material/Chapter 11 - Java Methods/C11JSEBasic/src/Main11.java
@@ -0,0 +1,9 @@
+/*
+class TestClass{
+    public static void main(String[] args){
+        System.out.println(ConnectionHelper.idle_timeout); //prints 0 (why?)
+        System.out.println(ConnectionHelper.url); //prints null (why?)
+        Request.Data rc = new Request.Data();
+    }
+}
+*/
