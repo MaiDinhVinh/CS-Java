4476c3897deff7ee91b6099722ad8f036bdb6c06
MaiDinhVinh <vingit96@gmail.com>
2024-09-15 21:47:59 +0700

    upload main file

---
+++ Java SE 8 Teaching Material/Chapter 11 - Java Methods/C11JSEBasic/src/Main4.java
@@ -0,0 +1,11 @@
+/*
+class TestClass {
+    public static void main(String[] args){
+        int a = doubleIt(100);
+        System.out.println(a); //prints 200
+    }
+    public static int doubleIt(int x){
+        return 2*x;
+    }
+}
+*/
