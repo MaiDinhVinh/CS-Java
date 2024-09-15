2a0c2c115d384cb58e7d9878486583e7a3c97385
MaiDinhVinh <vingit96@gmail.com>
2024-09-15 21:46:53 +0700

    upload main file

---
+++ Java SE 8 Teaching Material/Chapter 11 - Java Methods/C11JSEBasic/src/main17.java
@@ -0,0 +1,8 @@
+/*class Book{
+    int name;
+    static void printName(){
+        Book b1 = new Book();
+        Book b2 = new Book();
+        System.out.println(b1.name); //this will compile fine
+    }
+}*/
