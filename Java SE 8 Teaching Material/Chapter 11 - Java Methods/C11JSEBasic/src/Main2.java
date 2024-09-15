641fa59845211e84f3b6e2282f4f2e28fc1655a2
MaiDinhVinh <vingit96@gmail.com>
2024-09-15 21:47:20 +0700

    upload main file

---
+++ Java SE 8 Teaching Material/Chapter 11 - Java Methods/C11JSEBasic/src/Main2.java
@@ -0,0 +1,16 @@
+/*
+class TestClass{
+    static void doSomething(Integer i, short s){
+        System.out.println("1");
+    }
+    static void doSomething(int in, Short s){
+        System.out.println("1");
+    }
+    public static void main(String[] args){
+        int b = 10;
+//        short x = 20;
+        Short x = new Short((short)20);
+        doSomething(b, x);
+    }
+}
+*/
