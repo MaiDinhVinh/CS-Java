72a4a87a3638eed3c1f718441b5b45bfd3aac50c
MaiDinhVinh <vingit96@gmail.com>
2024-09-15 21:49:26 +0700

    upload main file

---
+++ Java SE 8 Teaching Material/Chapter 11 - Java Methods/C11JSEBasic/src/Main8.java
@@ -0,0 +1,16 @@
+/*
+class Data {
+    int value = 100;
+}
+class TestClass {
+    public static void main(String[] args){
+        Data d = new Data();
+        modifyData(d);
+        System.out.println(d.value);
+    }
+    public static void modifyData(Data x){
+        x = new Data();
+        x.value = 2*x.value;
+    }
+}
+*/
