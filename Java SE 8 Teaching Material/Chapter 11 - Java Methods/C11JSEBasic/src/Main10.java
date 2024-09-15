e10a410d4a726c8d99987f259c1edb315eaaa0ec
MaiDinhVinh <vingit96@gmail.com>
2024-09-15 21:43:02 +0700

    upload main file

---
+++ Java SE 8 Teaching Material/Chapter 11 - Java Methods/C11JSEBasic/src/Main10.java
@@ -0,0 +1,8 @@
+/*
+class ConnectionHelper{
+    static int idle_timeout;
+    static String url;
+    private static void ping(){ } //order of modifiers doesn't matter
+    static public final void check(){ } //order of modifiers doesn't matter
+}
+*/
