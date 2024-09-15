2a0d4d63f0dc94b70bdafc35dcd1c959c773c399
MaiDinhVinh <vingit96@gmail.com>
2024-09-15 21:45:11 +0700

    upload main file

---
+++ Java SE 8 Teaching Material/Chapter 11 - Java Methods/C11JSEBasic/src/Main15.java
@@ -0,0 +1,11 @@
+/*
+class TestClass {
+    public static void main(String[] args){
+        InstanceCounter ic = new InstanceCounter();
+        ic.printCount(); //accessing static method through a reference to an instance
+        new InstanceCounter().printCount(); //accessing static method through an implicit reference to an instance
+        InstanceCounter.printCount(); //accessing static method using the class name
+        System.out.println(InstanceCounter.count); //accessing static field using the class name
+    }
+}
+*/
