cc94c080e32cc3d8459a9da2fc5fd43061e42249
MaiDinhVinh <vingit96@gmail.com>
2024-09-15 21:47:41 +0700

    upload main file

---
+++ Java SE 8 Teaching Material/Chapter 11 - Java Methods/C11JSEBasic/src/Main3.java
@@ -0,0 +1,11 @@
+/*
+class TestClass{
+    public void m(char ch){
+        System.out.println("in char");
+    }
+    public static void main(String[] args) {
+        byte b = 10;
+        new TestClass().m(b);
+    }
+}
+*/
