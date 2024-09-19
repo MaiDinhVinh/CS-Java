044da3464a5de0d49ea1cc3fc09be06817fd49ba
MaiDinhVinh <vingit96@gmail.com>
2024-09-19 23:02:24 +0700

    upload changes

--- Java SE 8 Teaching Material/Chapter 12 + 13 - Working with Math, Random and String classes/C12-13JSEBasic/src/Main2.java
+++ Java SE 8 Teaching Material/Chapter 12 + 13 - Working with Math, Random and String classes/C12-13JSEBasic/src/Main2.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main2 {
+/*
+class TestClass{
+    static String code = "1234";
+    public static void checkCode(String str){
+        System.out.println(code == str);
+    }
+    public static void main(String[] args){
+        checkCode("1234");
+        checkCode(new String("1234"));
+    }
 }
+*/
