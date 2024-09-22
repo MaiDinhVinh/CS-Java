3aa09f3cb5f605a0a30d23d4b8d619740a11ebad
MaiDinhVinh <vingit96@gmail.com>
2024-09-23 01:34:20 +0700

    upload main file

--- Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main4.java
+++ Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main4.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
+/*
 public class Main4 {
+    public static void message() throws Exception{
+        if(true) throw new Exception();
+        else return;
+    }
+    public static void main(String[] args) { //no throws clause necessary
+        try{
+            message();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
+*/
