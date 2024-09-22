30fede6e097a34cdeab10247eb50c29a0e0b17c4
MaiDinhVinh <vingit96@gmail.com>
2024-09-23 01:32:17 +0700

    upload main2 file

---
+++ Java SE 8 Teaching Material/Chapter 12 + 13  Working with Math, Random and String classes/C12-13JSEBasic/src/Main2.java
@@ -0,0 +1,12 @@
+/*
+class TestClass{
+    static String code = "1234";
+    public static void checkCode(String str){
+        System.out.println(code == str);
+    }
+    public static void main(String[] args){
+        checkCode("1234");
+        checkCode(new String("1234"));
+    }
+}
+*/
