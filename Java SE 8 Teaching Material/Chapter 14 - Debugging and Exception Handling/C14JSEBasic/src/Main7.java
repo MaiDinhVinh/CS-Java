5ddf31d07bed65fb98a97b8e3a5399385b4959dc
MaiDinhVinh <vingit96@gmail.com>
2024-09-23 01:35:36 +0700

    upload main file

--- Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main7.java
+++ Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main7.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
-public class main7 {
-}
+/**
+ * Look at the parameter passed in into the method and the catch block*/
+/*public class Main7 {
+    public static double computeInterest(double p, double r, int t) throws Exception{
+        if(t<0) throw new Exception("t must be > 0");
+        else return p*r*t;
+    }
+    public static void main(String[] args){
+        double interest = 0.0;
+        try{
+            computeInterest(100, 0.1, -1);
+        }catch(Exception e){
+        }
+        System.out.println(interest);
+    }
+}*/
