0177d8aa2a2b30206ddb7008034963033449542c
MaiDinhVinh <vingit96@gmail.com>
2024-09-23 01:36:11 +0700

    upload main file

--- Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main8.java
+++ Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main8.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
+/*
 public class Main8 {
+    public static double computeInterest(double p, double r, int t) throws Exception{
+        if(t<0) throw new Exception("t must be > 0");
+        else return p*r*t;
+    }
+    public static void main(String[] args){
+        double interest = 0.0;
+        try{
+            computeInterest(100, 0.1, -1);
+        }catch(Exception e){
+            e.printStackTrace();
+            //or System.out.println(e);
+        }
+        System.out.println(interest);
+    }
 }
+*/
