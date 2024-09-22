9bbb4863bba9e8d2ad11bf65ba076345b238898b
MaiDinhVinh <vingit96@gmail.com>
2024-09-23 01:33:35 +0700

    upload main file

--- Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main2.java
+++ Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main2.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
-public class Main2 {
+/*
+//try what happen if we remove the try/catch block in doInterest method
+//What happen if we remove the throws clause in computeSimpleInterest method
+class TestClass{
+    public static void main(String[] args){
+        TestClass tc = new TestClass();
+        tc.doInterest();
+    }
+    public double computeSimpleInterest(double p, double r, double t){
+        if( t<0) throw new IllegalArgumentException("time is less than 0");
+        return p*r*t/100;
+    }
+    public void doInterest(){
+        try{
+            double interest = computeSimpleInterest(1000.0, 10.0, -1);
+            System.out.println("Computed interest "+interest);
+        }catch(IllegalArgumentException iae){
+            System.out.println("Problem in computing interest:"+iae.getMessage());
+            iae.printStackTrace();
+        }finally{
+            System.out.println("all done");
+        }
+    }
 }
+*/
