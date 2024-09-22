019df863134a38cce9378e44e50ff616cf8f7d04
MaiDinhVinh <vingit96@gmail.com>
2024-09-23 01:35:12 +0700

    upload main file'

--- Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main6.java
+++ Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main6.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class Main6 {
-}
+/*public class Main6 {
+    public static void message() throws Exception{
+        if(true) throw new Exception();
+        else return;
+    }
+    public static void callMessage() throws Throwable {
+        message();
+    }
+    //NOTICE THE TYPE OF EXCEPTION IS BEING THROWN RIGHT NOW
+}*/
