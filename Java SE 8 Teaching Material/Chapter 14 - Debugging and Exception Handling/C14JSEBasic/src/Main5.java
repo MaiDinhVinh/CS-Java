f181b382b2f98e5bf3c669406c84f07ffb83fe70
MaiDinhVinh <vingit96@gmail.com>
2024-09-23 23:23:50 +0700

    FINAL COMMIT

--- Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main5.java
+++ Java SE 8 Teaching Material/Chapter 14 - Debugging and Exception Handling/C14JSEBasic/src/Main5.java
@@ -1,5 +1,4 @@
-/*
-public class Main5 {
+/*public class Main5 {
     public static void message() throws Exception{
         if(true) throw new Exception();
         else return;
@@ -7,5 +6,4 @@ public class Main5 {
     public static void callMessage() throws Exception {
         message();
     }
-}
-*/
+}*/
