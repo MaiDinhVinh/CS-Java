9b3eecebe38a6eab2a194806e8fb7313376a3a2a
MaiDinhVinh <vingit96@gmail.com>
2024-09-29 21:11:53 +0700

    upload ex3 lab2 redo

--- redo lab/LAB2/src/EX3.java
+++ redo lab/LAB2/src/EX3.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX3 {
+    public static void main(String[] args) {
+        System.out.print("Enter temp in Fahrenheit: ");
+        Scanner sc = new Scanner(System.in);
+        double input = sc.nextDouble();
+        System.out.format("Temp %f in Fahrenheit = %f Celsius", input, ((input-32)/9*5));
+
+    }
 }
