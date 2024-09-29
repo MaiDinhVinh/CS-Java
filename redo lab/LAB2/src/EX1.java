e0527abe10a25120ff20b93dbd21cfebefd268ba
MaiDinhVinh <vingit96@gmail.com>
2024-09-29 21:10:39 +0700

    upload redo ex1 - lab2

--- redo lab/LAB2/src/EX1.java
+++ redo lab/LAB2/src/EX1.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX1 {
+    public static void main(String[] args) {
+        System.out.print("Enter the distance in inches: ");
+        Scanner sc = new Scanner(System.in);
+        double input = (double)sc.nextInt();
+        System.out.println(input + " inches" + " = " + (input*2.54) + " cm");
+    }
 }
