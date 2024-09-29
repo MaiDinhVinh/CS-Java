f8cde1d01a577692bdee5294d8c509b6081df495
MaiDinhVinh <vingit96@gmail.com>
2024-09-29 21:12:22 +0700

    upload ex4 lab2 redo

--- redo lab/LAB2/src/EX4.java
+++ redo lab/LAB2/src/EX4.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter height: ");
+        int height = sc.nextInt();
+        System.out.print("Enter width: ");
+        int width = sc.nextInt();
+        System.out.format("Perimeter of rectangle is: %d%n", (height+width)*2);
+        System.out.format("Area of rectangle is: %d", (height*width));
+    }
 }
