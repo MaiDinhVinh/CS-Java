182bf4ec3deb89e91165ece4f1bdd665d1fe4e91
MaiDinhVinh <vingit96@gmail.com>
2024-09-29 21:11:04 +0700

    upload ex2 lab 2 redo

--- redo lab/LAB2/src/EX2.java
+++ redo lab/LAB2/src/EX2.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int i = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int i2 = sc.nextInt();
+        System.out.format("Value entered are i=%d and i2=%d%n", i, i2);
+        int temp = 0;
+        temp = i;
+        i = i2;
+        i2 = temp;
+        System.out.format("Value after swap are i=%d and i2=%d%n", i, i2);
+    }
 }
