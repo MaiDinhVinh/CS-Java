b37bd5abc10e0dbc6c3ca74ed059874844fd85b4
MaiDinhVinh <vingit96@gmail.com>
2024-10-03 01:04:34 +0700

    initial commit

---
+++ redo lab/LAB2/src/BMI.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class EX4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your gender (0 for female, 1 for male): ");
+        int gInput = sc.nextInt();
+        System.out.print("Enter your weight (kg) and height(m): ");
+        double wInput = sc.nextDouble();
+        double hInput = sc.nextDouble();
+        double BMI = wInput/(hInput * hInput);
+        switch (gInput){
+            case 0:
+                if(BMI <= 19) System.out.println("You are a little skinny");
+                else if(BMI > 19 && BMI <= 24)
+                    System.out.println("You are in good shape");
+                else if (BMI > 24) System.out.println("You are a little big");
+                break;
+            case 1:
+                if(BMI <= 20) System.out.println("You are a little skinny");
+                else if (BMI > 20 && BMI <= 25)
+                    System.out.println("You are in good shape");
+                else if(BMI > 25) System.out.println("You are a little big");
+        }
+
+    }
+}
---
+++ redo lab/LAB2/src/Suffix.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class EX5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = "st";
+        String nd = "nd";
+        String rd = "rd";
+        String th = "th";
+        System.out.print("Enter value: ");
+        int input = sc.nextInt();
+        if(input%100 == 11 || input%100 == 12 || input%100 == 13){
+            System.out.format("%d%s", input, th);
+        }else{
+            if(input%10 == 1){
+                System.out.format("%d%s", input, st);
+            }else if(input%10 == 2){
+                System.out.format("%d%s", input, nd);
+            }else if(input%10 == 3){
+                System.out.format("%d%s", input, rd);
+            }else{
+                System.out.format("%d%s", input, th);
+            }
+        }
+    }
+}
