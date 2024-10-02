3bab50c6608b0b004af40d25445f515fce087257
MaiDinhVinh <vingit96@gmail.com>
2024-10-03 01:08:21 +0700

    upload lab2 redo

--- redo lab/LAB2/src/EX2.java
+++ redo lab/LAB2/src/EX2.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 3 distinct integers: ");
+        int i1 = sc.nextInt();
+        int i2 = sc.nextInt();
+        int i3 = sc.nextInt();
+        if(i1 > i2 && i1 > i3){
+            System.out.format("%d is the greatest", i1);
+        } else if(i2 > i1 && i2 > i3){
+            System.out.format("%d is the greatest", i2);
+        } else if(i3 > i1 && i3 > i2){
+            System.out.format("%d is the greatest", i3);
+        }
+    }
 }
