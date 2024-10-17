1d1ae299c8aa44151db2dae54be783e1d7cd2fb7
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 22:12:06 +0700

    upload ex2 lab4 redo

--- redo lab/LAB4/src/EX2.java
+++ redo lab/LAB4/src/EX2.java
@@ -1,6 +1,12 @@
-
-public class EX1 {
+import java.util.Scanner;
+public class EX2 {
+    public static double inchToCm(double input){
+        return input*2.54;
+    }
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the distance in inches: ");
+        double input = sc.nextDouble();
+        System.out.format("%f inche(s) = %f cm", input, inchToCm(input));
     }
 }
