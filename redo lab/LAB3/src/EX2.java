187d3191a241c0e7173f8216a70cb5210c219714
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 12:39:38 +0700

    upload lab3 ex2 redo

--- redo lab/LAB3/src/EX2.java
+++ redo lab/LAB3/src/EX2.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX2 {
+    public static void main(String[] args) {
+        int max = -Integer.MAX_VALUE;
+        int input = 0;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("Enter a number: ");
+            input = sc.nextInt();
+            if(input > max){
+                max = input;
+            }
+        } while(input >= 0);
+        System.out.format("The maximum number is %d", max);
+    }
 }
