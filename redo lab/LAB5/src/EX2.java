0cd399cf2c87bfd7dcecc093b144684ab651ee65
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 23:41:33 +0700

    upload ex2 lab5 redo

--- redo lab/LAB5/src/EX2.java
+++ redo lab/LAB5/src/EX2.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class EX2 {
+
+    public static boolean isAscending(int[] arr){
+        int element = -Integer.MAX_VALUE;
+        int duplicatedVal = 0;
+        for(int i: arr){
+            if(i > element){
+                element = i;
+            }else if(i == element){
+                duplicatedVal++;
+                element = i;
+            }else{
+                return false;
+            }
+        }
+        if(duplicatedVal == arr.length - 1) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {38, 38, 38, 38};
+        String result = isAscending(arr)?" ":" NOT ";
+        System.out.format("Elements are%sin ascending order%n", result);
+    }
 }
