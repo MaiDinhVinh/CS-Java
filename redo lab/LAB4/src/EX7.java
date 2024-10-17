f21166b0a2f09119dc2edf1a6865f3a423c5ae66
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 22:14:02 +0700

    upload ex7 lab4 redo

--- redo lab/LAB4/src/EX7.java
+++ redo lab/LAB4/src/EX7.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX7 {
+
+    public static boolean containDigit7(int input){
+        for (int i = input; i > 0; i/=10){
+            if(i % 10 == 7){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int sumWithout7(int input){
+        int sum = 0;
+        for(int i = 0; i <= input; i++){
+            if(containDigit7(i)) continue;
+            else sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumWithout7(30));
+    }
 }
