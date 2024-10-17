d98d2e2edf0bf95e454e17dc3bd637d707346c37
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 23:40:59 +0700

    upload ex1 lab5 redo

--- redo lab/LAB5/src/EX1.java
+++ redo lab/LAB5/src/EX1.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class EX1 {
+
+    public static int countDiv3(int[] arr){
+        int divisibleCount = 0;
+        for(int i: arr){
+            if(i % 3 == 0){
+                divisibleCount++;
+            }
+        }
+        return divisibleCount;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,0,0,0};
+        System.out.println(countDiv3(arr));
+    }
 }
