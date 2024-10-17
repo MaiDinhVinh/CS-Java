c2ca95477f94e538ae6dd83afaa7cbe0292606df
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 12:58:56 +0700

    redo ex5 lab3

--- redo lab/LAB3/src/Validate.java
+++ redo lab/LAB3/src/Validate.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class EX5 {
+public class Validate {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age;
@@ -7,8 +7,12 @@ public class EX5 {
         do{
             System.out.print("Enter age: ");
             age = sc.nextInt();
-            
-
-        }
+            attempt++;
+            if(age > 0){
+                System.out.format("Your age is %d%n", age);
+                System.out.format("Number of attempt: %s%n", attempt);
+                break;
+            }
+        }while(age <= 0);
     }
 }
