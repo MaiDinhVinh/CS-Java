14f746eb938d5df9151dfcf5912f97f2d611f1a2
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 23:43:27 +0700

    upload ex6 lab5 redo

--- redo lab/LAB5/src/EX6.java
+++ redo lab/LAB5/src/EX6.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class EX6 {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 2, 4},
+                {2, 1, 4, 2},
+                {2, 0, 0, 2},
+                {4, 2, 2, 1}
+        };
+        int prod1 = 1;
+        int colPos = arr.length - 1;
+        int prod2 = 1;
+        for(int i = 0; i < arr.length; i++, colPos--){
+            prod1 *= arr[i][i];
+            prod2 *= arr[i][colPos];
+        }
+        System.out.println(prod1);
+    }
 }
