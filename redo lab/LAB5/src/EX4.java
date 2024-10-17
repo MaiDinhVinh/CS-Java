2965077ec43ab301dea6f4c684517ec85718e189
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 23:42:25 +0700

    upload ex4 lab5 redo

--- redo lab/LAB5/src/EX4.java
+++ redo lab/LAB5/src/EX4.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class EX4 {
+
+    public static int sumLast3(int[] arr){
+        int sum = 0;
+        if(arr.length > 3){
+            for(int i = arr.length -1; i >= arr.length - 3; i--){
+                sum += arr[i];
+            }
+        }else{
+            for(int i: arr){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {};
+        System.out.println(sumLast3(arr));
+    }
 }
