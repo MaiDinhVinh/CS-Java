2cc64bce9e4e3fa38cc1b3a4e4ccb8a89511a5ca
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 22:14:55 +0700

    upload idk lab4 redo

--- redo lab/LAB4/src/MagicNumber.java
+++ redo lab/LAB4/src/MagicNumber.java
@@ -1,5 +1,15 @@
-public class EX6 {
+import java.util.Scanner;
+public class MagicNumber {
+
+    public static int getMagic(int input){
+        int firstMagic = input%10;
+        int secondMagic = input%1000/100;
+        int thirdMagic = input%100000/10000;
+        return (firstMagic + secondMagic + thirdMagic)%10;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println(getMagic(55377));
     }
 }
