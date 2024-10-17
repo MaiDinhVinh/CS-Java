f2befa3d0c9f841fcb6c0f658eae31f384d2df83
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 12:36:36 +0700

    redo lab3 ex1

--- redo lab/LAB3/src/EX1.java
+++ redo lab/LAB3/src/EX1.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = 0;
+        do{
+            System.out.print("Enter a number: ");
+            input = sc.nextInt();
+        }while (input >= 0);
+    }
 }
