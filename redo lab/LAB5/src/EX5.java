c78df8d10d71760597a2a3079d20d8285d240c84
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 23:43:03 +0700

    upload ex5 lab5 redo

--- redo lab/LAB5/src/EX5.java
+++ redo lab/LAB5/src/EX5.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class EX5 {
+    public static void main(String[] args) {
+        int min = Integer.MAX_VALUE;
+        int[][] arr = { {1, 2, 4, -2}, {-2, -3, -1, 0} };
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if(arr[i][j] < min){
+                    min = arr[i][j];
+                }
+            }
+        }
+        System.out.println(min);
+    }
 }
