490fec2899a8124ddf6dca2fda5b39e93f55c7f2
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 12:26:36 +0700

    upload lab3 redo

---
+++ redo lab/LAB3/src/Validate.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class EX5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age;
+        int attempt = 0;
+        do{
+            System.out.print("Enter age: ");
+            age = sc.nextInt();
+            
+
+        }
+    }
+}
