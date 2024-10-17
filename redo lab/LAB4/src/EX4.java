40e8a46b32c694ab42971ddf5d2e1b22320a0fe1
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 22:13:07 +0700

    upload ex4 lab4 redo

--- redo lab/LAB4/src/EX4.java
+++ redo lab/LAB4/src/EX4.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX4 {
+
+    public static boolean isLeapYear(int input){
+        if(input % 4 == 0 || input % 100 == 0 && input % 400 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input;
+        do{
+            System.out.print("Enter a year: ");
+            input = sc.nextInt();
+            if(input > 0){
+                if(isLeapYear(input)) System.out.println("It is a leap year");
+                else System.out.println("It is NOT a leap year");
+            }
+        }while(input > 0);
+    }
 }
