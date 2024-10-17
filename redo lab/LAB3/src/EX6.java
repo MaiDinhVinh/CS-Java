20cb307dc0ac9092d0ab17a20dd9d2b3f4c4ac3f
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 12:52:42 +0700

    upload ex6 lab3 redo

--- redo lab/LAB3/src/EX6.java
+++ redo lab/LAB3/src/EX6.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input;
+        int factorial = 1;
+        do{
+            System.out.print("Enter a number: ");
+            input = sc.nextInt();
+            if(input < 0){
+                System.out.println("You should enter a positive number !");
+            }
+        }while(input < 0);
+        if(input == 0){
+            System.out.println("0! = 1");
+        }else{
+            for(int i = 1; i <= input; i ++){
+                factorial *= i;
+            }
+            System.out.format("%d! = %d", input, factorial);
+        }
+    }
 }
