e704fe3fc65be641ee9a89f5cfc8af611d63873a
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 22:13:35 +0700

    upload ex5 lab4 redo

--- redo lab/LAB4/src/EX5.java
+++ redo lab/LAB4/src/EX5.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EX5 {
+
+    public static int getSmallest(int a, int b, int c){
+        int smallest = 0;
+        if(a < b && a < c){
+            smallest = a;
+        }else if (b < a && b < c){
+            smallest = b;
+        }else if(c < a && c < b){
+            smallest = c;
+        }
+        return smallest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        int c;
+        do{
+            System.out.print("Enter 3 integers: ");
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = sc.nextInt();
+            if(a != b && a != c && b != c)
+                System.out.format("%d is the smallest%n", getSmallest(a, b ,c));
+        }while (a != b && a != c && b != c);
+    }
 }
