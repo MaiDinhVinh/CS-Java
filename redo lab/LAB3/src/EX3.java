3a5841bbf03cd539de16895ed8bc1ceb93d6f1b1
MaiDinhVinh <vingit96@gmail.com>
2024-10-17 12:40:41 +0700

    upload ex3 lab3 redo

--- redo lab/LAB3/src/EX3.java
+++ redo lab/LAB3/src/EX3.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class EX3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 9; i <= 300; i++){
+            if(i % 7 == 0 && i % 63 != 0){
+                sum += i;
+            }
+        }
+        System.out.format("Sum of integers between 9 & 300 that are divisible by 7 but not by 63 is %s",
+                sum);
+    }
 }
