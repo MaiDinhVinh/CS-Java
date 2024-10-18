5f18df8f32c90407f41fb5962f08254792515d6c
MaiDinhVinh <vingit96@gmail.com>
2024-10-18 12:38:16 +0700

    upload ex8 ver1 lab5 redo

--- redo lab/LAB5/src/EX8_1.java
+++ redo lab/LAB5/src/EX8_1.java
@@ -1,5 +1,38 @@
-public class EX8 {
+public class EX8_1 {
     public static void main(String[] args) {
-        
+        int[] arr = {37, 13, 37, 10, 9};
+        int pivot = 11;
+        int lessThanOrEqualCount = 0;
+        int greaterThanCount = 0;
+        for(int i: arr){
+            if(i <= pivot){
+                lessThanOrEqualCount++;
+            }
+        }
+        for(int i: arr){
+            if(i > pivot){
+                greaterThanCount++;
+            }
+        }
+        int[] lessThanOrEqual = new int[lessThanOrEqualCount];
+        int[] greaterThan = new int[greaterThanCount];
+        int lessThanIndex = 0;
+        int greaterThanIndex = 0;
+        for(int i: arr){
+            if(i <= pivot){
+                lessThanOrEqual[lessThanIndex] = i;
+                lessThanIndex++;
+            }else if(i > pivot){
+                greaterThan[greaterThanIndex] = i;
+                greaterThanIndex++;
+            }
+        }
+        for(int i: lessThanOrEqual){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+        for(int i: greaterThan){
+            System.out.print(i + " ");
+        }
     }
 }
