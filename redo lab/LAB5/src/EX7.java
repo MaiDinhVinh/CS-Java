371e5b3f7e00f236234be037ff99ba0001c0e3c9
MaiDinhVinh <vingit96@gmail.com>
2024-10-18 12:37:48 +0700

    upload ex7 lab5 redo

--- redo lab/LAB5/src/EX7.java
+++ redo lab/LAB5/src/EX7.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class EX7 {
+
+    public static boolean isPartOf(int[] arr, int x){
+        for(int i: arr){
+            if(i == x){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSubSet(int[] arrA, int[] arrB){
+        if(arrA.length == 0) return false;
+        for(int i: arrA){
+            if(!(isPartOf(arrB, i))) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arrA = {};
+        int[] arrB = {37, 0, 13, 37, 3, 10, 9};
+        System.out.println(isSubSet(arrA, arrB));
+
+    }
 }
