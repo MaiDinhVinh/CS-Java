0f6df20f35dfab1b7e031bfbf8d6b69356a56868
MaiDinhVinh <vingit96@gmail.com>
2024-10-18 12:38:40 +0700

    upload ex8 ver2 lab5 redo

--- redo lab/LAB5/src/EX8_2.java
+++ redo lab/LAB5/src/EX8_2.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class EX8_2 {
+
+    public static void partition(int[] arr, int pivot){
+        int lessThanOrEqualCount = 0;
+        int greaterThanCount = 0;
+        for(int i: arr){
+            if(i <= pivot){
+                lessThanOrEqualCount++;
+            }
+        }
+        for(int i: arr){
+            if(i > pivot){
+                greaterThanCount++;
+            }
+        }
+        int[] lessThanOrEqual = new int[lessThanOrEqualCount];
+        int[] greaterThan = new int[greaterThanCount];
+        int lessThanIndex = 0;
+        int greaterThanIndex = 0;
+        for(int i: arr){
+            if(i <= pivot){
+                lessThanOrEqual[lessThanIndex] = i;
+                lessThanIndex++;
+            }else if(i > pivot){
+                greaterThan[greaterThanIndex] = i;
+                greaterThanIndex++;
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(i < lessThanIndex){
+                arr[i] = lessThanOrEqual[i];
+            }else if(i - lessThanIndex < greaterThanIndex){
+                arr[i] = greaterThan[i - lessThanIndex];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {37};
+        int pivot = 20;
+        partition(arr, pivot);
+        for(int i: arr){
+            System.out.println(i);
+        }
+    }
 }
