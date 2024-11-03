d156b8f2239ec78f62cdeb2deac6b5eaf4e201db
DucksaberVN <business@ducksabervn.com>
2024-11-04 01:40:33 +0700

    upload chap 1 codes

---
+++ self-study resources/J8SE1/C1J8SE1/src/Main.java
@@ -0,0 +1,64 @@
+//snippet 1
+/*class Main{
+
+}*/
+
+//snippet 2
+/*class Main{
+    String name;
+}*/
+//snippet 3
+/*class Animal {
+    String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+}*/
+
+//snippet 4
+/*class Zoo{
+    public static void main(String[] args) {
+    }
+}*/
+
+//snippet 5
+/*class Zoo{
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}*/
+
+//snippet 6
+/*import java.util.Date;
+import java.sql.Date;*/
+
+//snippet 7
+/*import java.util.*; //how to fix this ?
+import java.sql.*;
+
+class Main{
+    public static void main(String[] args) {
+        Date
+    }
+}*/
+
+//snippet 8
+/*import java.util.Date;
+import java.sql.*;
+class Main{
+    public static void main(String[] args) {
+        Date
+    }
+}*/
+
+//snippet 9
+/*import java.util.Date;
+class Main{
+    public static void main(String[] args) {
+        java.sql.Date
+    }
+}*/
