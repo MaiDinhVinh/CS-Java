3ddb39ae3561f9dac88868f4dcab0a85974f7636
DucksaberVN <business@ducksabervn.com>
2024-11-06 02:49:46 +0700

    upload main file

---
+++ self-study resources/J8SE1/C2J8SE1/src/Main.java
@@ -0,0 +1,35 @@
+public class Main{
+    public static void main(String[] args) {
+        //snippet 1
+        /*//Explain why these code are non-compilable ?
+        int x = 1.0;
+        short y = 1921222;
+        int z = 9f;
+        long t = 192301398193810323;*/
+
+        //snippet 2
+        /*int x = 0;
+        for(long y = 0, z = 4; x < 5 && y < 10; x++, y++) {
+            System.out.print(y + " ");
+        }
+        System.out.print(x);*/
+
+        //snippet 3
+        /*int x = 0;
+        for(long y = 0, x = 4; x < 5 && y < 10; x++, y++) {
+            System.out.print(x + " ");
+        }*/
+
+        //snippet 4
+        /*int x = 0;
+        long y = 10;
+        for(y = 0, x = 4; x < 5 && y < 10; x++, y++) {
+            System.out.print(x + " ");
+        }*/
+
+        //snippet 5
+        /*for(long y = 0, int x = 4; x < 5 && y<10; x++, y++) { // DOES NOT COMPILE
+            System.out.print(x + " ");
+        }*/
+    }
+}
