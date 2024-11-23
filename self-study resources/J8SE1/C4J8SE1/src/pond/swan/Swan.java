7d09b5692dee11e1ad5e8b3281e12d965c1f6eed
DucksaberVN <business@ducksabervn.com>
2024-11-23 21:29:50 +0700

    upload encapsulation example 2

--- self-study resources/J8SE1/C4J8SE1/src/pond/swan/Swan.java
+++ self-study resources/J8SE1/C4J8SE1/src/pond/swan/Swan.java
@@ -1,4 +1,25 @@
 package pond.swan;
+import pond.shore.Bird;
+public class Swan extends Bird{
+    public void swim() {
+        floatInWater(); // package access to superclass
+        System.out.println(text); // package access to superclass
+    }
+    public void helpOtherSwanSwim() {
+        Swan other = new Swan();
+        other.floatInWater(); // package access to superclass
+        System.out.println(other.text);// package access to superclass
+    }
+    public void helpOtherBirdSwim() {
+        Swan2 other = new Swan2();
+        other.floatInWater(); // DOES NOT COMPILE
+        //System.out.println(other.text); // DOES NOT COMPILE
+    }
+}
 
-public class Swan {
+class Swan2 extends Bird{
+    @Override
+    protected void floatInWater(){
+        System.out.println(this.text);
+    }
 }
