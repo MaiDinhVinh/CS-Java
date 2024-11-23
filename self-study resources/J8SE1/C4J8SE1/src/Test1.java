e32eed3e012e88edb8ee017091eae51dc96fbdbc
DucksaberVN <business@ducksabervn.com>
2024-11-23 21:28:39 +0700

    upload test file

--- self-study resources/J8SE1/C4J8SE1/src/Test1.java
+++ self-study resources/J8SE1/C4J8SE1/src/Test1.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Test1 {
+    public static int endOfDayPrice;
+    static{
+        //connect to stock exchange
+        //compute end of day price to VND
+        //add discount price, promotion price
+    }
 }
