dc59070d3215935b1803efdfc8c9fe00de20c990
DucksaberVN <business@ducksabervn.com>
2024-11-27 00:36:00 +0700

    upload unfinished work

--- self-study resources/J8SE1/C4J8SE1/src/Main8.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main8.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+class Hamster2 {
+    private String color;
+    private int weight;
+    public Hamster2(int weight) { // first constructor
+        //old code
+        /*this.weight = weight;
+        color = "brown";*/
 
-public class Main8 {
+        this(weight, "brown"); //new code
+    }
+    public Hamster2(int weight, String color) { // second constructor
+        this.weight = weight;
+        this.color = color;
+    }
 }
