b466db9468633433383c497c5a7961ed60b2eddd
DucksaberVN <business@ducksabervn.com>
2024-11-27 00:35:27 +0700

    upload unfinished work

--- self-study resources/J8SE1/C4J8SE1/src/Main6.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main6.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+/**
+ * Which of these classes will generate default constructor ?
+ * */
+class Rabbit1 {
+}
+class Rabbit2 {
+    public Rabbit2() { }
+}
+class Rabbit3 {
+    public Rabbit3(boolean b) { }
+}
+class Rabbit4 {
+    private Rabbit4() { }
+    //why should we be careful about this code ?
+}
 
-public class Main6 {
+class RabbitsMultiply{
+    public static void main(String[] args) {
+        Rabbit1 r1 = new Rabbit1();
+        Rabbit2 r2 = new Rabbit2();
+        Rabbit3 r3 = new Rabbit3(true);
+        //Rabbit4 r4 = new Rabbit4();
+    }
 }
