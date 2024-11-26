bcdbb829d480cb71dd5b4e99ba7f896e2032f3db
DucksaberVN <business@ducksabervn.com>
2024-11-27 00:34:48 +0700

    upload unfinished work

--- self-study resources/J8SE1/C4J8SE1/src/Main17.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main17.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+class NotImmutable {
+    private StringBuilder builder;
+    public NotImmutable(StringBuilder b) {
+        builder = b;
+    }
+    public StringBuilder getBuilder() {
+        return builder; //what should we do here ?
+    }
 
-public class Main17 {
+    //what can we also add here ?
 }
+
+
+class RunHere1{
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("initial");
+        NotImmutable problem = new NotImmutable(sb);
+        sb.append(" added");
+        StringBuilder gotBuilder = problem.getBuilder();
+        gotBuilder.append(" more");
+        System.out.println(problem.getBuilder());
+    }
+}
+
