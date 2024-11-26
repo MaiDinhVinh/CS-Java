ce806674fc1e09e247b4db1f1684e8af12393c8a
DucksaberVN <business@ducksabervn.com>
2024-11-27 00:33:33 +0700

    upload unfinished work

--- self-study resources/J8SE1/C4J8SE1/src/Main13.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main13.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
-public class Main13 {
+class YetMoreInitializationOrder {
+    static { add(2); }
+    static void add(int num) { System.out.print(num + " "); }
+    YetMoreInitializationOrder() { add(5); }
+    static { add(4); }
+    { add(6); }
+    static { new YetMoreInitializationOrder(); }
+    {  add(8); }
+    public static void main(String[] args){
+    }
 }
