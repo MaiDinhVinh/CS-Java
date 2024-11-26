0ff36dc8822b413d564d60603ee37ac0b77f94d9
DucksaberVN <business@ducksabervn.com>
2024-11-27 00:30:00 +0700

    upload unfinished work

--- self-study resources/J8SE1/C4J8SE1/src/Bunny.java
+++ self-study resources/J8SE1/C4J8SE1/src/Bunny.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Bunny {
+    private String color;
+    private int height;
+    private int length;
+    public Bunny(int length, int theHeight) {
+        length = this.length; //what is special about this code ?
+        height = theHeight; // what is special about this code ?
+        this.color = "white";
+    }
+    public static void main(String[] args) {
+        Bunny b = new Bunny(1, 2);
+        System.out.println(b.length + " " + b.height + " " + b.color);
+    }
 }
