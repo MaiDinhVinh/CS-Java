ce692b2f01a7f8d0ab3d3aa69b57131f066888ff
DucksaberVN <business@ducksabervn.com>
2024-11-27 00:33:11 +0700

    upload unfinished work

--- self-study resources/J8SE1/C4J8SE1/src/Main12.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main12.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+class InitializationOrder {
+    private String name = "Torchie";
+    { System.out.println(name); }
+    private static int COUNT = 0;
+    static{
+        System.out.println(COUNT);
+    }
 
-public class Main12 {
+    {
+        COUNT++; System.out.println(COUNT);
+    }
+
+    public InitializationOrder() {
+        System.out.println("constructor");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("read to construct");
+        new InitializationOrder();
+    }
 }
