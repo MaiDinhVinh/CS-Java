4e1e09af8bd669df5674d5d9759f787c44875235
DucksaberVN <business@ducksabervn.com>
2024-11-27 00:34:11 +0700

    upload unfinished work

--- self-study resources/J8SE1/C4J8SE1/src/Main15.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main15.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Main15 {
+    /**
+     * Identify the problem with the naming convention
+     * of each line of code here, if no problems are identified, skip*/
+    private boolean playing;
+    private String name;
+    public boolean getPlaying() { return playing; }
+    public boolean isPlaying() { return playing; }
+    public String name() { return name; }
+    public void updateName(String n) { name = n; }
+    public void setname(String n) { name = n; }
 }
