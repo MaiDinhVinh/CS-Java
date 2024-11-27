c0af6015d6af7ccdd846ff4389ed3dd835c8734c
DucksaberVN <business@ducksabervn.com>
2024-11-27 19:37:49 +0700

    chapter 4 work finished

--- self-study resources/J8SE1/C4J8SE1/src/Main18.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main18.java
@@ -2,13 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 public class Main18 {
     public static void main(String[] args) {
-        List<Animal> animals = new ArrayList<Animal>(); // list of animals
+        List<Animal> animals = new ArrayList<Animal>();
         animals.add(new Animal("fish", false, true));
         animals.add(new Animal("kangaroo", true, false));
         animals.add(new Animal("rabbit", true, false));
         animals.add(new Animal("turtle", false, true));
 
-        print(animals, new CheckIfHopper()); // pass class that does check
+        //old code
+        print(animals, new CheckIfHopper());
+
+        //new code
+        print(animals, a -> a.canHop());
+
+        //new code 2
+        print(animals, a -> a.canSwim());
+
+        //new code 3
+        print(animals, a -> ! a.canSwim());
+
     }
     private static void print(List<Animal> animals, CheckTrait checker) {
         for (Animal animal : animals) {
@@ -16,5 +27,5 @@ public class Main18 {
                 System.out.print(animal + " ");
             }
         System.out.println();
-        }
+    }
 }
