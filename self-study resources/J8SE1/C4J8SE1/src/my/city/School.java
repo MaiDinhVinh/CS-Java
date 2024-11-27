53ce16340483340288dbe44162df71d3571eb140
DucksaberVN <business@ducksabervn.com>
2024-11-27 19:38:58 +0700

    chapter 4 work finished

--- self-study resources/J8SE1/C4J8SE1/src/my/city/School.java
+++ self-study resources/J8SE1/C4J8SE1/src/my/city/School.java
@@ -1,4 +1,11 @@
 package my.city;
-
+import my.school.Classroom;
 public class School {
+    public static void main(String[] args) {
+        /*System.out.println(Classroom.globalKey);
+        Classroom room = new Classroom(101, "Mrs. Anderson");
+        System.out.println(room.roomNumber);
+        System.out.println(room.floor); //why this line doesn't get compile error ?
+        System.out.println(room.teacherName);*/
+    }
 }
