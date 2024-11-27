5ceb94d769c1bd4888d0ba4afeb8a549860795d0
DucksaberVN <business@ducksabervn.com>
2024-11-27 23:14:06 +0700

    chapter 4 completed

--- self-study resources/J8SE1/C4J8SE1/src/Main13.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main13.java
@@ -1,11 +1,25 @@
 class YetMoreInitializationOrder {
-    static { add(2); }
-    static void add(int num) { System.out.print(num + " "); }
-    YetMoreInitializationOrder() { add(5); }
-    static { add(4); }
-    { add(6); }
-    static { new YetMoreInitializationOrder(); }
-    {  add(8); }
+    static{
+        add(2);
+    }
+    static void add(int num){
+        System.out.print(num + " ");
+    }
+    YetMoreInitializationOrder(){
+        add(5);
+    }
+    static{
+        add(4);
+    }
+    {
+        add(6);
+    }
+    static{
+        new YetMoreInitializationOrder();
+    }
+    {
+        add(8);
+    }
     public static void main(String[] args){
     }
 }
