f05fb93bbc7e35dc044bd6495d4e6df4c79c50cf
DucksaberVN <business@ducksabervn.com>
2024-11-27 19:38:05 +0700

    chapter 4 work finished

--- self-study resources/J8SE1/C4J8SE1/src/Main19.java
+++ self-study resources/J8SE1/C4J8SE1/src/Main19.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
-public class Main19 {
+import java.util.*;
+import java.util.function.*;
+class PredicateSearch {
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Animal("fish", false, true));
+        print(animals, a -> a.canHop());
+    }
+    private static void print(List<Animal> animals, Predicate<Animal>
+            checker) {
+        for (Animal animal : animals) {
+            if (checker.test(animal)) System.out.print(animal + " ");
+        }
+        System.out.println();
+    }
 }
