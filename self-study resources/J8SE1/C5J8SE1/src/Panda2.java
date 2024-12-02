670e58c908d8eb438b68e86b9aa465572871edbe
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:28:35 +0700

    upload unfinished work

--- self-study resources/J8SE1/C5J8SE1/src/Panda2.java
+++ self-study resources/J8SE1/C5J8SE1/src/Panda2.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class Panda2 {
+public class Panda2 extends Bear2{
+    /*public void sneeze() {
+        System.out.println("Panda bear sneezes quietly");
+    }
+    public static void hibernate() {
+        System.out.println("Panda bear is going to sleep");
+    }*/
 }
