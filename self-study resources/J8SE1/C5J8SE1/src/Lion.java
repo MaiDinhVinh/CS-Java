11480b9309008c8c1a79a4f29216c5994ab0e76d
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:26:59 +0700

    upload unfinished work

--- self-study resources/J8SE1/C5J8SE1/src/Lion.java
+++ self-study resources/J8SE1/C5J8SE1/src/Lion.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+public class Lion extends Animal{
+    private void roar() {
+        System.out.println("The "+getAge()+" year old lion says: Roar!");
+        //what's wrong with this code ?
+        //System.out.println("The "+age+" year old lion says: Roar!");
 
-public class Lion {
+    }
 }
