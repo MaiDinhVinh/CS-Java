c1b5fb91ff4a7d19a48e6c6abd2bbf039e4f03b8
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:21:21 +0700

    upload unfinished work

--- self-study resources/J8SE1/C5J8SE1/src/Animal3.java
+++ self-study resources/J8SE1/C5J8SE1/src/Animal3.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Animal3 {
+    private int age;
+    private String name;
+    public Animal3(int age, String name) {
+        super();
+        this.age = age;
+        this.name = name;
+    }
+    public Animal3(int age) {
+        super();
+        this.age = age;
+        this.name = null;
+    }
 }
