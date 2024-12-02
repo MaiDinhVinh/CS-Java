a35d82b48cc39a74fc816f8c3509e3f62b45bf9d
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:31:31 +0700

    upload unfinished work

---
+++ self-study resources/J8SE1/C5J8SE1/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+        //snippet 1 - check Animal and Lion class
+
+        //snippet 2 - check Groundhog class
+
+        //snippet 3 - check Main2 class
+
+        //snippet 4 - check Animal2 and Zebra class
+
+        //snippet 5 - check Zoo class
+
+        //snippet 6 - check Animal3 and Gorilla class
+
+        //snippet 7 - check Mammal and Elephant class
+
+        //snippet 8 - check Mammal2 and Elephant2 class
+
+        //snippet 9 - check Mammal3 and Elephant3 class
+
+        //snippet 10 - check Chimpanzee class
+
+        //snippet 11 - check Shark class
+
+        //snippet 12 - check Shark class (2nd code)
+
+        //snippet 13 - check Shark class (3rd code)
+
+        //snippet 14 - check Shark class (4th code)
+
+        //snippet 15 - check Rabbit class
+
+        //snippet 16 - check Canine and Wolf class
+
+        //snippet 17 - check Camel and BactrianCamel class
+
+        //snippet 18 - check Insuff..., Reptile and Snake class
+
+        //snippet 19 - check Reptile2, Snake2 class
+
+        //snippet 20 - Check Camel2, BactrianCamel2 class
+
+        //snippet 21 - Check Bear, Panda class
+
+        //snippet 22 - Check Bear2, Panda2 class
+
+        //snippet 23 - check Bird, Penguin class
+    }
+}
