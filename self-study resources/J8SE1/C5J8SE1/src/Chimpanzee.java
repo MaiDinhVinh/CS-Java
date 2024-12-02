836207bebca4aefcb37e0b44665239f90ebc4232
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:25:21 +0700

    upload unfinished work

--- self-study resources/J8SE1/C5J8SE1/src/Chimpanzee.java
+++ self-study resources/J8SE1/C5J8SE1/src/Chimpanzee.java
@@ -1,2 +1,15 @@
-public class Main3 {
+class Primate {
+    public Primate() {
+        System.out.println("Primate");
+    }
+}
+class Ape extends Primate {
+    public Ape() {
+        System.out.println("Ape");
+    }
+}
+public class Chimpanzee extends Ape {
+    public static void main(String[] args) {
+        new Chimpanzee();
+    }
 }
