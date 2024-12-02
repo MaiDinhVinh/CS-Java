3708121ea2028c6e1149688294791bea2458a8b1
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:24:02 +0700

    upload unfinished work

--- self-study resources/J8SE1/C5J8SE1/src/Bear2.java
+++ self-study resources/J8SE1/C5J8SE1/src/Bear2.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Bear2 {
+    public static void sneeze() {
+        System.out.println("Bear is sneezing");
+    }
+    public void hibernate() {
+        System.out.println("Bear is hibernating");
+    }
 }
