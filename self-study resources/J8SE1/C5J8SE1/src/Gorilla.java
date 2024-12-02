a80e2533fed07a3fff284d50047aafd764e7ecb3
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:26:16 +0700

    upload unfinished work

--- self-study resources/J8SE1/C5J8SE1/src/Gorilla.java
+++ self-study resources/J8SE1/C5J8SE1/src/Gorilla.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class Gorilla {
+public class Gorilla extends Animal3{
+    public Gorilla(int age) {
+        super(age,"Gorilla");
+    }
+    public Gorilla() {
+        super(5);
+    }
 }
