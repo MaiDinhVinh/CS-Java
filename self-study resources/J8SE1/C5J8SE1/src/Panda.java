f0b01c8be490ad1740942955442bb0f726b9efd2
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:28:21 +0700

    upload unfinished work

--- self-study resources/J8SE1/C5J8SE1/src/Panda.java
+++ self-study resources/J8SE1/C5J8SE1/src/Panda.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class Panda {
+public class Panda extends Bear{
+    public static void eat() {
+        System.out.println("Panda bear is chewing");
+    }
+    public static void main(String[] args) {
+        Panda.eat();
+    }
 }
