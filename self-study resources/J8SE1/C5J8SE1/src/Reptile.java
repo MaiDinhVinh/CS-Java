c849fa4ee594bcf74a1a4b958ac454ff5471943a
DucksaberVN <business@ducksabervn.com>
2024-12-03 01:29:22 +0700

    upload unfinished work

--- self-study resources/J8SE1/C5J8SE1/src/Reptile.java
+++ self-study resources/J8SE1/C5J8SE1/src/Reptile.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Reptile {
+    protected boolean hasLegs() throws InsufficientDataException {
+        throw new InsufficientDataException();
+    }
+    protected double getWeight() throws Exception {
+        return 2;
+    }
 }
