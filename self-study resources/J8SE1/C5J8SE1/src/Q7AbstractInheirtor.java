eba961e670ece42d6f01b7bcf3ffebc616870ca0
DucksaberVN <business@ducksabervn.com>
2024-12-03 14:52:41 +0700

    1/2 chapter 5 completed

---
+++ self-study resources/J8SE1/C5J8SE1/src/Q7AbstractInheirtor.java
@@ -0,0 +1,12 @@
+public class Q7AbstractInheirtor
+        extends Q7AbstractClass{
+
+    public Q7AbstractInheirtor(){
+        super("Hello world !");
+    }
+
+    @Override
+    public void test(){
+        System.out.println("Hello from Q7");
+    }
+}
