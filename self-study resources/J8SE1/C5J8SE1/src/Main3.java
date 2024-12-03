2a75b0da1aa5dba416bc64b5f3c9e27ed044c931
DucksaberVN <business@ducksabervn.com>
2024-12-03 14:52:14 +0700

    1/2 chapter 5 completed

---
+++ self-study resources/J8SE1/C5J8SE1/src/Main3.java
@@ -0,0 +1,15 @@
+/*
+abstract class Cow {
+}
+
+class Chicken {
+    public abstract void peck();
+}
+
+abstract class Turtle {
+    public abstract void swim() {}
+    public abstract int getAge() {
+        return 10;
+    }
+}
+*/
