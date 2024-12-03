6886a1b46fa68dea42655caf67cdfa9f3f540b34
DucksaberVN <business@ducksabervn.com>
2024-12-03 14:52:32 +0700

    1/2 chapter 5 completed

---
+++ self-study resources/J8SE1/C5J8SE1/src/Q7AbstractClass.java
@@ -0,0 +1,8 @@
+public abstract class Q7AbstractClass {
+    String str = "hello world";
+    public Q7AbstractClass(String str){
+        this.str = str;
+    }
+
+    public abstract void test();
+}
