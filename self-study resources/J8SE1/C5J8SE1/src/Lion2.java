00664ca044078f571178d3afe3d0a40090fcec03
DucksaberVN <business@ducksabervn.com>
2024-12-03 14:51:50 +0700

    1/2 chapter 5 completed

---
+++ self-study resources/J8SE1/C5J8SE1/src/Lion2.java
@@ -0,0 +1,8 @@
+public class Lion2 extends BigCat {
+    public String getName() {
+        return "Lion";
+    }
+    public void roar() {
+        System.out.println("The Lion lets out a loud ROAR!");
+    }
+}
