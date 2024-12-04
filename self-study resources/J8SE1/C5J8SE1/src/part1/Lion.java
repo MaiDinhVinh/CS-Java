b186f14ffce9c35490cc3c9c0fc5961f07517bb5
DucksaberVN <business@ducksabervn.com>
2024-12-04 21:50:01 +0700

    1/2 chapter 5 presented

--- self-study resources/J8SE1/C5J8SE1/src/part1/Lion.java
+++ self-study resources/J8SE1/C5J8SE1/src/part1/Lion.java
@@ -4,7 +4,7 @@ public class Lion extends Animal{
     private void roar() {
         System.out.println("The "+getAge()+" year old lion says: Roar!");
         //what's wrong with this code ?
-        //System.out.println("The "+age+" year old lion says: Roar!");
+//        System.out.println("The "+age+" year old lion says: Roar!");
 
     }
 }
