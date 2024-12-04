a5f9058d45d6a3dd20374b2360f5ece0df5e2364
DucksaberVN <business@ducksabervn.com>
2024-12-04 21:49:42 +0700

    1/2 chapter 5 presented

--- self-study resources/J8SE1/C5J8SE1/src/part1/Elephant3.java
+++ self-study resources/J8SE1/C5J8SE1/src/part1/Elephant3.java
@@ -2,6 +2,16 @@ package part1;
 
 public class Elephant3 extends Mammal3{
     public Elephant3() {
-        super(10);
+        //super(10);
+        /*super();*/
+        this(10);
+    }
+
+    public Elephant3(int a){
+        System.out.println("a = " + a);
+    }
+
+    public static void main(String[] args) {
+        Elephant3 ele = new Elephant3();
     }
 }
