52e07029c29ddee71bcac171d5fbe52619536e27
DucksaberVN <business@ducksabervn.com>
2024-12-04 21:50:29 +0700

    1/2 chapter 5 presented

--- self-study resources/J8SE1/C5J8SE1/src/part1/Shark.java
+++ self-study resources/J8SE1/C5J8SE1/src/part1/Shark.java
@@ -19,23 +19,23 @@ public class Shark extends Fish{
     }
     public void displaySharkDetails() {
         //original code
-        /*System.out.print("Shark with age: "+getAge()); //explain this code
+        System.out.print("Shark with age: "+getAge()); //explain this code
         System.out.print(" and "+size+" meters long"); //explain this code
-        System.out.print(" with "+numberOfFins+" fins"); //explain this code*/
+        System.out.print(" with "+numberOfFins+" fins"); //explain this code
 
         //version 2
-        /*System.out.print("Shark with age: "+this.getAge()); //explain this code
+        System.out.print("Shark with age: "+this.getAge()); //explain this code
         System.out.print(" and "+this.size+" meters long"); //explain this code
-        System.out.print(" with "+this.numberOfFins+" fins"); //explain this code*/
+        System.out.print(" with "+this.numberOfFins+" fins"); //explain this code
 
         //version 3
-        /*System.out.print("Shark with age: "+super.getAge()); //explain this code
+        System.out.print("Shark with age: "+super.getAge()); //explain this code
         System.out.print(" and "+super.size+" meters long"); //explain this code
-        System.out.print(" with "+this.numberOfFins+" fins"); //explain this code*/
+        System.out.print(" with "+this.numberOfFins+" fins"); //explain this code
 
         //version 4
-        /*System.out.print("Shark with age: "+super.getAge()); //explain this code
+        System.out.print("Shark with age: "+super.getAge()); //explain this code
         System.out.print(" and "+super.size+" meters long"); //explain this code
-        System.out.print(" with "+super.numberOfFins+" fins"); //explain this code*/
+        //System.out.print(" with "+super.numberOfFins+" fins"); //explain this code
     }
 }
