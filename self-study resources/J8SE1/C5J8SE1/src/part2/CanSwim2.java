180c12874d3ac4db6839d7e3c8f0db0ac082cd64
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:48:49 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part2/CanSwim2.java
@@ -0,0 +1,7 @@
+package part2;
+
+public interface CanSwim2 {
+    public static final int MAXIMUM_DEPTH = 100;
+    public static final boolean UNDERWATER = true;
+    public static final String TYPE = "Submersible";
+}
