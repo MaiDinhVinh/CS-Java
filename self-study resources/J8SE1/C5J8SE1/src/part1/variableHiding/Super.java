c27091d670882f1d3596e740ec938d3ce6d03edb
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:47:20 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part1/variableHiding/Super.java
@@ -0,0 +1,13 @@
+package part1.variableHiding;
+
+public class Super {
+    public String str = "Super";
+
+    public static void main(String[] args) {
+        /*Super sup = new Super();
+        System.out.println(sup.str);*/
+
+        /*Sub sb = new Sub();
+        System.out.println(sb.str);*/
+    }
+}
