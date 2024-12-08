6cd42cceb8f50e65e803d5e953e000a483992514
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:48:03 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part2/Bear3.java
@@ -0,0 +1,13 @@
+package part2;
+
+/*public class Bear3 implements Herbivore4, Omnivore3{
+    @Override
+    public int eatPlants() {
+        System.out.println("Eating plants: 10");
+        return 10;
+    }
+    @Override
+    public void eatPlants() {
+        System.out.println("Eating plants");
+    }
+}*/
