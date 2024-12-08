a57c61b9c1680eb88a222cf52d193965f833437b
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:48:57 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part2/Carnivore.java
@@ -0,0 +1,8 @@
+package part2;
+
+public interface Carnivore {
+    /*public default void eatMeat(); // DOES NOT COMPILE
+    public int getRequiredFoodAmount() { // DOES NOT COMPILE
+        return 13;
+    }*/
+}
