dbd6a4ac80a1bede8bbd3c595aa1f4ea2f3e3d1c
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:46:25 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part1/overridingVShiding/Sub.java
@@ -0,0 +1,16 @@
+package part1.overridingVShiding;
+
+public class Sub extends Super{
+    @Override
+    public void method1(){
+        System.out.println("From Sub");
+    }
+
+    public static void main(String[] args) {
+        /*Sub sb = new Sub();
+        sb.method1();*/
+
+        /*Super sup = new Sub();
+        sup.method1();*/
+    }
+}
