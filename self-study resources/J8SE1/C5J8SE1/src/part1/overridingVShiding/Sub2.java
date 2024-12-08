8719aba9e67c2efb5bf7a29134fa33226cf05d9f
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:46:31 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part1/overridingVShiding/Sub2.java
@@ -0,0 +1,11 @@
+package part1.overridingVShiding;
+
+public class Sub2 extends Super2{
+    public static void method2(){
+        System.out.println("From Sub2");
+    }
+
+    public static void main(String[] args) {
+        method2();
+    }
+}
