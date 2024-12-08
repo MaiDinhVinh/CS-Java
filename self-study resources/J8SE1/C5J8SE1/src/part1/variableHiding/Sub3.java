7172dd6e5d7eb5ea9485113c1290999a451c8a33
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:47:07 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part1/variableHiding/Sub3.java
@@ -0,0 +1,18 @@
+package part1.variableHiding;
+
+public class Sub3 extends Super3{
+    public String str = "Sub3";
+
+    @Override
+    public void getStr(){
+        System.out.println(this.str);
+    }
+
+    public static void main(String[] args) {
+        /*Sub3 su3 = new Sub3();
+        su3.getStr();*/
+
+        /*Super3 sup3 = new Sub3();
+        sup3.getStr();*/
+    }
+}
