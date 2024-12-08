5bb0a77b770be890c434e305decd9956ef0d7f55
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:46:13 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/Main.java
@@ -0,0 +1,125 @@
+public class Main {
+    public static void main(String[] args) {
+        //snippet 1 - check Animal and Lion class
+
+        //snippet 2 - check Groundhog class
+
+        //snippet 3 - check Main2 class
+
+        //snippet 4 - check Animal2 and Zebra class
+
+        //snippet 5 - check Zoo class
+
+        //snippet 6 - check Animal3 and Gorilla class
+
+        //snippet 7 - check Mammal and Elephant class
+
+        //snippet 8 - check Mammal2 and Elephant2 class
+
+        //snippet 9 - check Mammal3 and Elephant3 class
+
+        //snippet 10 - check Chimpanzee class
+
+        //snippet 11 - check Shark class
+
+        //snippet 12 - check Shark class (2nd code)
+
+        //snippet 13 - check Shark class (3rd code)
+
+        //snippet 14 - check Shark class (4th code)
+
+        //snippet 15 - check Rabbit class
+
+        //snippet 16 - check Canine and Wolf class
+
+        //snippet 17 - check Camel and BactrianCamel class
+
+        //snippet 18 - check Insuff..., Reptile and Snake class
+
+        //snippet 19 - check Reptile2, Snake2 class
+
+        //snippet 20 - Check Camel2, BactrianCamel2 class
+
+        //snippet 21 - Check Bear, Panda class
+
+        //snippet 22 - Check Bear2, Panda2 class
+
+        //snippet 23 - check Bird, Penguin class
+
+        //snippet 24 - check Animal4, Swan class
+
+        //snippet 25 - check Main3 class
+
+        //snippet 26 - check Main4 class
+
+        //snippet 27 - also check Main4 class
+
+        //snippet 28 - check Whale, HumpbackWhale class
+
+        //snippet 29 - check Whale2, HumpbackWhale2 class
+
+        //snippet 30 - check Animal5, Walrus class
+
+        //snippet 31 - check Animal6, Bird2, Flamingo class
+
+        //snippet 32 - check Aminal7, Walrus2, Eagle class
+
+        //snippet 33 - check Animal8, BigCat, Lion2 class
+
+        //snippet 34 - check Animal9, BigCat2, Lion3
+
+        //snippet 35 - (check overriding... package) check Sub, Super classes
+
+        //snippet 36 - (check overriding... package) check Sub2, Super2 classes
+
+        //snippet 37 - (check overriding... package) check Sub3, Super3 classes
+
+        //snippet 38 - (check variable... package) check Sub, Super classes
+
+        //snippet 39 - (check variable... package) check Sub2, Super2 classes
+
+        //snippet 40 - (check variable... package) check Sub3, Super3 classes
+
+        ////////////////////////////////////////////
+        //            END OF PART 1               //
+        ////////////////////////////////////////////
+
+        /*! PART 2 STARTS IN "part2" PACKAGE !*/
+
+        //snippet 41 - check WalksOnFourLegs, HasTrunk, Herbivore interfaces and Elephant class
+
+        //snippet 42 - check Main2
+
+        //snippet 43 - check WalksOnFourLegs2 interface
+
+        //snippet 44 - check CanFly interface
+
+        //snippet 45 - check CanFly2 interface
+
+        //snippet 46 - check CanCrawl interface
+
+        //snippet 47 - check HasTail, HasWhiskers, Seal interfaces
+
+        //snippet 48 - check HasTail2, HasWhiskers2 interface,s Harbor, LeopardSeal classes
+
+        //snippet 49 - check CanRun, HasFur interfaces, Cheetah, Hyena classes
+
+        //snipept 50 - check Herbivore2, Omnivore interfaces, Bear class
+
+        //snippet 51 - check Herbivore3, Omnivore2 interfaces, Bear2 class
+
+        //snippet 52 - check Herbivore4, Omnivore3 interfaces, Bear3 class
+
+        //snippet 53 - check Herbivore5, Omnivore4, Supervore interfaces, AbstractBear class
+
+        //snippet 54 - check CanSwim interface
+
+        //snippet 55 - check CanSwim2 interface
+
+        //snippet 56 - check CanDig interface
+
+        //snippet 57 - check IsWarmBlooded interface
+
+        //snippet 59 - check Carnivore interface
+    }
+}
