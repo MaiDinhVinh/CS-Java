dac63f3820188c49ca9ac852c74253ca3b0800ac
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:47:25 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part1/variableHiding/Super2.java
@@ -0,0 +1,14 @@
+package part1.variableHiding;
+
+public class Super2 {
+    public String str = "Super2";
+    public static String staticStr = "static Super2";
+
+    public static void main(String[] args) {
+        /*Super2 sup2 = new Sub2();
+        System.out.println(sup2.str);
+        System.out.println(sup2.staticStr);*/
+
+        /*System.out.println(Super2.staticStr);*/
+    }
+}
