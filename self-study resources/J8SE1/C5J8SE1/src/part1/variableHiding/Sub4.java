7afe714e7f23f2fbc20349fa3012cbaff4ed80ba
DucksaberVN <business@ducksabervn.com>
2024-12-08 15:47:12 +0700

    chapter 5 (part2) finished

---
+++ self-study resources/J8SE1/C5J8SE1/src/part1/variableHiding/Sub4.java
@@ -0,0 +1,13 @@
+package part1.variableHiding;
+
+public class Sub4 extends Super4{
+    public String str = "Sub4";
+
+    public static void main(String[] args) {
+        /*Sub4 su4 = new Sub4();
+        su4.getStr();*/
+
+        /*Super4 sup4 = new Sub4();
+        sup4.getStr();*/
+    }
+}
