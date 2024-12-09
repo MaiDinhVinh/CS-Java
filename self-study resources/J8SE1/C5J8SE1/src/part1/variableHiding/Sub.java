627f34e15c254f7973035702072b4b5e797e52dc
DucksaberVN <business@ducksabervn.com>
2024-12-09 09:08:39 +0700

    commit changes

--- self-study resources/J8SE1/C5J8SE1/src/part1/variableHiding/Sub.java
+++ self-study resources/J8SE1/C5J8SE1/src/part1/variableHiding/Sub.java
@@ -4,10 +4,10 @@ public class Sub extends Super{
     public String str = "Sub";
 
     public static void main(String[] args) {
-        /*Sub sb = new Sub();
-        System.out.println(sb.str);*/
+        Sub sb = new Sub();
+        System.out.println(sb.str);
 
-        /*Super sup = new Super();
-        System.out.println(sup.str);*/
+        Super sup = new Super();
+        System.out.println(sup.str);
     }
 }
