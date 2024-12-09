25f655d39ca91fd0ea3500d8654baace5bece0c0
DucksaberVN <business@ducksabervn.com>
2024-12-09 09:08:07 +0700

    commit changes

--- self-study resources/J8SE1/C5J8SE1/src/part1/HumpbackWhale2.java
+++ self-study resources/J8SE1/C5J8SE1/src/part1/HumpbackWhale2.java
@@ -1,5 +1,6 @@
-/*
-public class HumpbackWhale2 extends Whale2{
+package part1;
+
+/*public class HumpbackWhale2 extends Whale2{
     private void sing() {
         System.out.println("Humpback whale is singing");
     }
