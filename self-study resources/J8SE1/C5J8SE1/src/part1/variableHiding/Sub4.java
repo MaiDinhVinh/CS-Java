6c2c251e5937ea069ad49a8aea9c413cfe956110
DucksaberVN <business@ducksabervn.com>
2024-12-09 09:09:05 +0700

    commit changes

--- self-study resources/J8SE1/C5J8SE1/src/part1/variableHiding/Sub4.java
+++ self-study resources/J8SE1/C5J8SE1/src/part1/variableHiding/Sub4.java
@@ -4,10 +4,10 @@ public class Sub4 extends Super4{
     public String str = "Sub4";
 
     public static void main(String[] args) {
-        /*Sub4 su4 = new Sub4();
-        su4.getStr();*/
+        Sub4 su4 = new Sub4();
+        su4.getStr();
 
-        /*Super4 sup4 = new Sub4();
-        sup4.getStr();*/
+        Super4 sup4 = new Sub4();
+        sup4.getStr();
     }
 }
