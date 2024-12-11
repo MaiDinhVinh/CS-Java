21fe6a4d1c98c831c9ac43d99be60ad5a7a21796
DucksaberVN <business@ducksabervn.com>
2024-12-11 19:25:09 +0700

    chapter 5 finished (finally)

---
+++ self-study resources/J8SE1/C5J8SE1/src/part3/Bird2.java
@@ -0,0 +1,10 @@
+package part3;
+
+public class Bird2 {
+    public String getName() {
+        return "Unknown";
+    }
+    public void displayInformation() {
+        System.out.println("The bird name is: "+getName());
+    }
+}
