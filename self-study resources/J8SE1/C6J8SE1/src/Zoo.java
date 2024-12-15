c44747f1013bf5eb894b8dc944fc7ef4a3013390
DucksaberVN <business@ducksabervn.com>
2024-12-15 14:48:04 +0700

    FINAL OCA COMMIT

---
+++ self-study resources/J8SE1/C6J8SE1/src/Zoo.java
@@ -0,0 +1,9 @@
+public class Zoo {
+    /**
+     * compile using {@code javac} and run it with {@code Zoo} argument
+     * and observe what happen ? (also explain what does it print)*/
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
