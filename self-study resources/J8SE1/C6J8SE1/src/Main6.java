8885a2d4a13e8cc099bc171682f0916169382ebe
DucksaberVN <business@ducksabervn.com>
2024-12-15 14:47:34 +0700

    FINAL OCA COMMIT

---
+++ self-study resources/J8SE1/C6J8SE1/src/Main6.java
@@ -0,0 +1,14 @@
+/*
+public class Main6{
+    public void bad() {
+        try {
+            eatCarrot();
+        } catch (NoMoreCarrotsException e ) {
+            System.out.print("sad rabbit");
+        }
+    }
+    public void good() throws NoMoreCarrotsException {
+        eatCarrot();
+    }
+    private static void eatCarrot() { }
+}*/
