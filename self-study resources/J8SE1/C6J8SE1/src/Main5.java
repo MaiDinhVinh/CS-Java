f62d939509812cc960ab24c960d0e37cdf4eb05d
DucksaberVN <business@ducksabervn.com>
2024-12-15 14:47:25 +0700

    FINAL OCA COMMIT

---
+++ self-study resources/J8SE1/C6J8SE1/src/Main5.java
@@ -0,0 +1,23 @@
+class NoMoreCarrotsException extends Exception {}
+class Bunny {
+    /*public static void main(String[] args) {
+        eatCarrot();
+    }*/
+
+    /*public static void main(String[] args)
+            throws NoMoreCarrotsException {
+        eatCarrot();
+    }*/
+
+    /*public static void main(String[] args) {
+        try {
+            eatCarrot();
+        } catch (NoMoreCarrotsException e ) {
+            System.out.print("sad rabbit");
+        }
+    }*/
+
+
+    private static void eatCarrot() throws NoMoreCarrotsException {
+    }
+}
