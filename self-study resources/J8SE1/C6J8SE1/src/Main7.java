a56a09796fdb85d092b9050e0270919e3847d453
DucksaberVN <business@ducksabervn.com>
2024-12-15 14:47:41 +0700

    FINAL OCA COMMIT

---
+++ self-study resources/J8SE1/C6J8SE1/src/Main7.java
@@ -0,0 +1,8 @@
+/*
+class CanNotHopException extends Exception { }
+class Hopper {
+    public void hop() { }
+}
+class Bunny extends Hopper {
+    public void hop() throws CanNotHopException { }
+}*/
