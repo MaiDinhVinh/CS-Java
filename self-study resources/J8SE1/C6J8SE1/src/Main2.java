3c86a1676f8b8996123e5540e1efd046d154a8b5
DucksaberVN <business@ducksabervn.com>
2024-12-15 18:25:20 +0700

    OCA Book Presentation completed

--- self-study resources/J8SE1/C6J8SE1/src/Main2.java
+++ self-study resources/J8SE1/C6J8SE1/src/Main2.java
@@ -2,15 +2,15 @@ class AnimalsOutForAWalk extends RuntimeException { }
 class ExhibitClosed extends RuntimeException { }
 class ExhibitClosedForLunch extends ExhibitClosed { }
 public class Main2{
-    /*public void visitPorcupine() {
-        try {
+    public void visitPorcupine() {
+        /*try {
             //seeAnimal();
         } catch (AnimalsOutForAWalk e) {
             System.out.print("try back later");
         } catch (ExhibitClosed e) {
             System.out.print("not today");
-        }
-    }*/
+        }*/
+    }
 
     /*public void visitMonkeys() {
         try {
