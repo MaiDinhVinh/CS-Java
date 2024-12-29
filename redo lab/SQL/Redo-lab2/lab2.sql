b2967797e772219f1b907c418f6cc633b52914a4
DucksaberVN <business@ducksabervn.com>
2024-12-29 22:59:15 +0700

    redo sql lab 2

---
+++ redo lab/SQL/Redo-lab2/lab2.sql
@@ -0,0 +1,43 @@
+--Restore NORTHWIND database
+
+--ex: find all information of Orders table
+
+--ex: find orderID, customerID, employeeID of Orders table
+
+--ex: find ShipCity, ShipAddress of Orders table
+
+--ex: concat ShipCity and ShipAddress as 1 column (eg. Adenauerallee 900-Stuttgart)
+
+--ex: find CompanyName and ContactName of Customers table
+
+--ex: find CustomerID, CompanyName as 'Customer Name' and ContactName as 'The Person' of Customers table
+
+--ex: find all Freight of Orders after 15% tax applied, named as 'Freight after 15% tax'
+
+--ex: find all distinct customerID of Orders table
+
+
+--ex: select all information of Orders table (except Freight + 20% tax) into new table OrdersWithTax
+
+
+--ex: find all Orders which have Freight greater than 60
+
+--ex: find all Orders which have Freight less than 10
+
+--ex: find all Orders which have Freight between 40 and 50
+
+--ex: find all customers from Germany
+
+--ex: find all customers whose contactName is %Paul%
+
+--ex: find all Mexico customers whose ContactTitle is Owner
+
+--ex: find all Orders such that OrderDate in 1997 and employeeID = 4
+
+--ex: find all Orders such that ShippedDate in November-1997 and Freight between 10 and 30
+
+--ex: find maximum freight in Orders in which EmployeeId = 3
+
+--ex: find how many Orders placed by customerID 'EASTC'
+
+--ex: find how many Orders placed by customerID 'EASTC' and orderDate in 1998
