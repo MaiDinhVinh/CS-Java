1de582849ebf3a6924c69c6e7879dd25d941a542
DucksaberVN <business@ducksabervn.com>
2024-12-29 15:51:04 +0700

    update github profile

---
+++ github profile java version/READMEJava/src/DucksaberVN.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+public interface DucksaberVN{
+
+    public static final String WELCOMING_SLOGAN = "Welcome to the ricefield (VN) 👋";
+
+    public static final String GOVERNMENT_NAME = "MaiDinhVinh";
+
+    public static final String NICKNAME = "😎 Nickname: DucksaberVN (Formerlly known as \"VinhDev\")";
+
+    public static final String WHO_AM_I = "☠ A typical highschool student";
+
+    public static final String FAVORITE_SUBJECT = "Physic hater, CS lover, Mathematic enjoyer";
+
+    /**
+     * I will try to obtain the famous {@code Oracle Certified Professional Java SE 8 (1z0-809)} in the future too !*/
+
+    public static final List<String> CURRENT_CERTIFIED_SKILL =  Arrays.asList(
+            "Oracle Certified Foundation Associate Java SE 8 (1z0-811)",
+            "Oracle Certified Associate Java SE 8 (1z0-808)"
+    );
+
+    public static final List<String> CURRENT_SKILL = Arrays.asList(
+            "Java SE 8", "MySQL (a bit)", "Python (Mainly for AI stuff)"
+    );
+
+    public static final List<String> CURRENT_PROJECT = Arrays.asList(
+            "TinyTensor(1st Season) as Hanoi Area Director"
+    );
+
+
+    public static final List<String> FAVORITE_GAME = Arrays.asList(
+      "Minecraft", "Geometry Dash", "CS2 (a bit)", "Smash Hit",
+      "A dance of ice and fire"
+    );
+
+    public static final String FAVORITE_SPORT = "Retired Badminton Defender (Arcsaber 11 Pro - Halbertec 8000)";
+
+    public static final String CONTACT_EMAIL = "business@ducksabervn.com";
+
+    public static final String PERSONAL_WEBSITE = "ducksabervn.com OR vinhdev.vn";
+}
+
