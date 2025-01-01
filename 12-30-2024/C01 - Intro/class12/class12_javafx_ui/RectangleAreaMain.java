8d730121897e8680c24e867e6bf45a80fde032a5
DucksaberVN <business@ducksabervn.com>
2025-01-02 02:02:02 +0700

    upload JavaFX_UI c01 lab files

---
+++ 12-30-2024/C01 - Intro/class12/class12_javafx_ui/RectangleAreaMain.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class12_javafx_ui;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Administrator
+ */
+public class RectangleAreaMain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        GridPane root = FXMLLoader.load(getClass().getResource("RectangleAreaUI.fxml"));
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Rectangle Area");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
