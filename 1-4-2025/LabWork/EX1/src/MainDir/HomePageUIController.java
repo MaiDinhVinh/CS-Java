6b8d7ca0ebf26e7521b1cb43fcfa566eb84c92c2
DucksaberVN <business@ducksabervn.com>
2025-01-05 23:41:27 +0700

    lab3 completed

---
+++ 1-4-2025/LabWork/EX1/src/MainDir/HomePageUIController.java
@@ -0,0 +1,31 @@
+package MainDir;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class HomePageUIController {
+
+    @FXML
+    void btnAboutClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToAbout();
+    }
+
+    @FXML
+    void btnBlogClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToBlog();
+    }
+
+    @FXML
+    void btnContactClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToContact();
+    }
+
+    @FXML
+    void btnHomeClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToHome();
+    }
+
+}
