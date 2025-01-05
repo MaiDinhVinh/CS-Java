5df5275155ac220061698c85922ca66d3ae6ffd3
DucksaberVN <business@ducksabervn.com>
2025-01-05 23:41:12 +0700

    lab3 completed

---
+++ 1-4-2025/LabWork/EX1/src/MainDir/ContactPageUIController.java
@@ -0,0 +1,30 @@
+package MainDir;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class ContactPageUIController {
+
+    @FXML
+    void btnAboutClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToAbout();
+    }
+
+    @FXML
+    void btnBlogClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToBlog();
+    }
+
+    @FXML
+    void btnContactClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToContact();
+    }
+
+    @FXML
+    void btnHomeClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToHome();
+    }
+}
