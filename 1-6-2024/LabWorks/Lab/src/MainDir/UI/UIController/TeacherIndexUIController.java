4185a65d3f24c7eb46108b617306d2ec7ecee691
DucksaberVN <business@ducksabervn.com>
2025-01-08 01:48:25 +0700

    update javafx_crud lab

--- 1-6-2024/LabWorks/Lab/src/MainDir/UI/UIController/TeacherIndexUIController.java
+++ 1-6-2024/LabWorks/Lab/src/MainDir/UI/UIController/TeacherIndexUIController.java
@@ -1,13 +1,35 @@
 package MainDir.UI.UIController;
 
 
+import MainDir.Beans.Teacher.Teacher;
 import MainDir.UI.fxmlAndMain.Navigator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TeacherIndexUIController {
+public class TeacherIndexUIController implements Initializable {
+
+    @FXML
+    private TableView<Teacher> teacherTv;
+
+    @FXML
+    private TableColumn<Teacher, Integer> teacherIDCol;
+
+    @FXML
+    private TableColumn<Teacher, String> teacherNameCol;
+
+    @FXML
+    private TableColumn<Teacher, String> teacherDOBCol;
+
+    @FXML
+    private TableColumn<Teacher, String> teacherSSIDCol;
+
 
     @FXML
     void btnBackToHomeClick(ActionEvent event) throws IOException {
@@ -24,4 +46,24 @@ public class TeacherIndexUIController {
         Navigator.getInstance().goToUpdateTeacher();
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        teacherTv.setItems(Teacher.selectAll());
+
+        teacherIDCol.setCellValueFactory((teacher) -> {
+            return teacher.getValue().getTeacherIdProperty();
+        });
+
+        teacherNameCol.setCellValueFactory((teacher) -> {
+            return teacher.getValue().getTeacherNameProperty();
+        });
+
+        teacherDOBCol.setCellValueFactory((teacher) -> {
+            return teacher.getValue().getTeacherDOBProperty();
+        });
+
+        teacherSSIDCol.setCellValueFactory((teacher) -> {
+            return teacher.getValue().getTeacherSSIDProperty();
+        });
+    }
 }
