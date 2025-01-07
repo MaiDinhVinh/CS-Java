9d1eaaec1015630fe1f471ff6dbedfe4a50410d5
DucksaberVN <business@ducksabervn.com>
2025-01-08 01:47:56 +0700

    update javafx_crud lab

--- 1-6-2024/LabWorks/Lab/src/MainDir/dbservice/MySQLService.java
+++ 1-6-2024/LabWorks/Lab/src/MainDir/dbservice/MySQLService.java
@@ -1,5 +1,15 @@
 package MainDir.dbservice;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLService {
-    private static final
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "123456789";
+    private static final String CONN_STRING = "jdbc:mysql://localhost/LabCurd1";
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+    }
 }
