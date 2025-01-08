e79699643d23b6608f8363b7444f6e23a3846d01
DucksaberVN <business@ducksabervn.com>
2025-01-09 00:38:11 +0700

    largest lab ever done tho
    i think some ppl who is faking their CS profile to get into US university should go kys (u useless shi). What you are doing is absolutely dumb and meaningless, YOU SHOULD KYS NOW 🗣️🔥🔥🔥🔥🖕
    
    sincerely
    
    (P/S: you guys dont know how hard can it be to become a real programmer tho)

--- 1-6-2025/LabWorks/Lab/src/MainDir/UI/UIController/UpdateTeacherUIController.java
+++ 1-6-2025/LabWorks/Lab/src/MainDir/UI/UIController/UpdateTeacherUIController.java
@@ -6,14 +6,12 @@ import MainDir.UI.fxmlAndMain.Navigator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class UpdateTeacherUIController implements Initializable {
@@ -32,6 +30,12 @@ public class UpdateTeacherUIController implements Initializable {
 
     private static Teacher updateTeacher;
 
+    private static String oldTeacherName;
+
+    private static LocalDate oldTeacherDOB;
+
+    private static String oldTeacherSSID;
+
     @FXML
     void btnBackToIndexClick(ActionEvent event) throws IOException {
         Navigator.getInstance().goToTeacherIndex();
@@ -44,6 +48,10 @@ public class UpdateTeacherUIController implements Initializable {
 
     public void initialize(Teacher selectedTeacher){
         UpdateTeacherUIController.updateTeacher = selectedTeacher;
+        UpdateTeacherUIController.oldTeacherName = UpdateTeacherUIController.updateTeacher.getTeacherName();
+        UpdateTeacherUIController.oldTeacherDOB = LocalDate.parse(
+                UpdateTeacherUIController.updateTeacher.getTeacherDOB());
+        UpdateTeacherUIController.oldTeacherSSID = UpdateTeacherUIController.updateTeacher.getTeacherSSID();
         txtTeacherName.setText(selectedTeacher.getTeacherName());
         dateDOBTeacher.setValue(LocalDate.parse(selectedTeacher.getTeacherDOB()));
         txtTeacherSSID.setText(selectedTeacher.getTeacherSSID());
@@ -74,36 +82,44 @@ public class UpdateTeacherUIController implements Initializable {
             boolean invalidSSID = false;
             if (txtTeacherSSID.getText().equals(""))
                 errorMessage.append("Teacher SSID cannot be empty !\n");
-            else{
-                switch(txtTeacherSSID.getText().substring(0, 2)){
-                    case "00":
-                    case "01":
-                    case "10":
-                    case "11":
-                        tempSSID.append(txtTeacherSSID.getText().substring(0, 2));
-                        break;
-                    default:
-                        invalidSSID = true;
+            else {
+                if(dateDOBTeacher.getValue() != null) {
+                    if(txtTeacherSSID.getText().length() == 11){
+                        switch (txtTeacherSSID.getText().substring(0, 2)) {
+                            case "00":
+                            case "01":
+                            case "10":
+                            case "11":
+                                tempSSID.append(txtTeacherSSID.getText().substring(0, 2));
+                                break;
+                            default:
+                                invalidSSID = true;
+                        }
+                        if (Integer.valueOf(
+                                Integer.parseInt(txtTeacherSSID.getText().substring(2, 4))
+                        ) != (dateDOBTeacher.getValue().getYear() % 100))
+                            invalidSSID = true;
+                        else
+                            tempSSID.append(txtTeacherSSID.getText().substring(2, 4));
+                        if (Integer.valueOf(
+                                Integer.parseInt(txtTeacherSSID.getText().substring(4, 6))
+                        ) != (dateDOBTeacher.getValue().getMonthValue()))
+                            invalidSSID = true;
+                        else
+                            tempSSID.append(txtTeacherSSID.getText().substring(4, 6));
+                        if (!invalidSSID)
+                            UpdateTeacherUIController.updateTeacher.setTeacherSSID(tempSSID.toString() +
+                                    txtTeacherSSID.getText().substring(6));
+                        else
+                            errorMessage.append("Invalid teacher SSID !\n");
+                    }else errorMessage.append("SSID must consist of 11 digits !\n");
+                }else{
+                    if(txtTeacherSSID.getText().length() > 11
+                            || txtTeacherSSID.getText().length() < 11)
+                        errorMessage.append("SSID must consist of 11 digits !\n");
+                    else errorMessage.append("Invalid teacher SSID !\n"); //already invalid since DOB is missing tho
                 }
             }
-
-            if(Integer.valueOf(
-                    Integer.parseInt(txtTeacherSSID.getText().substring(2, 4))
-            ) != (dateDOBTeacher.getValue().getYear()%100))
-                invalidSSID = true;
-            else
-                tempSSID.append(txtTeacherSSID.getText().substring(2, 4));
-            if(Integer.valueOf(
-                    Integer.parseInt(txtTeacherSSID.getText().substring(4, 6))
-            ) != (dateDOBTeacher.getValue().getMonthValue()))
-                invalidSSID = true;
-            else
-                tempSSID.append(txtTeacherSSID.getText().substring(4, 6));
-            if(!invalidSSID)
-                UpdateTeacherUIController.updateTeacher.setTeacherSSID(tempSSID.toString() +
-                        txtTeacherSSID.getText().substring(6));
-            else
-                errorMessage.append("Invalid teacher SSID !\n");
         }
         final String ERROR_MESSAGE = errorMessage.toString();
         if(ERROR_MESSAGE.equals("")){
@@ -111,8 +127,8 @@ public class UpdateTeacherUIController implements Initializable {
                 boolean isUpdated = Teacher.update(UpdateTeacherUIController.updateTeacher);
                 if(isUpdated){
                     Alert insertModSuccess = new Alert(Alert.AlertType.INFORMATION);
-                    insertModSuccess.setTitle("Insert new teacher completed");
-                    insertModSuccess.setHeaderText("Insert new teacher completed successfully");
+                    insertModSuccess.setTitle("Update new teacher completed");
+                    insertModSuccess.setHeaderText("Update new teacher completed successfully");
                     insertModSuccess.show();
                     msgLabel.setText("Message: Teacher is successfully updated !");
                 }
@@ -121,11 +137,26 @@ public class UpdateTeacherUIController implements Initializable {
             }
         }else{
             Alert insertModFailed = new Alert(Alert.AlertType.ERROR);
-            insertModFailed.setTitle("Insert new teacher failed");
-            insertModFailed.setHeaderText("Insert new teacher failed due to these following reason: \n"
+            insertModFailed.setTitle("Update new teacher failed");
+            insertModFailed.setHeaderText("Update new teacher failed due to these following reason: \n"
                     + ERROR_MESSAGE);
             insertModFailed.show();
             msgLabel.setText("Message: Error encountered !, please try again");
+        }
+    }
+
+    @FXML
+    void btnResetClick(ActionEvent event) {
+        Alert resetAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        resetAlert.setTitle("Reset confirmation");
+        resetAlert.setHeaderText("Are you sure you want to reset all ?");
+
+        Optional<ButtonType> confirmationResponse = resetAlert.showAndWait();
+        if(confirmationResponse.get() == ButtonType.OK){
+            txtTeacherName.setText(UpdateTeacherUIController.oldTeacherName);
+            dateDOBTeacher.setValue(UpdateTeacherUIController.oldTeacherDOB);
+            txtTeacherSSID.setText(UpdateTeacherUIController.oldTeacherSSID);
+            msgLabel.setText("Message: Resetted !");
         }
     }
 }
