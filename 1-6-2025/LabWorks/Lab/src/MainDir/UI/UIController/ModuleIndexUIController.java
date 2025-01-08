8435dbb26cce545a79878d4d1529a1707c323a60
DucksaberVN <business@ducksabervn.com>
2025-01-08 19:35:09 +0700

    demo javafx_crud completed

--- 1-6-2025/LabWorks/Lab/src/MainDir/UI/UIController/ModuleIndexUIController.java
+++ 1-6-2025/LabWorks/Lab/src/MainDir/UI/UIController/ModuleIndexUIController.java
@@ -6,12 +6,15 @@ import MainDir.UI.fxmlAndMain.Navigator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Optional;
 
 
 
@@ -44,7 +47,37 @@ public class ModuleIndexUIController implements Initializable {
 
     @FXML
     void btnUpdateModClick(ActionEvent event) throws IOException {
-        Navigator.getInstance().goToUpdateModule();
+        Module selectedMod = modTv.getSelectionModel().getSelectedItem();
+        Navigator.getInstance().goToUpdateModule(selectedMod);
+    }
+
+    @FXML
+    void btnDelModClick(ActionEvent event) {
+        Module selectedMod = modTv.getSelectionModel().getSelectedItem();
+
+        boolean isNull = selectedMod == null ? true : false;
+
+        if(isNull){
+            Alert deleteRowErr = new Alert(Alert.AlertType.ERROR);
+            deleteRowErr.setTitle("Delete module unsucessfully !");
+            deleteRowErr.setHeaderText("No book is selected !, please select a book to delete");
+            deleteRowErr.show();
+        }else{
+            Alert deleteRowConfirmation = new Alert(Alert.AlertType.CONFIRMATION);
+            deleteRowConfirmation.setTitle("Delete module confirmation");
+            deleteRowConfirmation.setHeaderText("Are you sure you want to delete this book ?");
+
+            Optional<ButtonType> confirmationResponse = deleteRowConfirmation.showAndWait();
+            if(confirmationResponse.get() == ButtonType.OK){
+                boolean isDeleted = Module.delete(selectedMod) ? true : false;
+                if(isDeleted){
+                    modTv.getItems().remove(selectedMod);
+                    System.out.println("module deleted");
+                }else{
+                    System.out.println("module deleted unsucessfully !");
+                }
+            }
+        }
     }
 
     @Override
