8e90777aba600713381f4254b8194fb8f54781ff
DucksaberVN <business@ducksabervn.com>
2025-01-12 02:02:08 +0700

    final javafx lab completed

---
+++ 1-10-2025/LabWork/EX/src/MainDir/UI/UIController/TeacherIndexUIController.java
@@ -0,0 +1,107 @@
+package MainDir.UI.UIController;
+
+import MainDir.Beans.Module.Module;
+import MainDir.Beans.Teacher.Teacher;
+import MainDir.UI.fxmlAndMain.Navigator;
+import MainDir.i18n_translation_handler.Translation;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class TeacherIndexUIController implements Initializable {
+
+    @FXML
+    private TableView<Teacher> teacherTv;
+
+    @FXML
+    private TableColumn<Teacher, Integer> teacherIDCol;
+
+    @FXML
+    private TableColumn<Teacher, String> teacherNameCol;
+
+    @FXML
+    private TableColumn<Teacher, String> teacherDOBCol;
+
+    @FXML
+    private TableColumn<Teacher, String> teacherSSIDCol;
+
+    @FXML
+    void btnBackToHomeClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToHome();
+    }
+
+    @FXML
+    void btnInsertTeacherClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToInsertTeacher();
+    }
+
+    @FXML
+    void btnUpdateTeacherClick(ActionEvent event) throws IOException {
+        Teacher selectedTeacher = teacherTv.getSelectionModel().getSelectedItem();
+        if(selectedTeacher == null){
+            Alert noSelectedTeacher = new Alert(Alert.AlertType.ERROR);
+            noSelectedTeacher.setTitle(Translation.getTranslator().getString("index.alert.teacher.update.error.title"));
+            noSelectedTeacher.setHeaderText(Translation.getTranslator().getString("index.alert.teacher.update.error.header"));
+            noSelectedTeacher.show();
+        }else Navigator.getInstance().goToUpdateTeacher(selectedTeacher);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        teacherTv.setItems(Teacher.selectAll());
+
+        teacherIDCol.setCellValueFactory((teacher) -> {
+            return teacher.getValue().getTeacherIdProperty();
+        });
+
+        teacherNameCol.setCellValueFactory((teacher) -> {
+            return teacher.getValue().getTeacherNameProperty();
+        });
+
+        teacherDOBCol.setCellValueFactory((teacher) -> {
+            return teacher.getValue().getTeacherDOBProperty();
+        });
+
+        teacherSSIDCol.setCellValueFactory((teacher) -> {
+            return teacher.getValue().getTeacherSSIDProperty();
+        });
+    }
+
+    @FXML
+    void btnTeacherDeleteClick(ActionEvent event) {
+        Teacher selectedTeacher = teacherTv.getSelectionModel().getSelectedItem();
+
+        boolean isNull = selectedTeacher == null ? true : false;
+
+        if(isNull){
+            Alert deleteRowErr = new Alert(Alert.AlertType.ERROR);
+            deleteRowErr.setTitle(Translation.getTranslator().getString("alert.teacher.delete.error.title"));
+            deleteRowErr.setHeaderText(Translation.getTranslator().getString("alert.teacher.delete.error.header"));
+            deleteRowErr.show();
+        }else{
+            Alert deleteRowConfirmation = new Alert(Alert.AlertType.CONFIRMATION);
+            deleteRowConfirmation.setTitle(Translation.getTranslator().getString("alert.teacher.delete.confirm.title"));
+            deleteRowConfirmation.setHeaderText(Translation.getTranslator().getString("alert.teacher.delete.confirm.header"));
+
+            Optional<ButtonType> confirmationResponse = deleteRowConfirmation.showAndWait();
+            if(confirmationResponse.get() == ButtonType.OK){
+                boolean isDeleted = Teacher.delete(selectedTeacher) ? true : false;
+                if(isDeleted){
+                    teacherTv.getItems().remove(selectedTeacher);
+                    System.out.println("teacher deleted");
+                }else{
+                    System.out.println("teacher deleted unsucessfully !");
+                }
+            }
+        }
+    }
+}
