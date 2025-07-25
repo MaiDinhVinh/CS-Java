a1f7126dedb63b53400c8a7844b8812b3492af84
DucksaberVN <business@ducksabervn.com>
2025-07-26 01:29:45 +0700

    java 17 migration part 2 completed

--- OCP17/JSE17Migration/src/Main.java
+++ OCP17/JSE17Migration/src/Main.java
@@ -1,4 +1,8 @@
-import java.sql.SQLOutput;
+import java.time.*;
+import java.time.chrono.*;
+import java.time.zone.*;
+import java.time.temporal.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -92,6 +96,58 @@ public class Main {
                 System.out.print("Invalid value");
                 break;
         }
+    }
+
+    //for snippet 55
+
+    private static void performAnimalEnrichment1(LocalDate start, LocalDate end) {
+        var upTo = start;
+        while (upTo.isBefore(end)) { // check if still before end
+            System.out.println("give new toy: " + upTo);
+            upTo = upTo.plusMonths(1); // add a month
+        } }
+
+    //for snippet 56
+
+    private static void performAnimalEnrichment2(LocalDate start, LocalDate end,
+                                                Period period) { // uses the generic period
+        var upTo = start;
+        while (upTo.isBefore(end)) {
+            System.out.println("give new toy: " + upTo);
+            upTo = upTo.plus(period); // adds the period
+        } }
+
+
+    //for snippet 72
+    public void oneException() throws IllegalArgumentException {}
+
+    //for snippet 73
+    public void twoExceptions() throws
+            IllegalArgumentException, InterruptedException {}
+
+    //for snippet 74 - which one is "effectively ..." ?
+    public String zooFriends() {
+        String name = "Harry the Hippo";
+        var size = 10;
+        boolean wet;
+        if(size > 100) size++;
+        name.substring(0);
+        wet = true;
+        return name;
+    }
+
+    //for snippet 75
+    public static void walk1(int... steps) {
+        //for snippet 77
+        int[] step2 = steps;
+        System.out.print(step2.length);
+    }
+    public static void walk2(int start, int... steps) {}
+//    public static void walk3(int... steps, int start) {}
+//    public static void walk4(int... start, int... steps) {}
+
+    public static void walkDog(int start, int... steps) {
+        System.out.println(steps.length);
     }
 
     public static void main(String[] args) {
@@ -275,7 +331,7 @@ public class Main {
 
         //snippet 32
 
-        int s = 10;
+//        int s = 10;
 //        int b = switch (s) {
 //            case 1 -> 10;
 //            case 2 -> 30;
@@ -385,5 +441,231 @@ public class Main {
 //        System.out.println("%9.4f".formatted(443.234));
 //        System.out.println("%08.3f".formatted(443.234));
 //        System.out.println("%09.4f".formatted(443.234));
+
+
+        ////////////////////////////////////////////////////////////////////////
+        ////// SECTION 2
+        ////////////////////////////////////////////////////////////////////////
+
+        //snippet 45
+
+//        System.out.println(Arrays.compare(new int[] {1}, new int[] {2}));
+
+        //snipept 46
+//        System.out.println(Arrays.compare(new int[] {1}, new String[] {"a"}));
+
+        //snippet 47
+//        System.out.println(Arrays.mismatch(new int[] {1}, new int[] {1}));
+//        System.out.println(Arrays.mismatch(new String[] {"a"},
+//                new String[] {"A"}));
+//        System.out.println(Arrays.mismatch(new int[] {1, 2}, new int[] {1}));
+
+        //snippet 48
+//        System.out.println(LocalDate.now());
+//        System.out.println(LocalTime.now());
+//        System.out.println(LocalDateTime.now());
+//        System.out.println(ZonedDateTime.now()); //explain printed output
+
+        //snippet 49
+
+//        var date1 = LocalDate.of(2022, Month.JANUARY, 20);
+//        var date2 = LocalDate.of(2022, 1, 20);
+
+        //snippet 50
+
+//        var date1 = LocalDate.of(2022, Month.JANUARY, 20);
+//        var date2 = LocalDate.of(2022, 1, 20);
+//        var time1 = LocalTime.of(6, 15);
+//        var time2 = LocalTime.of(6, 15, 30);
+//        var time3 = LocalTime.of(6, 15, 30, 200);
+//        var dateTime1 = LocalDateTime.of(2022, Month.JANUARY, 20, 6, 15, 30);
+//        var dateTime2 = LocalDateTime.of(date1, time1);
+//        var zone = ZoneId.of("US/Eastern");
+//        var zoned1 = ZonedDateTime.of(2022, 1, 20,
+//                6, 15, 30, 200, zone);
+//        var zoned2 = ZonedDateTime.of(date1, time1, zone);
+//        var zoned3 = ZonedDateTime.of(dateTime1, zone);
+
+        //snippet 51
+
+//        var d = new LocalDate();
+
+        //snippet 52
+
+//        var d = LocalDate.of(2022, Month.JANUARY, 32);
+
+        //snippet 53
+
+//        var date = LocalDate.of(2024, Month.JANUARY, 20);
+//        var time = LocalTime.of(5, 15);
+//        var dateTime = LocalDateTime.of(date, time)
+//                .minusDays(1).minusHours(10).minusSeconds(30);
+
+        //snippet 54
+
+//        var date = LocalDate.of(2024, Month.JANUARY, 20);
+//        date.plusDays(10); // ?
+//        System.out.println(date);
+
+        //snippet 55 - check performAnimalEnrichment1 method above
+
+//        var start = LocalDate.of(2022, Month.JANUARY, 1);
+//        var end = LocalDate.of(2022, Month.MARCH, 30);
+//        performAnimalEnrichment1(start, end);
+
+        //snippet 56 - check performAnimalEnrichment2 method above
+
+//        var start = LocalDate.of(2022, Month.JANUARY, 1);
+//        var end = LocalDate.of(2022, Month.MARCH, 30);
+//        var period = Period.ofMonths(1); // create a period
+//        performAnimalEnrichment(start, end, period);
+
+        //snippet 57
+
+//        var annually = Period.ofYears(1);
+//        var quarterly = Period.ofMonths(3);
+//        var everyThreeWeeks = Period.ofWeeks(3);
+//        var everyOtherDay = Period.ofDays(2);
+//        var everyYearAndAWeek = Period.of(1, 0, 7);
+
+        //snippet 58
+
+//        var wrong = Period.ofYears(1).ofWeeks(1);
+
+        //snippet 59
+
+//        var wrong = Period.ofYears(1);
+//        wrong = Period.ofWeeks(1);
+
+        //snippet 60
+
+//        System.out.println(Period.ofMonths(3));
+
+        //snippet 61
+//        var date = LocalDate.of(2022, 1, 20);
+//        var time = LocalTime.of(6, 15);
+//        var dateTime = LocalDateTime.of(date, time);
+//        var period = Period.ofMonths(1);
+//        System.out.println(date.plus(period));
+//        System.out.println(dateTime.plus(period));
+//        System.out.println(time.plus(period));
+
+        //snippet 62
+
+//        var daily = Duration.ofDays(1);
+//        var hourly = Duration.ofHours(1);
+//        var everyMinute = Duration.ofMinutes(1);
+//        var everyTenSeconds = Duration.ofSeconds(10);
+//        var everyMilli = Duration.ofMillis(1);
+//        var everyNano = Duration.ofNanos(1);
+
+        //snippet 63
+
+//        var daily = Duration.of(1, ChronoUnit.DAYS);
+//        var hourly = Duration.of(1, ChronoUnit.HOURS);
+//        var everyMinute = Duration.of(1, ChronoUnit.MINUTES);
+//        var everyTenSeconds = Duration.of(10, ChronoUnit.SECONDS);
+//        var everyMilli = Duration.of(1, ChronoUnit.MILLIS);
+//        var everyNano = Duration.of(1, ChronoUnit.NANOS);
+
+        //snippet 64
+
+//        var one = LocalTime.of(5, 15);
+//        var two = LocalTime.of(6, 30);
+//        var date = LocalDate.of(2016, 1, 20);
+//        System.out.println(ChronoUnit.HOURS.between(one, two));
+//        System.out.println(ChronoUnit.MINUTES.between(one, two));
+//        System.out.println(ChronoUnit.MINUTES.between(one, date)); //?
+
+        //snippet 65
+
+//        LocalTime time = LocalTime.of(3,12,45);
+//        System.out.println(time);
+//        LocalTime truncated = time.truncatedTo(ChronoUnit.MINUTES);
+//        System.out.println(truncated);
+//        truncated = time.truncatedTo(ChronoUnit.DAYS);
+//        System.out.println(truncated);
+
+        //snippet 66
+
+//        var date = LocalDate.of(2022, 5, 25);
+//        var period = Period.ofDays(1);
+//        var days = Duration.ofDays(1);
+//        System.out.println(date.plus(period));
+//        System.out.println(date.plus(days));
+
+        //snippet 67
+
+//        var now = Instant.now();
+//        // do something time consuming
+//        var later = Instant.now();
+
+        //snippet 68
+
+//        var duration = Duration.between(now, later);
+//        System.out.println(duration.toMillis());
+
+        //snippet 69
+
+//        var date = LocalDate.of(2022, 5, 25);
+//        var time = LocalTime.of(11, 55, 00);
+//        var zone = ZoneId.of("US/Eastern");
+//        var zonedDateTime = ZonedDateTime.of(date, time, zone);
+//        var instant = zonedDateTime.toInstant();
+//        System.out.println(zonedDateTime);
+//        System.out.println(instant);
+
+        //snippet 70
+
+//        var date = LocalDate.of(2022, Month.MARCH, 13);
+//        var time = LocalTime.of(1, 30);
+//        var zone = ZoneId.of("US/Eastern");
+//        var dateTime = ZonedDateTime.of(date, time, zone);
+//        System.out.println(dateTime);
+//        System.out.println(dateTime.getHour());
+//        System.out.println(dateTime.getOffset());
+//        dateTime = dateTime.plusHours(1);
+//        System.out.println(dateTime);
+//        System.out.println(dateTime.getHour());
+//        System.out.println(dateTime.getOffset());
+
+        //snippet 71
+
+//        var date = LocalDate.of(2022, Month.NOVEMBER, 6);
+//        var time = LocalTime.of(1, 30);
+//        var zone = ZoneId.of("US/Eastern");
+//        var dateTime = ZonedDateTime.of(date, time, zone);
+//        System.out.println(dateTime);
+//        dateTime = dateTime.plusHours(1);
+//        System.out.println(dateTime);
+//        dateTime = dateTime.plusHours(1);
+//        System.out.println(dateTime);
+
+        //snippet 72 - check oneException method above
+
+        //snippet 73 - check twoExceptions method above
+
+        //snippet 74 - check zooFriends method above
+
+        //snippet 75 - check walk1,walk2,walk3,walk4 methods above
+
+        //snippet 76
+
+//        int[] data = new int[] {1, 2, 3};
+//        walk1(data);
+//        walk1(1,2,3);
+
+        //snippet 77 - check inside method walk1 above
+
+        //snippet 78 - check walkDog method above
+
+//        walkDog(1); // 0
+//        walkDog(1, 2); // 1
+//        walkDog(1, 2, 3); // 2
+//        walkDog(1, new int[] {4, 5});
+
+        //snippet 79
+
+//        walkDog(1, null);
     }
 }
