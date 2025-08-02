a8246b178bf1b8330677670ed20c06c9eb6d180b
DucksaberVN <business@ducksabervn.com>
2025-08-03 01:57:38 +0700

    chapter 6 completed, chapter 7 partially completed

---
+++ OCP17/JSE17C6/src/Zoo.java
@@ -0,0 +1,12 @@
+public class Zoo {
+    public Zoo() {
+        System.out.println("Zoo created");
+//        super();
+    }
+
+    public Zoo(int a) {
+        super();
+        System.out.println("Zoo created");
+//        super();
+    }
+}
