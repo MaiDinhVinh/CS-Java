cdd0165a7ed9c27e518f191d7e84420d36db7412
DucksaberVN <business@ducksabervn.com>
2025-08-03 01:57:12 +0700

    chapter 6 completed, chapter 7 partially completed

---
+++ OCP17/JSE17C6/src/Season2.java
@@ -0,0 +1,9 @@
+public enum Season2 {
+    WINTER("Low"), SPRING("Medium"), SUMMER("High"), FALL("Medium");
+    private final String expectedVisitors;
+    private Season2(String expectedVisitors) {
+        this.expectedVisitors = expectedVisitors;
+    }
+    public void printExpectedVisitors() {
+        System.out.println(expectedVisitors);
+    } }
