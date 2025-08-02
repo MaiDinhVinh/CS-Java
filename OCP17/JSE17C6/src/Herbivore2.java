9db38b8f84295783e42b5510f9c3a5b2033ab730
DucksaberVN <business@ducksabervn.com>
2025-08-03 01:55:44 +0700

    chapter 6 completed, chapter 7 partially completed

---
+++ OCP17/JSE17C6/src/Herbivore2.java
@@ -0,0 +1,8 @@
+public interface Herbivore2 { public void eatPlants(); }
+interface Omnivore2 { public int eatPlants(); }
+//class Tiger implements Herbivore2, Omnivore2 {
+//    @Override
+//    public void eatPlants(){
+//        System.out.println("hello");
+//    }
+//}
