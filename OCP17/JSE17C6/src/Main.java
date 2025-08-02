2f1564db04111277f9fc8b48144cb85f273127e4
DucksaberVN <business@ducksabervn.com>
2025-08-03 01:55:49 +0700

    chapter 6 completed, chapter 7 partially completed

---
+++ OCP17/JSE17C6/src/Main.java
@@ -0,0 +1,78 @@
+public class Main{
+    public static void main(String[] args) {
+
+        //snippet 1 - Check Hamster class
+
+        //snippet 2 - check Gopher class
+
+        //snippet 3 - Check Zoo class
+
+        //snippet 4 - Check Mammal, "Seal", and Elephant classes
+
+        //snippet 5 - Check MouseHouse1 class
+
+        //snippet 6 - Check MouseHouse2 class
+
+        //snippet 7 - Check ZooTickets class
+
+        //snippet 8 - Check Chimpanzee class
+
+        //snippet 9 - Check Cuttlefish class
+
+        //snippet 10 - Check Okapi class
+
+        //snippet 11 - Check Beetle and RhinocerosBeetle classes
+
+        //snippet 12 - Check Bear and Panda classes
+
+        //snippet 13 - Check Meerkat class
+
+        //snippet 14 - Check Canine1 class
+
+        //snippet 15 - Check Canine2 class
+
+        //snippet 16 - check Canine2 class again
+
+        //snippet 17 - Check Animal class
+
+        //snippet 18 - Check Platypus class
+
+        //snippet 19 - check Nocturnal1 interface
+
+        //snippet 20 - check Herbivore interface
+
+        //snippet 21 - check Herbivore2 interface
+
+        //snippet 22 - check Walk interface
+
+        //snippet 23 - check Walk interface again
+
+        //snippet 24 - Check Walk interface again
+
+        //snippet 25 - check Schedule interface
+
+        //snippet 26 - check ZooTrainTour interface
+
+        //snippet 27 - check Season Enum
+
+        //snippet 28 - check Season Enum again
+
+        //snippet 29 - check Season Enum again
+
+        //snippet 30 - check Season Enum again
+
+        //snippet 31 - check Season Enum again
+
+        //snippet 32 - check Season Enum again
+
+        //snippet 33 - check Season2 Enum
+
+        //snippet 34 - check OnlyOne Enum
+
+        //snippet 35 - check Season3 Enum
+
+        //snippet 36 - check Season4 Enum
+
+        //snippet 37 - check Season5 Enum
+    }
+}
