a64959bdb1a966e49c05c538d359dcdc3f772295
DucksaberVN <business@ducksabervn.com>
2025-08-03 01:56:05 +0700

    chapter 6 completed, chapter 7 partially completed

---
+++ OCP17/JSE17C6/src/MouseHouse1.java
@@ -0,0 +1,10 @@
+public class MouseHouse1{
+    private final int volume;
+    private final String name;
+    public MouseHouse1() {
+        this.name = "Empty House"; //?
+    }
+    {
+        volume = 10; //?
+    }
+}
