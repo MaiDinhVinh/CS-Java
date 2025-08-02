37aeb4b809999aac83138622b8321ffff6e2ddcd
DucksaberVN <business@ducksabervn.com>
2025-08-03 01:57:22 +0700

    chapter 6 completed, chapter 7 partially completed

---
+++ OCP17/JSE17C6/src/Season4.java
@@ -0,0 +1,10 @@
+public enum Season4 {
+    WINTER {
+        public String getHours() { return "10am-3pm"; }
+    },
+    SUMMER {
+        public String getHours() { return "9am-7pm"; }
+    },
+    SPRING, FALL;
+    public String getHours() { return "9am-5pm"; }
+}
