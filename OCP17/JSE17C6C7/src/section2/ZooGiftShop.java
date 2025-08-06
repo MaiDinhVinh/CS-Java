1681da534da1bfea0407659b1b24858674405c8f
DucksaberVN <business@ducksabervn.com>
2025-08-07 00:40:39 +0700

    chapter 7 completed

---
+++ OCP17/JSE17C6C7/src/section2/ZooGiftShop.java
@@ -0,0 +1,13 @@
+package section2;
+
+public class ZooGiftShop {
+    abstract class SaleTodayOnly {
+        abstract int dollarsOff();
+    }
+    public int admission(int basePrice) {
+        SaleTodayOnly sale = new SaleTodayOnly() {
+            int dollarsOff() { return 3; }
+        }; // Don't forget the semicolon!
+        return basePrice - sale.dollarsOff();
+    }
+}
