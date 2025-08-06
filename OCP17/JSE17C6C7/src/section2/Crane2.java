9734dc469d0541f92755a1c41a167f29ea1ed48e
DucksaberVN <business@ducksabervn.com>
2025-08-07 00:37:45 +0700

    chapter 7 completed

---
+++ OCP17/JSE17C6C7/src/section2/Crane2.java
@@ -0,0 +1,17 @@
+package section2;
+
+public final class Crane2 {
+    private final int numberEggs;
+    private final String name;
+    public Crane2(int numberEggs, String name) {
+        if (numberEggs >= 0) this.numberEggs = numberEggs; // guard condition
+        else throw new IllegalArgumentException();
+        this.name = name;
+    }
+    public int getNumberEggs() {
+        return numberEggs;
+    }
+    public String getName() {
+        return name;
+    }
+}
