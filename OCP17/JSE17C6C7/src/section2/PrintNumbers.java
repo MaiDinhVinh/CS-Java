947df8f7a368ba6a896ad3e526c3de0b2df7fb7c
DucksaberVN <business@ducksabervn.com>
2025-08-07 00:39:18 +0700

    chapter 7 completed

---
+++ OCP17/JSE17C6C7/src/section2/PrintNumbers.java
@@ -0,0 +1,33 @@
+package section2;
+
+public class PrintNumbers {
+    private int length = 5;
+    public void calculate() {
+        final int width = 20;
+        class Calculator {
+            public void multiply() {
+                System.out.print(length * width);
+            }
+        }
+        var calculator = new Calculator();
+        calculator.multiply();
+    }
+    public static void main(String[] args) {
+         var printer = new PrintNumbers();
+         printer.calculate();
+    }
+
+    //for snippet 74
+
+//    public void processData() {
+//        final int length = 5;
+//        int width = 10;
+//        int height = 2;
+//        class VolumeCalculator {
+//            public int multiply() {
+//                return length * width * height; // DOES NOT COMPILE
+//            }
+//        }
+//        width = 2;
+//    }
+ }
