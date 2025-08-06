26db935ee29106bcd150b240ccab688bab92fcda
DucksaberVN <business@ducksabervn.com>
2025-08-07 00:37:39 +0700

    chapter 7 completed

---
+++ OCP17/JSE17C6C7/src/section2/Crane.java
@@ -0,0 +1,10 @@
+package section2;
+
+public class Crane {
+    int numberEggs;
+    String name;
+    public Crane(int numberEggs, String name) {
+        this.numberEggs = numberEggs;
+        this.name = name;
+    }
+}
