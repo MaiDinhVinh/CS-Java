ada8e291b03b20a223b1d52ff5b3cdf60d52c322
DucksaberVN <business@ducksabervn.com>
2025-08-07 00:38:43 +0700

    chapter 7 completed

---
+++ OCP17/JSE17C6C7/src/section2/Frog.java
@@ -0,0 +1,5 @@
+//public class sealed Frog permits GlassFrog {}
+//final class GlassFrog extends Frog {}
+//abstract sealed class Wolf permits Timber {}
+//final class Timber extends Wolf {}
+//final class MyWolf extends Wolf {}
