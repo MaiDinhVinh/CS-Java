a43498fd9f1082acd8885895034719d58eec9b1f
DucksaberVN <business@ducksabervn.com>
2025-08-07 00:38:32 +0700

    chapter 7 completed

---
+++ OCP17/JSE17C6C7/src/section2/Fish.java
@@ -0,0 +1,11 @@
+package section2;
+
+class Bird2 {}
+public class Fish {
+    public static void main(String[] args) {
+        Fish fish = new Fish();
+//        if (fish instanceof Bird2 b) {
+//            // Do stuff
+//        }
+    }
+}
