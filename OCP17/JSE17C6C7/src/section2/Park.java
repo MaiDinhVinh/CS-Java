3e38b9667f093a5c1e2e53e0ce59747552c5a3b7
DucksaberVN <business@ducksabervn.com>
2025-08-07 00:39:06 +0700

    chapter 7 completed

---
+++ OCP17/JSE17C6C7/src/section2/Park.java
@@ -0,0 +1,11 @@
+package section2;
+
+public class Park {
+    static class Ride {
+        private int price = 6;
+    }
+    public static void main(String[] args) {
+        var ride = new Ride();
+        System.out.println(ride.price);
+    }
+}
