034fe8c443d8f2c4c68cf636d8054cb2dc6b8c39
DucksaberVN <business@ducksabervn.com>
2025-08-10 00:55:54 +0700

    chapter 8 partially completed

---
+++ OCP17/JSE17C8/src/Main.java
@@ -0,0 +1,39 @@
+public class Main{
+    public static void main(String[] args) {
+        //snippet 1 - Check Animal record
+
+        //snippet 2 - Check Animal record again
+
+        //snippet 3 - Check Animal record again
+
+        //snippet 4 - Check Animal record again
+
+        //snippet 5 - check Sprint interface
+
+        //snippet 6 - check Dance interface
+
+        //snippet 7 - check Dash interface
+
+        //snippet 8 - check Soar interface
+
+        //snippet 9 - check LearnToSpeak interface
+
+        //snippet 10 - check LearnToSpeak interface again
+
+        //snippet 11 - check Converter interface
+
+        //snippet 12 - check StringStart interface
+
+        //snippet 13 - check StringChecker interface
+
+        //snippet 14 - check StringChecker interface again
+
+        //snippet 15 - check StringParameterChecker interface
+
+        //snippet 16 - check StringTwoParameterChecker interface
+
+        //snippet 17 - check EmptyStringCreator interface
+
+        //snippet 18 - check StringCopier interface
+    }
+}
