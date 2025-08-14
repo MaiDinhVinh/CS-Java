5736cfdf8635ae4752885db1112c17a83b1bc5f7
DucksaberVN <business@ducksabervn.com>
2025-08-14 13:08:11 +0700

    chapter 8 completed, chapter 9 begin

--- OCP17/JSE17C6C7/src/section2/ZooGiftShop.java
+++ OCP17/JSE17C6C7/src/section2/ZooGiftShop.java
@@ -1,12 +1,13 @@
 package section2;
 
 public class ZooGiftShop {
+    private int a;
     abstract class SaleTodayOnly {
         abstract int dollarsOff();
     }
     public int admission(int basePrice) {
         SaleTodayOnly sale = new SaleTodayOnly() {
-            int dollarsOff() { return 3; }
+            int dollarsOff() { return 3 - ZooGiftShop.this.a; }
         }; // Don't forget the semicolon!
         return basePrice - sale.dollarsOff();
     }
