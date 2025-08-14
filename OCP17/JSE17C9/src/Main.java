fab6465008528be340717660d7ec505045184cd0
DucksaberVN <business@ducksabervn.com>
2025-08-14 13:08:51 +0700

    chapter 8 completed, chapter 9 begin

---
+++ OCP17/JSE17C9/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        //snippet 1
+
+//        Collection<String> list = new ArrayList<>();
+//        list.add("Magician");
+//        list.add("Assistant");
+//        System.out.println(list); // [Magician, Assistant]
+//        list.removeIf(s -> s.startsWith("A"));
+//        System.out.println(list);
+
+        //snippet 2
+
+//        Collection<String> set = new HashSet<>();
+//        set.add("Wand");
+//        set.add("");
+//        set.removeIf(String::isEmpty); // s -> s.isEmpty()
+//        System.out.println(set);
+
+        //snippet 3
+
+//        Collection<String> cats = List.of("Annie", "Ripley");
+//        cats.forEach(System.out::println);
+//        cats.forEach(c -> System.out.println(c));
+
+        //snippet 4
+
+//        var list1 = List.of(1, 2);
+//        var list2 = List.of(2, 1);
+//        var set1 = Set.of(1, 2);
+//        var set2 = Set.of(2, 1);
+//
+//        System.out.println(list1.equals(list2));
+//        System.out.println(set1.equals(set2));
+//        System.out.println(list1.equals(set1));
+
+        //snippet 5
+
+//        var heights = new ArrayList<Integer>();
+//        heights.add(null);
+//        int h = heights.get(0);
+    }
+}
