7b198ef862d4c6f782d1c7bb17ad892962bf40f5
DucksaberVN <business@ducksabervn.com>
2025-11-06 19:09:15 +0700

    ocp17 comeback

---
+++ OCP17/OCP17C1/src/NumberPicker4.java
@@ -0,0 +1,10 @@
+import java.lang.System; //explain these imports
+import java.lang.*;
+import java.util.Random;
+import java.util.*;
+public class NumberPicker4 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+    }
+}
