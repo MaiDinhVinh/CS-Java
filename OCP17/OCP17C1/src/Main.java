d6d8abfafd867275609bcafe76cd8d332a2c4e9a
DucksaberVN <business@ducksabervn.com>
2025-11-06 19:08:52 +0700

    ocp17 comeback

---
+++ OCP17/OCP17C1/src/Main.java
@@ -0,0 +1,54 @@
+public class Main{
+    public static void main(String[] args) {
+        //snippet 1 - Check Animal1 class
+
+        //snippet 2 - Check Animal2 class
+
+        //snippet 3 - Check Animal3 class
+
+        //snippet 4 - Check Animal4 class
+
+        //snippet 5 - Check Animal5 class
+
+        //snippet 6 - Check Zoo1 class
+
+        //snippet 7 - Check Zoo2 class
+
+        //snippet 8 - Check NumberPicker class
+
+        //snippet 9 - Check NumberPicker2 class
+
+        //snippet 10 - Check NumberPicker3 class
+
+        //snippet 11 - Check NumberPicker4 class
+
+        //snippet 12 - Check Conflicts1 class
+
+        //snippet 13 - Check Conflicts2 class
+
+        //snippet 14 - Check Conflict3 class
+
+        //snippet 15 - Check Conflict4 class
+
+        //snippet 16 - Check Conflict5 class
+
+        //snippet 17 - Check packagea and packageb classes
+
+        //snippet 18 - use ??? to compile packagea and packageb
+        //note: try to swap position as well
+
+        //snippet 19 - use ??? to run packagea and packageb
+
+        //snippet 20 - use ??? with ? flag to run packagea and packageb
+
+        //snippet 21 - use ??? with ? flag to compile and run packagea and package b
+
+        //snippet 22 - use ??? to zip packagea and packageb
+
+        //snippet 23 - check Meerkat in structure package
+
+        //snippet 24 - check Meerkat2 in structure package
+
+        //snippet 25 - check Meerkat3 in structure package
+    }
+}
