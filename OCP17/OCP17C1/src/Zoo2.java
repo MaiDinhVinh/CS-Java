beba04dfee12e3e91936b85dcdb86526f86a09c2
DucksaberVN <business@ducksabervn.com>
2025-11-06 19:09:57 +0700

    ocp17 comeback

---
+++ OCP17/OCP17C1/src/Zoo2.java
@@ -0,0 +1,7 @@
+public class Zoo2 {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        //explain what does this do ?
+    }
+}
