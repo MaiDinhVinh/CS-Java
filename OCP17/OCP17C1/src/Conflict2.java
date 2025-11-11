2f106b597456582ced07ce17868b1925844dc0a8
DucksaberVN <business@ducksabervn.com>
2025-11-11 14:06:16 +0700

    Update Conflict2.java

--- OCP17/OCP17C1/src/Conflict2.java
+++ OCP17/OCP17C1/src/Conflict2.java
@@ -1,8 +1,8 @@
-import java.util.Date;
-
-//explain ?
-
+//import java.util.Date;
+//
+////explain ?
+//
 //import java.sql.Date;
-public class Conflict2 {
-    Date date;
-}
+//public class Conflict2 {
+//    Date date;
+//}
