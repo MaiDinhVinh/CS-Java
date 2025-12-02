8cc57cc31cd02e28a0ca50872f1e02e2a7e3dcc8
DucksaberVN <business@ducksabervn.com>
2025-12-02 07:07:24 +0700

    chapter 6-7 revise finished

--- OCP17/old progress/JSE17C6C7/src/section2/Frog.java
+++ OCP17/old progress/JSE17C6C7/src/section2/Frog.java
@@ -1,5 +1,6 @@
-//public class sealed Frog permits GlassFrog {}
+//package section2;
+//public sealed class Frog permits GlassFrog {}
 //final class GlassFrog extends Frog {}
-//abstract sealed class Wolf permits Timber {}
-//final class Timber extends Wolf {}
-//final class MyWolf extends Wolf {}
+//abstract sealed class Wolf2 permits Timber {}
+//final class Timber extends Wolf2 {}
+//final class MyWolf extends Wolf2 {}
