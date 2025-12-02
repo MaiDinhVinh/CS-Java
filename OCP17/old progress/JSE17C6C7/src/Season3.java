8ca203e9e56aaf6e0585a21a5227efd7be83b2d9
DucksaberVN <business@ducksabervn.com>
2025-12-02 07:06:53 +0700

    chapter 6-7 revise finished

--- OCP17/old progress/JSE17C6C7/src/Season3.java
+++ OCP17/old progress/JSE17C6C7/src/Season3.java
@@ -1,15 +1,19 @@
 public enum Season3 {
     WINTER {
-        public String getHours() { return "10am-3pm"; }
+        public String getHours()
+        { return "10am-3pm"; }
     },
     SPRING {
-        public String getHours() { return "9am-5pm"; }
+        public String getHours()
+        { return "9am-5pm"; }
     },
     SUMMER {
-        public String getHours() { return "9am-7pm"; }
+        public String getHours()
+        { return "9am-7pm"; }
     },
     FALL {
-        public String getHours() { return "9am-5pm"; }
+        public String getHours()
+        { return "9am-5pm"; }
     };
     public abstract String getHours();
 }
