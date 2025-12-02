2b4871001a04648505e83fce9f84b9d53428dcf7
DucksaberVN <business@ducksabervn.com>
2025-12-02 07:07:08 +0700

    chapter 6-7 revise finished

--- OCP17/old progress/JSE17C6C7/src/section2/Fish.java
+++ OCP17/old progress/JSE17C6C7/src/section2/Fish.java
@@ -1,11 +1,15 @@
 package section2;
 
 class Bird2 {}
-public class Fish {
+public class Fish extends Bird2{
     public static void main(String[] args) {
-        Fish fish = new Fish();
+//        Fish fish = new Fish();
 //        if (fish instanceof Bird2 b) {
 //            // Do stuff
 //        }
+        Bird2 b = new Bird2();
+        if (b instanceof Fish){
+            //do stuff
+        }
     }
 }
