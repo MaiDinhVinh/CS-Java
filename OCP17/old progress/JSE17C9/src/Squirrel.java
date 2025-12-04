c2add090841ed91b383f73634fca3a0d8cfd1c86
DucksaberVN <business@ducksabervn.com>
2025-12-04 19:57:10 +0700

    Create Squirrel.java

---
+++ OCP17/old progress/JSE17C9/src/Squirrel.java
@@ -0,0 +1,20 @@
+public class Squirrel {
+    private int weight;
+    private String species;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
