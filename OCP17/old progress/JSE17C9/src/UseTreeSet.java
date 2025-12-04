09844d9e492e1ad5f79b6c1000cda4882f3d72ce
DucksaberVN <business@ducksabervn.com>
2025-12-04 19:57:16 +0700

    Create UseTreeSet.java

---
+++ OCP17/old progress/JSE17C9/src/UseTreeSet.java
@@ -0,0 +1,19 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UseTreeSet {
+    static class Rabbit{ int id; }
+    public static void main(String[] args) {
+        Set<Duck> ducks = new TreeSet<>();
+        ducks.add(new Duck("Puddles"));
+
+        Set<Rabbit> rabbits = new TreeSet<>();
+//        rabbits.add(new Rabbit()); //explain ?
+
+
+        //for snippet 53
+
+//        Set<Rabbit> rabbits2 = new TreeSet<>((r1, r2) -> r1.id - r2.id);
+//        rabbits.add(new Rabbit());
+    }
+}
