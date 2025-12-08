73818e06f52d80f770023642a95185ddf63cfddc
DucksaberVN <business@ducksabervn.com>
2025-12-08 19:58:20 +0700

    Create CrateRecord.java

---
+++ OCP17/old progress/JSE17C9/src/CrateRecord.java
@@ -0,0 +1,8 @@
+public record CrateRecord<T>(T contents) {
+    @Override
+    public T contents() {
+        if (contents == null)
+            throw new IllegalStateException("missing contents");
+        return contents;
+    }
+}
