2b7f70052e24e94be0a2533bd86f84d9f3117f3d
DucksaberVN <business@ducksabervn.com>
2025-12-08 19:59:47 +0700

    Create SizeLimitedCrate.java

---
+++ OCP17/old progress/JSE17C9/src/SizeLimitedCrate.java
@@ -0,0 +1,8 @@
+public class SizeLimitedCrate<T, U> {
+    private T contents;
+    private U sizeLimit;
+    public SizeLimitedCrate(T contents, U sizeLimit) {
+        this.contents = contents;
+        this.sizeLimit = sizeLimit;
+    }
+}
