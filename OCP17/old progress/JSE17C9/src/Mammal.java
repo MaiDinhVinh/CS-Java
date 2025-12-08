d1dd1759a16886ac600616b77261701b79fd29f5
DucksaberVN <business@ducksabervn.com>
2025-12-08 19:59:05 +0700

    Create Mammal.java

---
+++ OCP17/old progress/JSE17C9/src/Mammal.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mammal {
+    public List<CharSequence> play() {
+        return null;
+    }
+    public CharSequence sleep() {
+        return null;
+    }
+}
+class Monkey extends Mammal {
+    public ArrayList<CharSequence> play() {
+        return null;
+    }
+}
+class Goat extends Mammal {
+//    public List<String> play() { ... }
+    public String sleep() {
+        return null;
+    }
+}
