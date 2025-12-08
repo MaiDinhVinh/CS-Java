dd16964d5002626809d8229de8a5121bdfaee092
DucksaberVN <business@ducksabervn.com>
2025-12-08 19:58:53 +0700

    Create LongTailAnimal2.java

---
+++ OCP17/old progress/JSE17C9/src/LongTailAnimal2.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongTailAnimal2 {
+    protected void chew(List<Object> input) {}
+}
+class Anteater extends LongTailAnimal2 {
+//    protected void chew(List<Double> input) {}
+
+    //for snippet 61
+    protected void chew(List<Object> input) {}
+    protected void chew(ArrayList<Double> input) {}
+}
