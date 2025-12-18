750f2d145c7cdfc4c74dbe62e3b0e2e055dcbc44
DucksaberVN <business@ducksabervn.com>
2025-12-18 18:30:18 +0700

    Update Main.java

--- OCP17/JSE17C10/src/Main.java
+++ OCP17/JSE17C10/src/Main.java
@@ -2,6 +2,10 @@ import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 import java.util.stream.*;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.minBy;
 
 public class Main{
     //for snippet 1
@@ -566,7 +570,150 @@ public class Main{
 //        newBag.tryAdvance(System.out::print);
 //        newBag.tryAdvance(System.out::print);
 
-        
+        ////////////////////////////////////
+        /// SECTION 3
+        ////////////////////////////////////
 
+        //snippet 83
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        String result = ohMy.collect(Collectors.joining(", "));
+//        System.out.println(result);
+
+        //snippet 84
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Double result = ohMy.collect(Collectors.averagingInt(String::length));
+//        System.out.println(result);
+
+        //snippet 85
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        TreeSet<String> result = ohMy
+//                .filter(s -> s.startsWith("t"))
+//                .collect(Collectors.toCollection(TreeSet::new));
+//        System.out.println(result);
+
+        //snippet 86
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<String, Integer> map = ohMy.collect(
+//                Collectors.toMap(s -> s, String::length));
+//        System.out.println(map);
+
+        //snippet 87
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Integer, String> map = ohMy.collect(Collectors.toMap(
+//                String::length,
+//                k -> k)); //explain ?
+
+        //snippet 88
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Integer, String> map = ohMy.collect(Collectors.toMap(
+//                String::length,
+//                k -> k,
+//                (s1, s2) -> s1 + "," + s2));
+//        System.out.println(map);
+//        System.out.println(map.getClass());
+
+        //snippet 89
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        TreeMap<Integer, String> map = ohMy.collect(Collectors.toMap(
+//                String::length,
+//                k -> k,
+//                (s1, s2) -> s1 + "," + s2,
+//                TreeMap::new));
+//        System.out.println(map);
+//        System.out.println(map.getClass());
+
+        //snippet 90
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Integer, List<String>> map = ohMy.collect(
+//                Collectors.groupingBy(String::length));
+//        System.out.println(map);
+
+        //snippet 91
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Integer, Set<String>> map = ohMy.collect(
+//                Collectors.groupingBy(
+//                        String::length,
+//                        Collectors.toSet()));
+//        System.out.println(map);
+
+        //snippet 92
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        TreeMap<Integer, Set<String>> map = ohMy.collect(
+//                Collectors.groupingBy(
+//                        String::length,
+//                        TreeMap::new,
+//                        Collectors.toSet()));
+//        System.out.println(map);
+
+        //snippet 93
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        TreeMap<Integer, List<String>> map = ohMy.collect(
+//                Collectors.groupingBy(
+//                        String::length,
+//                        TreeMap::new,
+//                        Collectors.toList()));
+//        System.out.println(map);
+
+        //snipept 94
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Boolean, List<String>> map = ohMy.collect(
+//                Collectors.partitioningBy(s -> s.length() <= 5));
+//        System.out.println(map);
+
+        //snippet 95
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Boolean, List<String>> map = ohMy.collect(
+//                Collectors.partitioningBy(s -> s.length() <= 7));
+//        System.out.println(map);
+
+        //snippet 96
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Boolean, Set<String>> map = ohMy.collect(
+//                Collectors.partitioningBy(
+//                        s -> s.length() <= 7,
+//                        Collectors.toSet()));
+//        System.out.println(map);
+
+        //snippet 97
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Integer, Long> map = ohMy.collect(
+//                Collectors.groupingBy(
+//                        String::length,
+//                        Collectors.counting()));
+//        System.out.println(map);
+
+        //snippet 98
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        Map<Integer, Optional<Character>> map = ohMy.collect(
+//                Collectors.groupingBy(
+//                        String::length,
+//                        Collectors.mapping(
+//                                s -> s.charAt(0),
+//                                Collectors.minBy((a, b) -> a - b)))); //explain ?
+//        System.out.println(map);
+
+        //snippet 99
+
+//        var ohMy = Stream.of("lions", "tigers", "bears");
+//        var map = ohMy.collect(groupingBy(String::length,
+//                Collectors.mapping(s -> s.charAt(0), minBy((a, b) -> a - b))));
+//        System.out.println(map); //error ?
+
+        //snippet 100 - check Main3
     }
 }
