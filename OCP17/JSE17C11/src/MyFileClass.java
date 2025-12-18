4fce0c122c6f231a6da5bef3dafd69e642724791
DucksaberVN <business@ducksabervn.com>
2025-12-18 18:31:14 +0700

    Create MyFileClass.java

---
+++ OCP17/JSE17C11/src/MyFileClass.java
@@ -0,0 +1,7 @@
+public class MyFileClass implements AutoCloseable {
+    private final int num;
+    public MyFileClass(int num) { this.num = num; }
+    @Override public void close() {
+        System.out.println("Closing: " + num);
+    }
+}
