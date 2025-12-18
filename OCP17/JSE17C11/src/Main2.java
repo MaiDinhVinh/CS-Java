8160e91050f8cf0f8e89c782eb078952699b5e78
DucksaberVN <business@ducksabervn.com>
2025-12-18 18:30:52 +0700

    Create Main2.java

---
+++ OCP17/JSE17C11/src/Main2.java
@@ -0,0 +1,30 @@
+class NoMoreCarrotsException extends Exception {}
+class Bunny {
+    public static void main(String[] args) throws NoMoreCarrotsException{
+        //for snippet 10
+
+//        eatCarrot();
+
+        //for snippet 11 - check ... clause
+
+        //for snippet 12
+
+//        try {
+//            eatCarrot();
+//        } catch (NoMoreCarrotsException e) {
+//            System.out.print("sad rabbit");
+//        }
+    }
+    private static void eatCarrot() throws NoMoreCarrotsException {}
+
+    //for snippet 13
+
+//    public void bad() {
+//        try {
+//            eatCarrot2();
+//        } catch (NoMoreCarrotsException e) {
+//            System.out.print("sad rabbit");
+//        }
+//    }
+//    private void eatCarrot2() {}
+}
