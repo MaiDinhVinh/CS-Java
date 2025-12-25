4d23a199519c83c0f96042ba29fb8e811fcfbb88
DucksaberVN <business@ducksabervn.com>
2025-12-25 14:14:20 +0700

    Create Main.java

---
+++ OCP17/JSE17C12/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        //snippet 1 - check zoo.animal.feeding module
+        // also explain InteliJ special case and how to create a Java module in InteliJ)
+
+        //snippet 2 - compile the zoo.animal.feeding module using javac (explain first then run)
+
+        //snippet 3 - run the zoo.animal.feeding (explain first then run)
+
+        //snippet 4 - repeat snippet 3 but use shorter ...
+
+        //snippet 5 - pack the module into Jar file using ...
+
+        //snippet 6 - run the jar file
+
+        //snippet 7 - check the zoo.animal.feeding mod-in4.java
+
+        //snippet 8 - recompile and rejar the module
+
+        //snippet 9 - check zoo.animal.care module
+
+        //snipept 10 - compile and run the zoo.animal.care module
+
+        //snippet 11 - check zoo.animal.talk module - also compile and run too
+
+        //snippet 12 - check zoo.staff module - also compile and run too
+
+        //snippet 13 - check zoo.animal.talks module-info.java file
+    }
+}
