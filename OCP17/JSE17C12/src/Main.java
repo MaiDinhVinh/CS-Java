e153fd3a68cffc42360221e3149284ed70f41d73
DucksaberVN <business@ducksabervn.com>
2025-12-29 15:20:58 +0700

    Update Main.java

--- OCP17/JSE17C12/src/Main.java
+++ OCP17/JSE17C12/src/Main.java
@@ -26,5 +26,68 @@ public class Main {
         //snippet 12 - check zoo.staff module - also compile and run too
 
         //snippet 13 - check zoo.animal.talks module-info.java file
+
+
+        /////////////////////////////////
+        /// SECTION 2
+        /////////////////////////////////
+
+        //snippet 14 - check zoo.animal.care mod-info
+
+        //snippet 15 - check zoo.animal.talks mod-info
+
+        //snippet 16 - check zoo.animal.talks mod-info
+
+        //snippet 17 - check zoo.animal.talks mod-info
+
+        //snippet 18 - check zoo.animal.talks mod-info
+
+        //snippet 19 - check zoo.tours.api module
+
+        //snippet 20 - check Tour interface in api module
+
+        //snippet 21 - check zoo.tours.api mod-info
+
+        //snippet 22 - compile and jar the api module
+
+        //snippet 23 - check TourFinder class in zoo.tours.reservation module
+
+        //snippet 24 - check zoo.tours.reservation mod-info
+
+        //snippet 25 - compile and jar the zoo.tours.reservation module
+
+        //snippet 26 - check zoo.visitor module
+
+        //snippet 27 - check zoo.visitor mod-info
+
+        //snippet 28 - compile and jar the zoo.visitor module
+
+        //snippet 29 - check zoo.tours.agency module
+
+        //snippet 30 - check zoo.tours.agency mod-info
+
+        //snipept 31 - compile and jarthe zoo.tours.agency module
+
+        //snippet 32 - run the java command on feeding module
+
+        //snippet 33 - check care mod-info and run the java command
+
+        //snippet 34 - run the java command with jar directory
+
+        //snippet 35 - run the java command with jar directory
+
+        //snippet 36 - run the jar command with zoo.animal.feeding jar file
+
+        //snippet 37 - check Animatronic class in zoo.dinos module
+
+        //snippet 38 - compile the Animatronic class and jar it
+
+        //snippet 39 - run jdeps on the Animatronic jar
+
+        //snippet 40 - run jdeps with ... flags and ... flags on the Animatronic jar
+
+        //snippet 41 - run jdeps with .. flag on Animatronic jar
+
+        //snippet 42 - run jlnk with talks module
     }
 }
