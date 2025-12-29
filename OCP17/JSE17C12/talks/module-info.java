b774799ed3b3cff2173e7915da3eb5552fedd401
DucksaberVN <business@ducksabervn.com>
2025-12-29 15:21:21 +0700

    Update module-info.java

--- OCP17/JSE17C12/talks/module-info.java
+++ OCP17/JSE17C12/talks/module-info.java
@@ -8,4 +8,26 @@ module zoo.animal.talks {
     //for snippet 13
 
     exports zoo.animal.talks.content to zoo.staff; //make sure to comment other
+
+    //for snippet 15 - comments out ... and turn off comment for snippet 14
+
+    //for snippet 16
+
+//    opens zoo.animal.talks.schedule;
+//    opens zoo.animal.talks.media to zoo.staff;
 }
+
+//for snippet 17
+
+//open module zoo.animal.talks{
+//        exports zoo.animal.talks.media;
+//        exports zoo.animal.talks.schedule;
+//        requires zoo.animal.feeding;
+//        requires zoo.animal.care;
+//}
+
+//for snippet 18
+
+//open module zoo.animal.talks{
+//    opens zoo.animal.talks.content
+//}
