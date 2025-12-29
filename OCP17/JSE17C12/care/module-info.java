d35016e7f13bbe9122d981747b7a609eb871b2ba
DucksaberVN <business@ducksabervn.com>
2025-12-29 15:19:32 +0700

    Update module-info.java

--- OCP17/JSE17C12/care/module-info.java
+++ OCP17/JSE17C12/care/module-info.java
@@ -1,4 +1,13 @@
 module zoo.animal.care {
     exports zoo.animal.care.medical;
     requires zoo.animal.feeding;
+
+    //for snippet 14
+
+//    requires transitive zoo.animal.feeding;
+
+    //for snippet 33 - disable other, this is for demo only
+
+//    exports zoo.animal.care.medical to zoo.staff;
+//    requires transitive zoo.animal.feeding;
 }
