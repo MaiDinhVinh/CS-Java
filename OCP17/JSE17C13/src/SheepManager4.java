f491f674add9248cc11588ab7514c6de55de6036
DucksaberVN <business@ducksabervn.com>
2026-01-03 18:51:39 +0700

    Create SheepManager4.java

---
+++ OCP17/JSE17C13/src/SheepManager4.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SheepManager4 {
+    private AtomicInteger sheepCount = new AtomicInteger(0);
+    private void incrementAndReport() {
+        synchronized (this){
+            System.out.print(sheepCount.incrementAndGet()+" ");
+        }
+    }
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(20);
+        try {
+            SheepManager4 manager = new SheepManager4();
+            for(int i = 0; i < 10; i++)
+                service.submit(() -> manager.incrementAndReport());
+        } finally {
+            service.shutdown();
+        }
+    }
+}
