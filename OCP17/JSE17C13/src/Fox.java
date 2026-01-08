2fc0536cd573478253bf97dc668beedb4e1b461e
DucksaberVN <business@ducksabervn.com>
2026-01-08 17:02:21 +0700

    Create Fox.java

---
+++ OCP17/JSE17C13/src/Fox.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.*;
+class Food {}
+class Water {}
+public record Fox(String name) {
+    public void eatAndDrink(Food food, Water water) {
+        synchronized(food) {
+            System.out.println(name() + " Got Food!");
+            move();
+            synchronized(water) {
+                System.out.println(name() + " Got Water!");
+            } } }
+    public void drinkAndEat(Food food, Water water) {
+        synchronized(water) {
+            System.out.println(name() + " Got Water!");
+            move();
+            synchronized(food) {
+                System.out.println(name() + " Got Food!");
+            } } }
+    public void move() {
+        try { Thread.sleep(100); } catch (InterruptedException e) {}
+    }
+    public static void main(String[] args) {
+// Create participants and resources
+        var foxy = new Fox("Foxy");
+        var tails = new Fox("Tails");
+        var food = new Food();
+        var water = new Water();
+// Process data
+        var service = Executors.newScheduledThreadPool(10);
+        try {
+            service.submit(() -> foxy.eatAndDrink(food,water));
+            service.submit(() -> tails.drinkAndEat(food,water));
+        } finally {
+            service.shutdown();
+        }
+    }
+}
