a9742036ee48f7235a5427d261748589989e0393
DucksaberVN <business@ducksabervn.com>
2026-01-18 19:15:19 +0700

    Create BabyChimpanzee.java

---
+++ OCP17/JSE17C14/src/BabyChimpanzee.java
@@ -0,0 +1,27 @@
+public class BabyChimpanzee extends Chimpanzee {
+    private static final long serialVersionUID = 3L;
+    private String mother = "Mom";
+
+    public BabyChimpanzee() {
+        super();
+    }
+
+    public BabyChimpanzee(String name, char type) {
+        super(name, 0, type);
+    }
+
+    public String getMother() {
+        return mother;
+    }
+
+    public void setMother(String mother) {
+        this.mother = mother;
+    }
+
+    @Override
+    public String toString() {
+        return "BabyChimpanzee{" +
+                "mother='" + mother + '\'' +
+                "} " + super.toString();
+    }
+}
