74c88d47ca5ad6584882982172385384284e988e
DucksaberVN <business@ducksabervn.com>
2026-01-18 19:15:31 +0700

    Create Cat2.java

---
+++ OCP17/JSE17C14/src/Cat2.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class Cat2 implements Serializable {
+    private Tail2 tail = new Tail2();
+}
+class Tail2 implements Serializable {
+    private Fur2 fur = new Fur2();
+}
+class Fur2 {}
